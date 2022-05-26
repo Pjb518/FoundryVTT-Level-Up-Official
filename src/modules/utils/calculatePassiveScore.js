39a8c1d0b35af3a29f62a76462a3d15f1bdf64f2
Phil <me@phillip.best>
2022-05-26 15:53:42 +0100

    Corrects the passive perception score calculation that was previously double-counting the global ability check bonus.

--- src/modules/utils/calculatePassiveScore.js
+++ src/modules/utils/calculatePassiveScore.js
@@ -5,8 +5,10 @@ export default function calculatePassiveScore(skill, rollData) {
 
   return getDeterministicBonus(
     10
-    + skill.deterministicBonus
-    + skill.bonuses.passive
-    + rollData.abilities[ability].check.deterministicBonus
+    + Number(skill.deterministicBonus)
+    + Number(skill.bonuses.passive)
+    + Number(rollData.abilities[ability].check.deterministicBonus)
+    // Remove the double addition of the global check bonus
+    - Number(getDeterministicBonus(rollData.bonuses.abilities.check, rollData))
   );
 }
