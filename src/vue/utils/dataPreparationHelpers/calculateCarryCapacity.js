c9f8b212c8e0f76c6974de5b03dbf1c3c5b4b08e
Phil <me@phillip.best>
2022-06-15 03:47:49 +0100

    Adds a basic calculation for maximum carry capacity.

--- src/vue/utils/dataPreparationHelpers/calculateCarryCapacity.js
+++ src/vue/utils/dataPreparationHelpers/calculateCarryCapacity.js
@@ -1,3 +1,7 @@
 export default function calculateCarryCapacity(actorData) {
-  return 200;
+  const strengthScore = actorData.data.abilities.str.value;
+  const { size } = actorData.data.traits;
+  const baseCarryCapacityMultiplier = CONFIG.A5E.carryCapacityMultiplier[size];
+
+  return strengthScore * baseCarryCapacityMultiplier * 15;
 }
