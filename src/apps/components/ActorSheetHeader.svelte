32312934588870f46dbca6184dcd04bab2b6cb77
Phil <me@phillip.best>
2022-07-25 06:55:09 +0100

    Adds a new StatusTrack component for the fatigue and strife tracks.

--- src/apps/components/ActorSheetHeader.svelte
+++ src/apps/components/ActorSheetHeader.svelte
@@ -1,10 +1,37 @@
 <script>
     import AbilityScore from "./AbilityScore.svelte";
+    import StatusTrack from "./StatusTrack.svelte";
 
     export let actor;
 </script>
 
-<header>
+<header class="sheet-header">
+    <div class="a5e-sheet-header__left">
+        <img
+            src={$actor.img}
+            alt={$actor.name}
+            title={$actor.name}
+            data-edit="img"
+            class="a5e-image a5e-image--actor-header"
+        />
+
+        <StatusTrack
+            {actor}
+            icon="fa-running"
+            tooltipText="A5E.Fatigue"
+            trackProperty="fatigue"
+            value={$actor.system.attributes.fatigue}
+        />
+
+        <StatusTrack
+            {actor}
+            icon="fa-brain"
+            tooltipText="A5E.Strife"
+            trackProperty="strife"
+            value={$actor.system.attributes.strife}
+        />
+    </div>
+
     <ul class="ability-score-list">
         {#each Object.entries($actor.system.abilities) as [abilityLabel, ability]}
             <AbilityScore {ability} {abilityLabel} {actor} />
@@ -20,5 +47,19 @@
         padding: 0;
         list-style: none;
         font-family: "Modesto Condensed", serif;
+    }
+
+    .sheet-header {
+        display: grid;
+        padding: 1rem;
+        padding-bottom: 0.75rem;
+        column-gap: 1rem;
+        grid-template-areas:
+            "left primary-details"
+            "left secondary-details"
+            "left movement"
+            "left primary-attributes";
+        grid-template-columns: max-content 1fr;
+        row-gap: 0.25rem;
     }
 </style>
