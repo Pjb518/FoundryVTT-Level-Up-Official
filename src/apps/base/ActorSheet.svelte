ddf870fead5eba3b560cdc47b69b2d43c89826ff
Phil <me@phillip.best>
2022-07-25 06:09:33 +0100

    Adds a new ActorSheetHeader component

--- src/apps/base/ActorSheet.svelte
+++ src/apps/base/ActorSheet.svelte
@@ -5,7 +5,7 @@
     import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store";
     import { getContext } from "svelte";
 
-    import AbilityScore from "../components/AbilityScore.svelte";
+    import ActorSheetHeader from "../components/ActorSheetHeader.svelte";
 
     export let { actorDocument } = getContext("external").application;
     export let elementRoot;
@@ -15,11 +15,7 @@
 
 <ApplicationShell bind:elementRoot>
     <main>
-        <ul class="ability-score-list">
-            {#each Object.entries($actor.system.abilities) as [abilityLabel, ability]}
-                <AbilityScore {ability} {abilityLabel} {actor} />
-            {/each}
-        </ul>
+        <ActorSheetHeader {actor} />
     </main>
 </ApplicationShell>
 
---
+++ src/apps/components/ActorSheetHeader.svelte
@@ -0,0 +1,24 @@
+<script>
+    import AbilityScore from "./AbilityScore.svelte";
+
+    export let actor;
+</script>
+
+<header>
+    <ul class="ability-score-list">
+        {#each Object.entries($actor.system.abilities) as [abilityLabel, ability]}
+            <AbilityScore {ability} {abilityLabel} {actor} />
+        {/each}
+    </ul>
+</header>
+
+<style>
+    .ability-score-list {
+        display: flex;
+        gap: 0.5rem;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        font-family: "Modesto Condensed", serif;
+    }
+</style>
