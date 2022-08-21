c1756be106ee6c6acb67499e809f78f73cd27ed6
Phil <me@phillip.best>
2022-08-21 07:02:22 +0100

    Adds roll type and boilerplate to all of the new roll config components.

--- src/apps/components/itemActionsConfig/rollsConfig/AttackRollConfig.svelte
+++ src/apps/components/itemActionsConfig/rollsConfig/AttackRollConfig.svelte
@@ -1,7 +1,7 @@
 <script>
-    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
-    import actionHasAttackRoll from "../../../utils/actionHasAttackRoll";
     import updateDocumentDataFromField from "../../../utils/updateDocumentDataFromField";
+
+    import RollTypeConfig from "./RollTypeConfig.svelte";
 
     export let action;
     export let actionId;
@@ -10,31 +10,7 @@
     export let rollId;
 </script>
 
-<div>
-    <label for={`${actionId}-${rollId}-roll-type`}>Roll Type</label>
-
-    <select
-        id={`${actionId}-${rollId}-roll-type`}
-        class="u-w-fit"
-        name={`${actionId}-${rollId}-roll-type`}
-        on:change={({ target }) =>
-            updateDocumentDataFromField(
-                $item,
-                `system.actions.${actionId}.rolls.${rollId}.type`,
-                target.value
-            )}
-    >
-        {#each Object.entries(CONFIG.A5E.rollTypes) as [key, name] (key)}
-            <option
-                value={key}
-                selected={roll.type === key}
-                disabled={key === "attack" && actionHasAttackRoll(action)}
-            >
-                {localize(name)}
-            </option>
-        {/each}
-    </select>
-</div>
+<RollTypeConfig {action} {actionId} {item} {roll} {rollId} />
 
 <div>
     <label for={`${actionId}-${rollId}-proficient`}>
