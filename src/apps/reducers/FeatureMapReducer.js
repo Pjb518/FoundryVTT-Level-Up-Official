12b772c1c5792443278ad2f14f0e64ffeece6914
Phil <me@phillip.best>
2022-08-24 22:40:28 +0100

    Fixes a mistake in the FeatureMapReducer that was causing the features tab to display spells instead of features.

--- src/apps/reducers/FeatureMapReducer.js
+++ src/apps/reducers/FeatureMapReducer.js
@@ -2,7 +2,7 @@ import { DynMapReducer } from '@typhonjs-fvtt/runtime/svelte/store';
 
 export default class FeatureMapReducer extends DynMapReducer {
   initialize() {
-    this.filters.add((item) => item.type === 'spell');
+    this.filters.add((item) => item.type === 'feature');
     this.sort.set((a, b) => a.sort - b.sort);
   }
 }
