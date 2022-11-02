1cbe35b5a20374c0b1969573890ce1d6ef77fd62
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-11-02 06:16:43 -0700

    Adds resources to core actor page

--- src/apps/components/ActorCorePage.svelte
+++ src/apps/components/ActorCorePage.svelte
@@ -1,62 +1,89 @@
 <script>
-    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
-    import { getContext } from "svelte";
+	import { localize } from '@typhonjs-fvtt/runtime/svelte/helper';
+	import { getContext } from 'svelte';
 
-    import ItemWrapper from "./item/ItemWrapper.svelte";
+	import ResourceTrack from './actor/ResourceTrack.svelte';
+	import ItemWrapper from './item/ItemWrapper.svelte';
 
-    const actor = getContext("actor");
-    const { favorites } = actor;
+	export let resources;
+
+	const actor = getContext('actor');
+	const { favorites } = actor;
+
+	$: resources = $actor.system.resources;
 </script>
 
 <header class="section-header">
-    <i class="fas fa-star heading-icon" />
+	<i class="fas fa-star heading-icon" />
 
-    <h3>
-        {localize("A5E.FavoriteItems")}
-    </h3>
+	<h3>
+		{localize('A5E.Resources')}
+	</h3>
+</header>
+
+<ul class="resources-container">
+	{#each Object.entries(resources) as [source, resource]}
+		<ResourceTrack {resource} {source} />
+	{/each}
+</ul>
+
+<header class="section-header">
+	<i class="fas fa-star heading-icon" />
+
+	<h3>
+		{localize('A5E.FavoriteItems')}
+	</h3>
 </header>
 
 <ul class="items-container">
-    {#each [...$favorites] as item}
-        <ItemWrapper>
-            <img class="item-image" src={item.img} alt={item.name} />
-            {item.name}
-        </ItemWrapper>
-    {/each}
+	{#each [...$favorites] as item}
+		<ItemWrapper>
+			<img class="item-image" src={item.img} alt={item.name} />
+			{item.name}
+		</ItemWrapper>
+	{/each}
 </ul>
 
 <style lang="scss">
-    .heading-icon {
-        font-size: 0.833rem;
-    }
+	.heading-icon {
+		font-size: 0.833rem;
+	}
 
-    .item-image {
-        height: 1.75rem;
-        width: 1.75rem;
-        border-radius: 3px;
-    }
+	.item-image {
+		height: 1.75rem;
+		width: 1.75rem;
+		border-radius: 3px;
+	}
 
-    .items-container {
-        display: flex;
-        flex-direction: column;
-        gap: 0.25rem;
-        padding: 0;
-        padding-right: 0.375rem;
-        margin: 0;
-        margin-right: -0.375rem;
-        list-style: none;
-        overflow-y: auto;
-    }
+	.items-container {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		padding: 0;
+		padding-right: 0.375rem;
+		margin: 0;
+		margin-right: -0.375rem;
+		list-style: none;
+		overflow-y: auto;
+	}
 
-    .section-header {
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-        width: 100%;
-        padding: 0.25rem;
-        padding-top: 0;
-        border-bottom: 1px solid #ccc;
-        font-size: 1rem;
-        font-family: "Modesto Condensed", serif;
-    }
+	.section-header {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+		width: 100%;
+		padding: 0.25rem;
+		padding-top: 0;
+		border-bottom: 1px solid #ccc;
+		font-size: 1rem;
+		font-family: 'Modesto Condensed', serif;
+	}
+
+	.resources-container {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(6rem, 1fr));
+		gap: 0.5rem;
+		width: 100%;
+		align-items: center;
+	}
 </style>
---
+++ src/apps/components/actor/ResourceTrack.svelte
@@ -0,0 +1,89 @@
+<script>
+	import { getContext } from 'svelte';
+
+	import updateDocumentDataFromField from '../../utils/updateDocumentDataFromField';
+
+	export let resource;
+	export let source;
+
+	const actor = getContext('actor');
+</script>
+
+<div class="resource resource-{source}">
+	<header class="resource-header">
+		<input
+			type="text"
+			name="system.resources.{source}.label"
+			value={resource.label}
+			class="a5e-input a5e-input--slim resource-label"
+			placeholder={source.capitalize()}
+			on:change={({ target }) =>
+				updateDocumentDataFromField($actor, target.name, target.value)}
+		/>
+	</header>
+
+	<div class="resource-value-container">
+		<input
+			type="number"
+			name="system.resources.{source}.value"
+			value={resource.value}
+			class="a5e-input a5e-input--inline-item a5e-input--small resource-number-input"
+			placeholder="0"
+			on:change={({ target }) =>
+				updateDocumentDataFromField($actor, target.name, target.value)}
+		/>
+
+		<span class="resource-seperator"> / </span>
+
+		<input
+			type="number"
+			name="system.resources.{source}.max"
+			value={resource.max}
+			class="a5e-input a5e-input--inline-item a5e-input--small resource-number-input"
+			placeholder="0"
+			on:change={({ target }) =>
+				updateDocumentDataFromField($actor, target.name, target.value)}
+		/>
+	</div>
+
+	<div class="resource-setting">
+		<i class="fas fa-gear" />
+	</div>
+</div>
+
+<style lang="scss">
+	.resource {
+		position: relative;
+		padding: 0.125rem;
+		border: 1px solid #ccc;
+		border-radius: 2px;
+		height: 4.5rem;
+	}
+
+	.resource-label {
+		padding: 0;
+		border: 0;
+		background: transparent;
+		color: inherit;
+		text-align: center;
+		text-overflow: ellipsis;
+	}
+
+	.resource-value-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding-bottom: 0.275rem;
+	}
+
+	.resource-number-input {
+		width: calc(3ch + 1rem);
+	}
+
+	.resource-setting {
+		position: absolute;
+		bottom: 0.125rem;
+		right: 0.125rem;
+		cursor: pointer;
+	}
+</style>
