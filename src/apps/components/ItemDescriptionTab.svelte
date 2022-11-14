f1d7182687e6845d7c06d5edcb030f06f81ff591
Phil <me@phillip.best>
2022-11-14 11:58:40 +0000

    Adds text editor and description to Item sheets.

--- src/apps/components/ItemDescriptionTab.svelte
+++ src/apps/components/ItemDescriptionTab.svelte
@@ -1,4 +1,36 @@
 <script>
+    import { getContext } from "svelte";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+    import { TJSProseMirror } from "@typhonjs-fvtt/svelte-standard/component";
+
+    const item = getContext("item");
+
+    function updateDescription(event) {
+        const { content } = event.detail;
+
+        $item.update({
+            "system.description": content === "<p></p>" ? "" : content,
+        });
+    }
 </script>
 
-<div>DESCRIPTION GOES HERE!!</div>
+<div class="editor">
+    <!-- svelte-ignore missing-declaration (TextEditor) -->
+    <TJSProseMirror
+        content={$item.system.description || localize("A5E.NoDescription")}
+        enrichedContent={TextEditor.enrichHTML($item.system.description, {
+            async: false,
+        })}
+        on:editor:save={(event) => updateDescription(event)}
+        on:editor:start={() => console.log("! event - editor:start")}
+    />
+</div>
+
+<style lang="scss">
+    .editor {
+        height: 100%;
+
+        // Nudges the edit icon down 1px. Removing this hides the top border for the button.
+        --tjs-editor-edit-top: 1px;
+    }
+</style>
