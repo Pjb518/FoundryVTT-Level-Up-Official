a964c5ed30cb523da4811ea196459b9d5fcf0c79
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-11-18 12:57:24 -0800

    Adds derived headings for feature reducer and updates feature page to use reducer

--- src/apps/components/ActorFeaturesPage.svelte
+++ src/apps/components/ActorFeaturesPage.svelte
@@ -1,56 +1,63 @@
 <script>
-    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
-    import { getContext } from "svelte";
+	import { getContext } from 'svelte';
+	import { TJSInput } from '@typhonjs-fvtt/svelte-standard/component';
+	import { createFilterQuery } from '@typhonjs-fvtt/svelte-standard/store';
 
-    import ItemWrapper from "./item/ItemWrapper.svelte";
+	import addReducerFilter from '../utils/addReducerFilter';
 
-    const actor = getContext("actor");
-    const { features } = actor;
+	import ItemCategory from './item/ItemCategory.svelte';
+
+	const actor = getContext('actor');
+	const { features } = actor;
+
+	const filterSearch = createFilterQuery('name');
+	const input = {
+		store: filterSearch,
+		placeholder: 'Search...',
+		type: 'search',
+	};
+
+	addReducerFilter(features, { id: 'searchFilter', filter: filterSearch });
 </script>
 
-<header class="section-header">
-    <h3>
-        {localize("A5E.TabFeatures")}
-    </h3>
-</header>
+<div class="features-page">
+	<header class="search-container">
+		<TJSInput {input} />
 
-<ul class="items-container">
-    {#each [...$features] as item}
-        <ItemWrapper>
-            <img class="item-image" src={item.img} alt={item.name} />
-            {item.name}
-        </ItemWrapper>
-    {/each}
-</ul>
+		<i class="fas fa-sort" />
+		<i class="fas fa-filter" />
+	</header>
+
+	<section class="features-main-container">
+		{#each Object.entries($features._types) as [label, items]}
+			{#if items.length}
+				<ItemCategory {label} {items} type="featureTypes" />
+			{/if}
+		{/each}
+	</section>
+
+	<footer class="features-footer" />
+</div>
 
 <style lang="scss">
-    .item-image {
-        height: 1.75rem;
-        width: 1.75rem;
-        border-radius: 3px;
-    }
+	.features-page {
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		gap: 0.5rem;
+		overflow: hidden;
+	}
+	.search-container {
+		display: flex;
+		align-items: center;
+		gap: 0.25rem;
+	}
 
-    .items-container {
-        display: flex;
-        flex-direction: column;
-        gap: 0.25rem;
-        padding: 0;
-        padding-right: 0.375rem;
-        margin: 0;
-        margin-right: -0.375rem;
-        list-style: none;
-        overflow-y: auto;
-    }
-
-    .section-header {
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-        width: 100%;
-        padding: 0.25rem;
-        padding-top: 0;
-        border-bottom: 1px solid #ccc;
-        font-size: 1rem;
-        font-family: "Modesto Condensed", serif;
-    }
+	.features-main-container {
+		display: flex;
+		flex-grow: 1;
+		flex-direction: column;
+		gap: 0.25rem;
+		overflow-y: auto;
+	}
 </style>
