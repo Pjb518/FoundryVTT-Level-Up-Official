134ed317338258aa5af858821d6f3b88e9b7be5a
Charles Miller <shadowmil@gmail.com>
2022-11-20 00:18:44 -0500

    Adds the TraitSelectDialog class and svelte
    This class allows you to generate customized trait selection. It requires you provide a list of of traitGroups (For example, simple, martial and rare weapons), along with the label for that group.
    
    If a traitGroup includes no traits, it will assume you are attempting to create a "custom" grouping, which allows the user to enter a comma seperated list of elements.
    
    It also has a number of option parameters:
    
    * options.selected - Defines the list of preselected elements.
    * options.disabled - Defines a list of traits which selection status is disabled.
    * options.selectionLimit - Requires the user to selected elements + an additional amounts from being added. A negative amount would require the user to remove selections.
    
    The class resolves it's `promise` property when the user has submited their choices

---
+++ src/apps/dialogs/TraitSelect/TraitSelectDialog.js
@@ -0,0 +1,83 @@
+// eslint-disable-next-line import/no-unresolved
+import { TJSDialog } from '@typhonjs-fvtt/runtime/svelte/application';
+
+import TraitSelectDialogComponent from './TraitSelectDialog.svelte';
+
+/**
+ * @typedef {{string: string}} TraitGroup A collection of identifying information about a grouping
+ * of traits.
+ * @property {string} TraitGroup.label The i18n string for the label of the group.
+ * @property {{string: string}?} TraitGroup.traits An object map of the system name to i18n string,
+ * omittion of this element indicates this grouping is a custom data entry for the user.
+ */
+
+/**
+ * Provides a dialog selecting from a generic use of traits.
+ */
+
+export default class TraitSelectDialog extends TJSDialog {
+  /**
+   * @param {string} title The title of the window, this string will be localized.
+   * @param {[TraitGroup]} traitGroups An array of
+   * @param {Object} options Options to provide
+   * @param {[string]?} [options.selected] An array containing preselected traits
+   * @param {[string]?} [options.disabled] When a trait name is included in the disabled array
+   * users will not be able to change the selection station.
+   * @param {Number?} [options.selectionLimit] When provided, the dialog will prevent users from
+   * selecting more than `options.selected` plus `options.selectionLimit`. If you wish to prevent
+   * users from disable currently selected elements, include them in the `options.disabled`.
+   */
+  constructor(title, traitGroups, options) {
+    if (!Array.isArray(traitGroups)) throw Error('TraitSelectDialog: traitGroups must be an array!');
+    if (typeof title !== 'string') throw Error('TraitSelectDialog: title must be a string!');
+
+    const props = {
+      traitGroups,
+      selected: [],
+      disabled: []
+    };
+
+    if (options) {
+      if (Array.isArray(options.selected)) props.selected = options.selected;
+      if (Array.isArray(options.disabled)) props.disabled = options.disabled;
+      if (typeof options.selectionLimit === 'number') props.selectionLimit = options.selectionLimit;
+    }
+
+    super({
+      title: game.i18n.localize(title),
+      content: {
+        class: TraitSelectDialogComponent,
+        props
+      }
+    }, { classes: ['a5e-sheet'], width: 480 });
+
+    this.promise = new Promise((resolve) => {
+      this.resolve = resolve;
+    });
+  }
+
+  /** @inheritdoc */
+  close(options) {
+    this.#resolvePromise(null);
+    return super.close(options);
+  }
+
+  /**
+   * Generates the resulted select as Resolves dialog promise's
+   *
+   * @param {object} results
+   * @returns
+   */
+  submit(results) {
+    console.log(results);
+    this.#resolvePromise(results);
+    return super.close();
+  }
+
+  #resolvePromise(data) {
+    if (this.resolve) {
+      console.log(data);
+      this.resolve(data);
+    }
+  }
+}
---
+++ src/apps/dialogs/TraitSelect/TraitSelectDialog.svelte
@@ -0,0 +1,229 @@
+<svelte:options accessors={true} />
+
+<script>
+    import { getContext } from "svelte";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+
+    export let traitGroups;
+    export let selected;
+    export let disabled;
+    export let selectionLimit;
+
+    // We generate a list of disabled custom traits in order to remove them from the custom
+    // text box. This ensures these traits can not be edited. However we must still check that
+    // they won't be added either.
+    let _disabledCustom = disabled.filter((trait) => isCustomTrait(trait));
+    let _disabledSelected = selected.filter((trait) =>
+        _disabledCustom.includes(trait)
+    );
+
+    // We add selected length to the selectionLimit, since selectionLimit limits how many more
+    // elements we can add to selected. This simplifies the logic of deselecting an element and
+    // adding a different element. Since now all we have to test for is
+    // that selected.length == selectionLimit.
+    if (selectionLimit) {
+        selectionLimit += selected.length;
+    }
+
+    const application = getContext("external").application;
+
+    function isCustomTrait(trait) {
+        for (let group of traitGroups) {
+            if (!group.traits) continue;
+
+            for (let definedTrait in group.traits) {
+                if (definedTrait === trait) return false;
+            }
+        }
+
+        return true;
+    }
+
+    function isDisabled(trait) {
+        // Disable all nonchecked, check marks if we've reached selectionLimit
+        if (
+            selectionLimit &&
+            selected.length >= selectionLimit &&
+            !selected.includes(trait)
+        ) {
+            return true;
+        }
+
+        return disabled.includes(trait);
+    }
+
+    function setTrait(trait, status) {
+        if (status) {
+            selected = [...selected, trait];
+        } else {
+            selected.findSplice((selected) => selected === trait);
+        }
+
+        // Prevent duplicate values
+        selected = [...new Set(selected)];
+        updateDisabledStatus();
+    }
+
+    function getCustom() {
+        const custom = selected.filter(
+            (trait) => isCustomTrait(trait) && !_disabledCustom.includes(trait)
+        );
+
+        return custom.join(";");
+    }
+
+    function setCustom(element) {
+        // Split, trim, remove empty & disabled
+        const customList = element.value
+            .split(";")
+            .map((i) => i.trim())
+            .filter((i) => i && !_disabledCustom.includes(i));
+
+        selected = selected
+            // Remove all custom from selected
+            .filter((trait) => !isCustomTrait(trait))
+            // Add back disabled selected (our input box doesn't control these)
+            .concat(_disabledSelected);
+
+        // Test to ensure that the custom added selection isn't going to exceed the limit.
+        if (selectionLimit) {
+            // Calculate how much we're over the limit by.
+            const overLimit =
+                selected.length + customList.length - selectionLimit;
+
+            // Warn the user and truncate the customList
+            if (overLimit > 0) {
+                ui.notifications.warn(
+                    "You have added too many custom traits, we will remove the extra for you"
+                );
+                customList.splice(-overLimit);
+            }
+        }
+
+        // Add the custom traits to the selected and update
+        selected = [...selected, ...customList];
+        element.value = getCustom();
+        updateDisabledStatus();
+    }
+
+    function submitForm() {
+        application.submit(selected);
+    }
+
+    // Updates the disabled/checked values. Since any checkbox can modifiy the disabled state of any
+    // Due to the selectionLimit, we must maintain the list of _disabled elemets seperately so that
+    // Svelte knows to update those elements.
+    let _disabled = {};
+    let _disableSubmit = false;
+    function updateDisabledStatus() {
+        for (let group of traitGroups) {
+            for (let trait in group.traits ?? {}) {
+                _disabled[trait] = isDisabled(trait);
+            }
+        }
+
+        if (selectionLimit && selected.length !== selectionLimit) {
+            _disableSubmit = true;
+        } else {
+            _disableSubmit = false;
+        }
+    }
+    updateDisabledStatus();
+</script>
+
+<form>
+    {#each traitGroups as group}
+        {#if group.traits}
+            <section class="trait-list">
+                <h3>{localize(group.label)}</h3>
+                <div class="container">
+                    {#each Object.entries(group.traits) as [trait, label] (trait)}
+                        <input
+                            type="checkbox"
+                            id="trait-{trait}"
+                            checked={selected.includes(trait)}
+                            disabled={_disabled[trait]}
+                            value={trait}
+                            on:change={({ target }) => {
+                                setTrait(trait, target.checked);
+                            }}
+                        />
+
+                        <label for={`trait-${trait}`}>
+                            {localize(label)}
+                        </label>
+                    {/each}
+                </div>
+            </section>
+        {:else}
+            <h3>{localize(group.label)}</h3>
+            <input
+                class="a5e-input"
+                type="text"
+                name="custom-traits"
+                value={getCustom()}
+                on:change={({ target }) => setCustom(target)}
+            />
+
+            <p class="hint">{localize("A5E.HintSeparateBySemiColon")}</p>
+        {/if}
+    {/each}
+
+    <div class="button-container">
+        <button disabled={_disableSubmit} on:click|preventDefault={submitForm}
+            >Submit</button
+        >
+    </div>
+</form>
+
+<style lang="scss">
+    form {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        padding: 0.75rem;
+        gap: 1rem;
+        overflow: auto;
+    }
+
+    h3 {
+        font-size: 1.5rem;
+        margin-bottom: 0.5rem;
+        border-bottom: 1px solid gray;
+    }
+
+    .trait-list {
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 0.2rem;
+        }
+
+        input {
+            display: none;
+
+            &:checked + label {
+                background: #2b6537;
+                border-color: darken($color: #2b6537, $amount: 5);
+                color: #f6f2eb;
+            }
+
+            &:disabled + label {
+                color: #999;
+            }
+        }
+
+        label {
+            border-radius: 3px;
+            border: 1px solid #bbb;
+            font-size: 1rem;
+            padding: 0.25rem 0.5rem;
+            cursor: pointer;
+            transition: all 0.15s ease-in-out;
+        }
+    }
+
+    .button-container {
+        display: flex;
+    }
+</style>
