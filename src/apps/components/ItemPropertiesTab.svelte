39308767ee88ead2be41a849d3e1d3094788f694
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-12-22 04:43:41 -0800

    Adds vertical scroll to item properties tab

--- src/apps/components/ItemPropertiesTab.svelte
+++ src/apps/components/ItemPropertiesTab.svelte
@@ -4,20 +4,30 @@
 
   import FeatureConfiguration from "./itemPropertiesConfig/FeatureConfiguration.svelte";
   import ManeuverConfiguration from "./itemPropertiesConfig/ManeuverConfiguration.svelte";
+  import MaterialConfiguration from "./itemPropertiesConfig/MaterialConfiguration.svelte";
   import ObjectConfiguration from "./itemPropertiesConfig/ObjectConfiguration.svelte";
+  import SpellConfiguration from "./itemPropertiesConfig/SpellConfiguration.svelte";
+  import WeaponConfiguration from "./itemPropertiesConfig/WeaponConfiguration.svelte";
 
   const item = getContext("item");
 </script>
 
-<div class="u-flex u-flex-col u-gap-xl">
+<div class="u-flex u-flex-col u-gap-xl properties__container">
   {#if $item.type === "feature"}
     <FeatureConfiguration />
   {:else if $item.type === "maneuver"}
     <ManeuverConfiguration />
   {:else if $item.type === "object"}
     <ObjectConfiguration />
+    <MaterialConfiguration />
+    {#if $item.system.objectType === "weapon"}
+      <WeaponConfiguration />
+    {/if}
   {/if}
 </div>
 
 <style lang="scss">
+  .properties__container {
+    overflow-y: auto;
+  }
 </style>
