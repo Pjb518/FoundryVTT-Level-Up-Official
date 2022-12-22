dac865d494314b33d6ec96e89fe3032efe95ec72
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-12-21 23:49:36 -0800

    Adds properties page to item sheet

---
+++ src/apps/components/ItemPropertiesTab.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { getContext } from "svelte";
+  import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+
+  const item = getContext("item");
+</script>
+
+<style lang="scss">
+</style>
--- src/apps/sheets/ItemSheet.svelte
+++ src/apps/sheets/ItemSheet.svelte
@@ -1,61 +1,62 @@
 <svelte:options accessors={true} />
 
 <script>
-    import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
-    import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store";
-    import { getContext, setContext } from "svelte";
+  import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
+  import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store";
+  import { getContext, setContext } from "svelte";
 
-    import ItemActionsTab from "../components/ItemActionsTab.svelte";
-    import ItemDescriptionTab from "../components/ItemDescriptionTab.svelte";
-    import ItemSheetHeader from "../components/itemSheetHeader/ItemSheetHeader.svelte";
-    import NavigationBar from "../components/navigation/NavigationBar.svelte";
+  import ItemActionsTab from "../components/ItemActionsTab.svelte";
+  import ItemDescriptionTab from "../components/ItemDescriptionTab.svelte";
+  import ItemPropertiesTab from "../components/ItemPropertiesTab.svelte";
+  import ItemSheetHeader from "../components/itemSheetHeader/ItemSheetHeader.svelte";
+  import NavigationBar from "../components/navigation/NavigationBar.svelte";
 
-    export let { itemDocument } = getContext("external").application;
-    export let elementRoot;
+  export let { itemDocument } = getContext("external").application;
+  export let elementRoot;
 
-    function updateCurrentTab(event) {
-        currentTab = tabs[event.detail];
-    }
+  function updateCurrentTab(event) {
+    currentTab = tabs[event.detail];
+  }
 
-    const tabs = [
-        {
-            name: "description",
-            label: "Description",
-            component: ItemDescriptionTab,
-        },
-        {
-            name: "properties",
-            label: "Properties",
-            component: ItemActionsTab,
-        },
-        {
-            name: "actions",
-            label: "Actions",
-            component: ItemActionsTab,
-        },
-    ];
+  const tabs = [
+    {
+      name: "description",
+      label: "Description",
+      component: ItemDescriptionTab,
+    },
+    {
+      name: "properties",
+      label: "Properties",
+      component: ItemPropertiesTab,
+    },
+    {
+      name: "actions",
+      label: "Actions",
+      component: ItemActionsTab,
+    },
+  ];
 
-    $: currentTab = tabs[0];
+  $: currentTab = tabs[0];
 
-    setContext("item", new TJSDocument(itemDocument));
+  setContext("item", new TJSDocument(itemDocument));
 </script>
 
 <ApplicationShell bind:elementRoot>
-    <main>
-        <ItemSheetHeader />
+  <main>
+    <ItemSheetHeader />
 
-        <NavigationBar {currentTab} {tabs} on:tab-change={updateCurrentTab} />
+    <NavigationBar {currentTab} {tabs} on:tab-change={updateCurrentTab} />
 
-        <svelte:component this={currentTab.component} />
-    </main>
+    <svelte:component this={currentTab.component} />
+  </main>
 </ApplicationShell>
 
 <style lang="scss">
-    main {
-        display: flex;
-        flex-direction: column;
-        height: 100%;
-        padding: 0.75rem;
-        gap: 0.5rem;
-    }
+  main {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    padding: 0.75rem;
+    gap: 0.5rem;
+  }
 </style>
