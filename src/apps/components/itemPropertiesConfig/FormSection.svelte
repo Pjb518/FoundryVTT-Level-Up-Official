eced48f38702993a651cb88d8f17c0d8b19fc624
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-12-22 01:44:24 -0800

    Adds the FormSection svelte component for building out item properties section

---
+++ src/apps/components/itemPropertiesConfig/FormSection.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+
+  export let heading = null;
+  export let hint = null;
+  export let inline = false;
+</script>
+
+<div
+  class="a5e-box u-flex u-flex-wrap u-gap-md u-p-md"
+  class:u-align-center={inline}
+>
+  {#if heading}
+    <h3 class="u-flex-shrink-0 u-text-bold u-text-sm" class:u-w-30={inline}>
+      {localize(heading)}
+    </h3>
+
+    <slot />
+
+    {#if hint}
+      <small>{localize(hint)}</small>
+    {/if}
+  {/if}
+</div>
