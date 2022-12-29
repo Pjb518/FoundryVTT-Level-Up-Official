994bf07f299fff31f38cf449d9034195f7c2b67c
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-12-29 04:52:44 -0800

    Adds migration script for numerical weight

---
+++ src/modules/migrations/v100Migrations/helpers/migrateItemWeight.js
@@ -0,0 +1,11 @@
+export default function migrateItemWeight(itemData, updateData) {
+  if (!itemData.system.weight?.length) return;
+
+  // Extract numbers from string
+  const weightString = itemData.system.weight;
+  const matches = weightString.match(/\d+/);
+
+  const weight = matches ? Number(matches[0]) : 0;
+
+  updateData['system.weight'] = weight;
+}
