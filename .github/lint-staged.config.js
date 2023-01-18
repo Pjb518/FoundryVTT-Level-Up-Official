5cf11e5fcb73009b9f3763fa113c082da34d83c5
Dave Matney <matneyx@gmail.com>
2023-01-17 18:25:39 -0700

    Fixed ids, since I deleted a classFeature

--- .github/lint-staged.config.js
+++ .github/lint-staged.config.js
@@ -2,6 +2,7 @@ import micromatch from 'micromatch'
 
 export default {
   "*.{js,mjs,cjs,jsx,ts,tsx,vue}": 'eslint --fix',
+  "packs/ids.json": "jsonsort",
   "packs/classFeatures/*.json": files => {
     const matched = micromatch.not(files, '_*Schema.json')
     return [
