f750efb60dc8c18db4aca4bbcb81a3876b78063f
JoshLaunchcode <holo.sf0@gmail.com>
2023-02-05 01:35:21 -0600

    adds changes to entity.js that didn't come through with previous pushj

--- src/modules/actor/entity.js
+++ src/modules/actor/entity.js
@@ -28,6 +28,7 @@ import WeaponProficienciesConfigDialog from '../../vue/WeaponProficienciesConfig
 
 import calculateManeuverDC from '../utils/calculateManeuverDC';
 import calculatePassiveScore from '../utils/calculatePassiveScore';
+import calculateSpellcastingMod from '../utils/calculateSpellcastingMod';
 import calculateSpellDC from '../utils/calculateSpellDC';
 import getBaseAbilityMod from '../utils/getBaseAbilityMod';
 import getDialogData from '../dice/getDialogData';
@@ -261,7 +262,7 @@ export default class Actor5e extends Actor {
 
     data.level = this.system.details.level;
 
-    data.spell = abilities[attributes.spellcasting]
+    data.spellMod = calculateSpellcastingMod(this.system);
 
     data.spellDC = calculateSpellDC(this.system);
     data.maneuverDC = calculateManeuverDC(this.system);
