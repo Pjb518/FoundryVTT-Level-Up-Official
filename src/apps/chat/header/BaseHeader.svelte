fa96bf37660434a8d1706cdb20e3ad120dface3a
Phil <me@phillip.best>
2023-02-11 01:40:15 +0000

    Adds a new shared BaseHeader component for check chat card headers.

---
+++ src/apps/chat/header/BaseHeader.svelte
@@ -0,0 +1,51 @@
+<script>
+    export let altText;
+    export let img;
+    export let subtitle;
+    export let title;
+</script>
+
+<header class="card-header">
+    <img class="actor-image" src={img} alt={altText} />
+
+    <div>
+        <h2 class="card-title">{title}</h2>
+
+        {#if subtitle}
+            <h3 class="card-subtitle">
+                {subtitle}
+            </h3>
+        {/if}
+    </div>
+</header>
+
+<style>
+    .actor-image {
+        border: 0;
+        width: 2.5rem;
+        height: 2.5rem;
+        border-radius: 4px;
+    }
+
+    .card-header {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        padding: 0.25rem;
+        padding-bottom: 0;
+    }
+
+    .card-title {
+        margin-bottom: 0;
+        font-size: 1rem;
+        font-weight: bold;
+        border-bottom: 0;
+    }
+
+    .card-subtitle {
+        margin-bottom: 0;
+        font-size: 0.833rem;
+        border-bottom: 0;
+        color: #7e7960;
+    }
+</style>
--- src/apps/chat/header/SavingThrowHeader.svelte
+++ src/apps/chat/header/SavingThrowHeader.svelte
@@ -2,6 +2,8 @@
     import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store";
     import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
 
+    import BaseHeader from "./BaseHeader.svelte";
+
     export let message;
 
     const actor = new TJSDocument(fromUuidSync($message.flags?.a5e?.actorId));
@@ -9,51 +11,8 @@
     const { abilities } = CONFIG.A5E;
 </script>
 
-<header class="card-header">
-    <img
-        class="actor-image"
-        src={$actor.img ?? $actor.actor.img}
-        alt={$actor.name}
-    />
-
-    <div>
-        <h2 class="card-title">
-            {`${localize(abilities[abilityKey])} Saving Throw`}
-        </h2>
-
-        <!-- <h3 class="card-subtitle">
-            {localize(abilities[abilityKey] ?? "No Ability Selected")}
-        </h3> -->
-    </div>
-</header>
-
-<style>
-    .actor-image {
-        border: 0;
-        width: 2.5rem;
-        height: 2.5rem;
-        border-radius: 4px;
-    }
-
-    .card-header {
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-        padding: 0.25rem;
-        padding-bottom: 0;
-    }
-
-    .card-title {
-        margin-bottom: 0;
-        font-size: 1rem;
-        font-weight: bold;
-        border-bottom: 0;
-    }
-
-    .card-subtitle {
-        margin-bottom: 0;
-        font-size: 0.833rem;
-        border-bottom: 0;
-        color: #7e7960;
-    }
-</style>
+<BaseHeader
+    img={$actor.img ?? $actor.actor.img}
+    alt={$actor.name}
+    title={`${localize(abilities[abilityKey])} Save`}
+/>
