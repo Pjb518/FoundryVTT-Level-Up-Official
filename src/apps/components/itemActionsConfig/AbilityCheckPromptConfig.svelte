42d56a9016416b3d5d92e7976b694c9f63953499
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-02-13 01:44:03 -0800

    Localizes all action prompt configs

--- src/apps/components/itemActionsConfig/AbilityCheckPromptConfig.svelte
+++ src/apps/components/itemActionsConfig/AbilityCheckPromptConfig.svelte
@@ -1,151 +1,156 @@
 <script>
-  import { getContext } from "svelte";
-  import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+    import { getContext } from "svelte";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
 
-  import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
+    import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
 
-  const item = getContext("item");
-  const actionId = getContext("actionId");
+    const item = getContext("item");
+    const actionId = getContext("actionId");
 
-  const { abilities } = CONFIG.A5E;
+    const { abilities } = CONFIG.A5E;
 
-  export let prompt;
-  export let promptId;
+    export let prompt;
+    export let promptId;
 </script>
 
 <section class="action-config__wrapper">
-  <div class="a5e-field-group a5e-field-group--label">
-    <label for={`${actionId}-${promptId}-label`}>Label</label>
-
-    <input
-      id={`${actionId}-${promptId}-label`}
-      name={`${actionId}-${promptId}-label`}
-      type="text"
-      value={prompt.label ?? ""}
-      on:change={({ target }) =>
-        updateDocumentDataFromField(
-          $item,
-          `system.actions.${actionId}.prompts.${promptId}.label`,
-          target.value
-        )}
-    />
-  </div>
-
-  <div class="a5e-field-group">
-    <h3 class="a5e-field-group__heading">
-      {localize("A5E.ItemAbilityCheckType")}
-    </h3>
-
-    <div class="option-list">
-      <input
-        class="option-input"
-        type="radio"
-        name={`${actionId}-${promptId}-ability`}
-        id={`${actionId}-${promptId}-ability-none`}
-        value=""
-        checked={(prompt.ability ?? true) || prompt.ability === ""}
-        on:change={() =>
-          updateDocumentDataFromField(
-            $item,
-            `system.actions.${actionId}.prompts.${promptId}`,
-            { "-=ability": null }
-          )}
-      />
-
-      <label class="option-label" for={`${actionId}-${promptId}-ability-none`}>
-        {localize("A5E.None")}
-      </label>
-
-      {#each Object.entries(abilities) as [ability, label]}
-        <input
-          class="option-input"
-          type="radio"
-          name={`${actionId}-${promptId}-ability`}
-          id={`${actionId}-${promptId}-ability-${ability}`}
-          value={ability}
-          checked={prompt.ability === ability}
-          on:change={({ target }) =>
-            updateDocumentDataFromField(
-              $item,
-              `system.actions.${actionId}.prompts.${promptId}.ability`,
-              target.value
-            )}
-        />
-
-        <label
-          class="option-label"
-          for={`${actionId}-${promptId}-ability-${ability}`}
-        >
-          {localize(label)}
+    <div class="a5e-field-group a5e-field-group--label">
+        <label for={`${actionId}-${promptId}-label`}>
+            {localize("A5E.Label")}
         </label>
-      {/each}
+
+        <input
+            id={`${actionId}-${promptId}-label`}
+            name={`${actionId}-${promptId}-label`}
+            type="text"
+            value={prompt.label ?? ""}
+            on:change={({ target }) =>
+                updateDocumentDataFromField(
+                    $item,
+                    `system.actions.${actionId}.prompts.${promptId}.label`,
+                    target.value
+                )}
+        />
     </div>
-  </div>
 
-  <div class="a5e-field-group a5e-field-group--formula">
-    <label for={`${actionId}-${promptId}-dc`}>
-      {localize("A5E.ItemAbilityCheckDC")}
-    </label>
+    <div class="a5e-field-group">
+        <h3 class="a5e-field-group__heading">
+            {localize("A5E.ItemAbilityCheckType")}
+        </h3>
 
-    <input
-      id={`${actionId}-${promptId}-dc`}
-      name={`${actionId}-${promptId}-dc`}
-      type="text"
-      value={prompt.abilityDC ?? ""}
-      on:change={({ target }) =>
-        updateDocumentDataFromField(
-          $item,
-          `system.actions.${actionId}.prompts.${promptId}.abilityDC`,
-          target.value
-        )}
-    />
-  </div>
+        <div class="option-list">
+            <input
+                class="option-input"
+                type="radio"
+                name={`${actionId}-${promptId}-ability`}
+                id={`${actionId}-${promptId}-ability-none`}
+                value=""
+                checked={(prompt.ability ?? true) || prompt.ability === ""}
+                on:change={() =>
+                    updateDocumentDataFromField(
+                        $item,
+                        `system.actions.${actionId}.prompts.${promptId}`,
+                        { "-=ability": null }
+                    )}
+            />
 
-  <div class="a5e-field-group ">
-    <label for={`${actionId}-${promptId}-save-effect`}>
-      {localize("A5E.ItemEffectOnCheck")}
-    </label>
+            <label
+                class="option-label"
+                for={`${actionId}-${promptId}-ability-none`}
+            >
+                {localize("A5E.None")}
+            </label>
 
-    <input
-      id={`${actionId}-${promptId}-save-effect`}
-      name={`${actionId}-${promptId}-save-effect`}
-      type="text"
-      value={prompt.onSave ?? ""}
-      on:change={({ target }) =>
-        updateDocumentDataFromField(
-          $item,
-          `system.actions.${actionId}.prompts.${promptId}.onSave`,
-          target.value
-        )}
-    />
-  </div>
+            {#each Object.entries(abilities) as [ability, label]}
+                <input
+                    class="option-input"
+                    type="radio"
+                    name={`${actionId}-${promptId}-ability`}
+                    id={`${actionId}-${promptId}-ability-${ability}`}
+                    value={ability}
+                    checked={prompt.ability === ability}
+                    on:change={({ target }) =>
+                        updateDocumentDataFromField(
+                            $item,
+                            `system.actions.${actionId}.prompts.${promptId}.ability`,
+                            target.value
+                        )}
+                />
+
+                <label
+                    class="option-label"
+                    for={`${actionId}-${promptId}-ability-${ability}`}
+                >
+                    {localize(label)}
+                </label>
+            {/each}
+        </div>
+    </div>
+
+    <div class="a5e-field-group a5e-field-group--formula">
+        <label for={`${actionId}-${promptId}-dc`}>
+            {localize("A5E.ItemAbilityCheckDC")}
+        </label>
+
+        <input
+            id={`${actionId}-${promptId}-dc`}
+            name={`${actionId}-${promptId}-dc`}
+            type="text"
+            value={prompt.abilityDC ?? ""}
+            on:change={({ target }) =>
+                updateDocumentDataFromField(
+                    $item,
+                    `system.actions.${actionId}.prompts.${promptId}.abilityDC`,
+                    target.value
+                )}
+        />
+    </div>
+
+    <div class="a5e-field-group ">
+        <label for={`${actionId}-${promptId}-save-effect`}>
+            {localize("A5E.ItemEffectOnCheck")}
+        </label>
+
+        <input
+            id={`${actionId}-${promptId}-save-effect`}
+            name={`${actionId}-${promptId}-save-effect`}
+            type="text"
+            value={prompt.onSave ?? ""}
+            on:change={({ target }) =>
+                updateDocumentDataFromField(
+                    $item,
+                    `system.actions.${actionId}.prompts.${promptId}.onSave`,
+                    target.value
+                )}
+        />
+    </div>
 </section>
 
 <style lang="scss">
-  .option {
-    &-input {
-      display: none;
+    .option {
+        &-input {
+            display: none;
 
-      &:checked + .option-label {
-        background: #2b6537;
-        border-color: darken($color: #2b6537, $amount: 5);
-        color: #f6f2eb;
-      }
-    }
+            &:checked + .option-label {
+                background: #2b6537;
+                border-color: darken($color: #2b6537, $amount: 5);
+                color: #f6f2eb;
+            }
+        }
 
-    &-label {
-      border-radius: 3px;
-      border: 1px solid #bbb;
-      padding: 0.125rem 0.25rem;
-      cursor: pointer;
-      transition: all 0.15s ease-in-out;
-    }
+        &-label {
+            border-radius: 3px;
+            border: 1px solid #bbb;
+            padding: 0.125rem 0.25rem;
+            cursor: pointer;
+            transition: all 0.15s ease-in-out;
+        }
 
-    &-list {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 0.25rem;
-      font-size: 0.694rem;
+        &-list {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 0.25rem;
+            font-size: 0.694rem;
+        }
     }
-  }
 </style>
--- src/apps/components/itemActionsConfig/GenericPromptConfig.svelte
+++ src/apps/components/itemActionsConfig/GenericPromptConfig.svelte
@@ -1,88 +1,90 @@
 <script>
-  import { getContext } from "svelte";
-  import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+    import { getContext } from "svelte";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
 
-  import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
+    import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
 
-  const item = getContext("item");
-  const actionId = getContext("actionId");
+    const item = getContext("item");
+    const actionId = getContext("actionId");
 
-  export let prompt;
-  export let promptId;
+    export let prompt;
+    export let promptId;
 </script>
 
 <section class="action-config__wrapper">
-  <div class="a5e-field-group a5e-field-group--label">
-    <label for={`${actionId}-${promptId}-label`}>Label</label>
+    <div class="a5e-field-group a5e-field-group--label">
+        <label for={`${actionId}-${promptId}-label`}>
+            {localize("A5E.Label")}
+        </label>
 
-    <input
-      id={`${actionId}-${promptId}-label`}
-      name={`${actionId}-${promptId}-label`}
-      type="text"
-      value={prompt.label ?? ""}
-      on:change={({ target }) =>
-        updateDocumentDataFromField(
-          $item,
-          `system.actions.${actionId}.prompts.${promptId}.label`,
-          target.value
-        )}
-    />
-  </div>
+        <input
+            id={`${actionId}-${promptId}-label`}
+            name={`${actionId}-${promptId}-label`}
+            type="text"
+            value={prompt.label ?? ""}
+            on:change={({ target }) =>
+                updateDocumentDataFromField(
+                    $item,
+                    `system.actions.${actionId}.prompts.${promptId}.label`,
+                    target.value
+                )}
+        />
+    </div>
 
-  <div class="a5e-field-group a5e-field-group--formula">
-    <label for={`${actionId}-${promptId}-roll-formula`}>
-      {localize("A5E.RollFormula")}
-    </label>
+    <div class="a5e-field-group a5e-field-group--formula">
+        <label for={`${actionId}-${promptId}-roll-formula`}>
+            {localize("A5E.RollFormula")}
+        </label>
 
-    <input
-      id={`${actionId}-${promptId}-roll-formula`}
-      name={`${actionId}-${promptId}-roll-formula`}
-      type="text"
-      value={prompt.formula ?? ""}
-      on:change={({ target }) =>
-        updateDocumentDataFromField(
-          $item,
-          `system.actions.${actionId}.prompts.${promptId}.formula`,
-          target.value
-        )}
-    />
-  </div>
+        <input
+            id={`${actionId}-${promptId}-roll-formula`}
+            name={`${actionId}-${promptId}-roll-formula`}
+            type="text"
+            value={prompt.formula ?? ""}
+            on:change={({ target }) =>
+                updateDocumentDataFromField(
+                    $item,
+                    `system.actions.${actionId}.prompts.${promptId}.formula`,
+                    target.value
+                )}
+        />
+    </div>
 
-  <div class="a5e-field-group a5e-field-group--formula">
-    <label for={`${actionId}-${promptId}-dc`}>
-      {localize("A5E.DC")}
-    </label>
+    <div class="a5e-field-group a5e-field-group--formula">
+        <label for={`${actionId}-${promptId}-dc`}>
+            {localize("A5E.DC")}
+        </label>
 
-    <input
-      id={`${actionId}-${promptId}-dc`}
-      name={`${actionId}-${promptId}-dc`}
-      type="text"
-      value={prompt.dc ?? ""}
-      on:change={({ target }) =>
-        updateDocumentDataFromField(
-          $item,
-          `system.actions.${actionId}.prompts.${promptId}.dc`,
-          target.value
-        )}
-    />
-  </div>
+        <input
+            id={`${actionId}-${promptId}-dc`}
+            name={`${actionId}-${promptId}-dc`}
+            type="text"
+            value={prompt.dc ?? ""}
+            on:change={({ target }) =>
+                updateDocumentDataFromField(
+                    $item,
+                    `system.actions.${actionId}.prompts.${promptId}.dc`,
+                    target.value
+                )}
+        />
+    </div>
 
-  <div class="a5e-field-group ">
-    <label for={`${actionId}-${promptId}-save-effect`}>
-      {localize("A5E.ItemEffectOnCheck")}
-    </label>
+    <div class="a5e-field-group ">
+        <label for={`${actionId}-${promptId}-save-effect`}>
+            {localize("A5E.ItemEffectOnCheck")}
+        </label>
 
-    <input
-      id={`${actionId}-${promptId}-save-effect`}
-      name={`${actionId}-${promptId}-save-effect`}
-      type="text"
-      value={prompt.onSave ?? ""}
-      on:change={({ target }) =>
-        updateDocumentDataFromField(
-          $item,
-          `system.actions.${actionId}.prompts.${promptId}.onSave`,
-          target.value
-        )}
-    />
-  </div>
+        <input
+            id={`${actionId}-${promptId}-save-effect`}
+            name={`${actionId}-${promptId}-save-effect`}
+            type="text"
+            value={prompt.onSave ?? ""}
+            on:change={({ target }) =>
+                updateDocumentDataFromField(
+                    $item,
+                    `system.actions.${actionId}.prompts.${promptId}.onSave`,
+                    target.value
+                )}
+        />
+    </div>
 </section>
