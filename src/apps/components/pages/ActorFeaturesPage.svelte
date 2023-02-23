29ea27df9daa89ab6e803d6defeb2677b2a9929e
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-02-23 03:52:42 -0800

    Changes gap of reducers to 0.75rem instead of 0.25rem to account for all the added configuration

--- src/apps/components/pages/ActorFeaturesPage.svelte
+++ src/apps/components/pages/ActorFeaturesPage.svelte
@@ -39,7 +39,7 @@
         display: flex;
         flex-grow: 1;
         flex-direction: column;
-        gap: 0.25rem;
+        gap: 0.75rem;
         overflow-y: auto;
         overflow-x: hidden;
     }
--- src/apps/components/pages/ActorManeuversPage.svelte
+++ src/apps/components/pages/ActorManeuversPage.svelte
@@ -93,7 +93,7 @@
         display: flex;
         flex-grow: 1;
         flex-direction: column;
-        gap: 0.25rem;
+        gap: 0.75rem;
         overflow-y: auto;
         overflow-x: hidden;
     }
