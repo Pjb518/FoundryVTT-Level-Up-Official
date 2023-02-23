68d626765287045c8879cd4cb0b5bca220fbb6f8
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-02-23 00:57:16 -0800

    Adds support for tokenizer when changing the actor sheet token.

--- src/apps/handlers/editDocumentImage.js
+++ src/apps/handlers/editDocumentImage.js
@@ -1,4 +1,14 @@
-export default function editImage(document) {
+export default async function editImage(document) {
+  // Add support for tokenizer
+  if (game.modules.get('vtta-tokenizer')?.active) {
+    if (['character', 'npc'].includes(document.type)) {
+      // eslint-disable-next-line no-undef
+      Tokenizer?.tokenizeActor(document);
+
+      return null;
+    }
+  }
+
   const current = document.img;
   const filePicker = new FilePicker({
     type: 'image',
