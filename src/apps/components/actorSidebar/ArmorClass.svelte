c559b82f8a4c68041ba4dcb34d7ad53a20ab2e60
Phil <me@phillip.best>
2023-02-24 20:58:17 +0000

    Improves the shape of the AC background shield. Disables AC input when sheet is locked.

--- src/apps/components/actorSidebar/ArmorClass.svelte
+++ src/apps/components/actorSidebar/ArmorClass.svelte
@@ -4,16 +4,11 @@
     import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
 
     const actor = getContext("actor");
+
+    $: sheetIsLocked = $actor.flags?.a5e?.sheetIsLocked ?? true;
 </script>
 
-<!-- <div
-        v-if="sheetIsLocked"
-        class="a5e-ability-score__value a5e-ability-score__value--ac"
-    >
-        {$actor.system.attributes.ac}
-    </div> -->
-
-<li>
+<li class="ac-wrapper">
     <h4 class="ac-label">AC</h4>
     <input
         class="ac-value"
@@ -21,6 +16,7 @@
         type="number"
         value={$actor.system.attributes.ac}
         placeholder="10"
+        disabled={sheetIsLocked}
         on:change={({ target }) =>
             updateDocumentDataFromField(
                 $actor,
@@ -28,30 +24,56 @@
                 Number(target.value)
             )}
     />
+
+    <svg
+        class="ac-background"
+        version="1.1"
+        x="0px"
+        y="0px"
+        viewBox="0 0 90 100"
+        xml:space="preserve"
+    >
+        <path
+            d="M45,100C-2.6,79.3,0,12.6,0,12.6c0-2.2,1.8-4,4.4-4.6l39.1-7.9C44,0,44.5,0,45,0c0.5,0,1,0,1.4,0.1L85.5,8
+            c2.6,0.5,4.4,2.4,4.4,4.6C90,12.6,92.6,79.3,45,100L45,100z"
+        />
+    </svg>
 </li>
 
 <style lang="scss">
     .ac-value,
     .ac-value[type="number"] {
+        position: relative;
         display: flex;
         align-items: center;
-        height: 26px;
+        height: 2.5rem;
+        width: 2.5rem;
+        margin: auto;
         border: 0;
         background: none;
         font-weight: inherit;
         text-align: center;
         font-size: 1.44rem;
-        width: 2.5rem;
-
-        background-color: rgb(0 0 0/ 0.15);
-        border-radius: 50% 50% 50% 50% / 12% 12% 88% 88%;
-        height: 2.5rem;
-        margin: auto;
+        z-index: 1;
     }
 
     .ac-label {
         font-size: 1rem;
         text-align: center;
         padding-bottom: 0.125rem;
+    }
+
+    .ac-wrapper {
+        position: relative;
+    }
+
+    .ac-background {
+        position: absolute;
+        height: 2.5rem;
+        top: 0;
+        left: 50%;
+        transform: translate(-50%, 50%);
+        fill: rgba(0, 0, 0, 0.15);
+        z-index: 0;
     }
 </style>
