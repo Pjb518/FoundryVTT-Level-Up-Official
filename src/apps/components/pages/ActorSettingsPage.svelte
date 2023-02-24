ab31be3418d9dee9cd3ecb2b53d09625b0cd4779
Phil <me@phillip.best>
2023-02-24 21:36:52 +0000

    Adds a new double carry capacity setting, replacing the old carry capacity multiplier.

--- src/apps/components/pages/ActorSettingsPage.svelte
+++ src/apps/components/pages/ActorSettingsPage.svelte
@@ -102,26 +102,29 @@
         </FormSection>
 
         {#if $actor.flags?.a5e?.trackInventoryWeight ?? true}
-            <FormSection
-                heading="A5E.SettingsCarryCapacityMultiplier"
-                hint="Do not increase this number to account for your size category."
-            >
-                <div class="u-w-full">
-                    <div class="u-w-20">
+            <FormSection>
+                <div class="u-align-center u-flex u-gap-md">
+                    <div class="u-align-center u-flex u-gap-md">
                         <input
-                            class="a5e-input"
-                            type="number"
-                            data-dtype="Number"
-                            min="1"
-                            name="flags.a5e.carryCapacityMultiplier"
-                            value={flags.a5e?.carryCapacityMultiplier || 1}
+                            class="u-pointer"
+                            type="checkbox"
+                            name="flags.a5e.doubleCarryCapacity"
+                            id={`${$actor.id}-track-currency-weight`}
+                            checked={flags.a5e?.doubleCarryCapacity ?? false}
                             on:change={({ target }) =>
                                 updateDocumentDataFromField(
                                     $actor,
                                     target.name,
-                                    Number(target.value)
+                                    target.checked
                                 )}
                         />
+
+                        <label
+                            class="u-pointer"
+                            for={`${$actor.id}-track-currency-weight`}
+                        >
+                            {localize("A5E.SettingsDoubleCarryCapacity")}
+                        </label>
                     </div>
                 </div>
             </FormSection>
--- src/apps/handlers/calculateCarryCapacity.js
+++ src/apps/handlers/calculateCarryCapacity.js
@@ -2,7 +2,7 @@ export default function calculateCarryCapacity(actorData) {
   const strengthScore = actorData.system.abilities.str.value;
   const { size } = actorData.system.traits;
   const baseCarryCapacityMultiplier = CONFIG.A5E.carryCapacityMultiplier[size];
-  const carryCapacityMultiplier = Number(actorData.flags.a5e?.carryCapacityMultiplier) || 1;
+  const carryCapacityMultiplier = actorData.flags.a5e?.doubleCarryCapacity ? 2 : 1;
 
   return strengthScore * baseCarryCapacityMultiplier * carryCapacityMultiplier * 15;
 }
