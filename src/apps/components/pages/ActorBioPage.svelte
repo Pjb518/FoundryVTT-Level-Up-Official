cacaafb035f77b6547f017868eafe352fd6a446c
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-02-24 01:05:31 -0800

    Possibly Fixes issue with notes not showing up on initial load

--- src/apps/components/pages/ActorBioPage.svelte
+++ src/apps/components/pages/ActorBioPage.svelte
@@ -1,71 +1,76 @@
 <script>
-  import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
-  import { getContext } from "svelte";
-  import { TJSProseMirror } from "@typhonjs-fvtt/svelte-standard/component";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+    import { getContext } from "svelte";
+    import { TJSProseMirror } from "@typhonjs-fvtt/svelte-standard/component";
 
-  import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
+    import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
 
-  const actor = getContext("actor");
+    const actor = getContext("actor");
 
-  export let currentEditor;
+    export let currentEditor;
 
-  const traitsLabel = {
-    age: "A5E.DetailsAge",
-    eyeColor: "A5E.DetailsEyeColor",
-    hairColor: "A5E.DetailsHairColor",
-    skinColor: "A5E.DetailsSkinColor",
-    height: "A5E.DetailsHeight",
-    weight: "A5E.DetailsWeight",
-    gender: "A5E.DetailsGender",
-  };
+    const traitsLabel = {
+        age: "A5E.DetailsAge",
+        eyeColor: "A5E.DetailsEyeColor",
+        hairColor: "A5E.DetailsHairColor",
+        skinColor: "A5E.DetailsSkinColor",
+        height: "A5E.DetailsHeight",
+        weight: "A5E.DetailsWeight",
+        gender: "A5E.DetailsGender",
+    };
 
-  function onSelectEditor(editor) {
-    currentEditor = editor;
-  }
+    function onSelectEditor(editor) {
+        currentEditor = editor;
+    }
 
-  function updateDescription(event) {
-    const { content } = event.detail;
+    function updateDescription(event) {
+        const { content } = event.detail;
 
-    $actor.update({
-      [`system.details.${currentEditor}`]: content === "<p></p>" ? "" : content,
-    });
-  }
+        $actor.update({
+            [`system.details.${currentEditor}`]:
+                content === "<p></p>" ? "" : content,
+        });
+    }
 
-  $: currentEditor = "bio";
-  $: details = $actor.system.details;
+    $: currentEditor = "bio";
+    $: details = $actor.system.details;
 </script>
 
 <section class="a5e-box u-p-md a5e-form__section--bio-wrapper">
-  {#each Object.entries(traitsLabel) as [key, label]}
-    <div
-      class="u-flex u-align-center u-gap-md u-justify-space-between"
-      data-type={key}
-    >
-      <h3 class="u-text-bold u-text-sm u-flex-shrink-0 u-mb-0">
-        {localize(label)}
-      </h3>
+    {#each Object.entries(traitsLabel) as [key, label]}
+        <div
+            class="u-flex u-align-center u-gap-md u-justify-space-between"
+            data-type={key}
+        >
+            <h3 class="u-text-bold u-text-sm u-flex-shrink-0 u-mb-0">
+                {localize(label)}
+            </h3>
 
-      <div class="a5e-input-container a5e-input-container--bio">
-        <input
-          class="a5e-input a5e-input--slim"
-          type="text"
-          name="system.details.{key}"
-          value={details[key]}
-          on:change={({ target }) => {
-            updateDocumentDataFromField($actor, target.name, target.value);
-          }}
-        />
-      </div>
-    </div>
-  {/each}
+            <div class="a5e-input-container a5e-input-container--bio">
+                <input
+                    class="a5e-input a5e-input--slim"
+                    type="text"
+                    name="system.details.{key}"
+                    value={details[key]}
+                    on:change={({ target }) => {
+                        updateDocumentDataFromField(
+                            $actor,
+                            target.name,
+                            target.value
+                        );
+                    }}
+                />
+            </div>
+        </div>
+    {/each}
 </section>
 
 <section />
 
 <section class="u-flex u-flex-grow u-gap-lg">
-  <div class="u-flex u-flex-col u-flex-grow">
-    <div
-      class="
+    <div class="u-flex u-flex-col u-flex-grow">
+        <div
+            class="
                 u-align-center
                 u-border-b
                 u-border-gray
@@ -74,11 +79,11 @@
                 u-mb-md
                 u-pb-md
             "
-    >
-      <!-- svelte-ignore a11y-missing-attribute -->
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <a
-        class="
+        >
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <a
+                class="
                     a5e-button
                     u-border
                     u-hover-bg-green
@@ -87,20 +92,21 @@
                     u-rounded
                     u-text-sm
                     u-transition
+                    u-hover-text-shadow-none:hover
                 "
-        class:u-border-gray={currentEditor !== "bio"}
-        class:u-bg-green={currentEditor === "bio"}
-        class:u-border-green={currentEditor === "bio"}
-        class:u-text-light={currentEditor === "bio"}
-        on:click={() => onSelectEditor("bio")}
-      >
-        {localize("A5E.DetailsBackstory")}
-      </a>
+                class:u-border-gray={currentEditor !== "bio"}
+                class:u-bg-green={currentEditor === "bio"}
+                class:u-border-green={currentEditor === "bio"}
+                class:u-text-light={currentEditor === "bio"}
+                on:click={() => onSelectEditor("bio")}
+            >
+                {localize("A5E.DetailsBackstory")}
+            </a>
 
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <!-- svelte-ignore a11y-missing-attribute -->
-      <a
-        class="
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a
+                class="
                     a5e-button
                     u-border
                     u-hover-bg-green
@@ -109,39 +115,40 @@
                     u-rounded
                     u-text-sm
                     u-transition
+                    u-hover-text-shadow-none:hover
                 "
-        class:u-border-gray={currentEditor !== "appearance"}
-        class:u-bg-green={currentEditor === "appearance"}
-        class:u-border-green={currentEditor === "appearance"}
-        class:u-text-light={currentEditor === "appearance"}
-        on:click={() => onSelectEditor("appearance")}
-      >
-        {localize("A5E.DetailsAppearance")}
-      </a>
-    </div>
+                class:u-border-gray={currentEditor !== "appearance"}
+                class:u-bg-green={currentEditor === "appearance"}
+                class:u-border-green={currentEditor === "appearance"}
+                class:u-text-light={currentEditor === "appearance"}
+                on:click={() => onSelectEditor("appearance")}
+            >
+                {localize("A5E.DetailsAppearance")}
+            </a>
+        </div>
 
-    <div class="editor">
-      <!-- svelte-ignore missing-declaration -->
-      <TJSProseMirror
-        content={$actor.system.details[currentEditor] ||
-          localize("A5E.NoDescription")}
-        enrichedContent={TextEditor.enrichHTML(
-          $actor.system.details[currentEditor],
-          {
-            async: false,
-          }
-        )}
-        on:editor:save={(event) => updateDescription(event)}
-      />
+        <div class="editor">
+            <!-- svelte-ignore missing-declaration -->
+            <TJSProseMirror
+                content={$actor.system.details[currentEditor] ||
+                    localize("A5E.NoDescription")}
+                enrichedContent={TextEditor.enrichHTML(
+                    $actor.system.details[currentEditor],
+                    {
+                        async: false,
+                    }
+                )}
+                on:editor:save={(event) => updateDescription(event)}
+            />
+        </div>
     </div>
-  </div>
 </section>
 
 <style lang="scss">
-  .editor {
-    height: 100%;
+    .editor {
+        height: 100%;
 
-    // Nudges the edit icon down 1px. Removing this hides the top border for the button.
-    --tjs-editor-edit-top: 1px;
-  }
+        // Nudges the edit icon down 1px. Removing this hides the top border for the button.
+        --tjs-editor-edit-top: 1px;
+    }
 </style>
--- src/apps/components/pages/ActorNotesPage.svelte
+++ src/apps/components/pages/ActorNotesPage.svelte
@@ -1,39 +1,36 @@
 <script>
-  import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
-  import { getContext } from "svelte";
-  import { TJSProseMirror } from "@typhonjs-fvtt/svelte-standard/component";
+    import { getContext } from "svelte";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+    import { TJSProseMirror } from "@typhonjs-fvtt/svelte-standard/component";
 
-  import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
+    const actor = getContext("actor");
 
-  const actor = getContext("actor");
+    function updateDescription(event) {
+        const { content } = event.detail;
 
-  function updateDescription(event) {
-    const { content } = event.detail;
+        $actor.update({
+            "system.details.notes": content === "<p></p>" ? "" : content,
+        });
+    }
 
-    $actor.update({
-      "system.details.notes": content === "<p></p>" ? "" : content,
-    });
-  }
-
-  $: details = $actor.system.details;
+    $: content = $actor.system.details.notes || localize("A5E.NoDescription");
+    $: enrichedContent = TextEditor.enrichHTML(content, { async: false });
 </script>
 
 <div class="editor">
-  <!-- svelte-ignore missing-declaration -->
-  <TJSProseMirror
-    content={$actor.system.details.notes || localize("A5E.NoDescription")}
-    enrichedContent={TextEditor.enrichHTML($actor.system.details.notes, {
-      async: false,
-    })}
-    on:editor:save={(event) => updateDescription(event)}
-  />
+    <!-- svelte-ignore missing-declaration -->
+    <TJSProseMirror
+        {content}
+        {enrichedContent}
+        on:editor:save={(event) => updateDescription(event)}
+    />
 </div>
 
 <style lang="scss">
-  .editor {
-    height: 100%;
+    .editor {
+        height: 100%;
 
-    // Nudges the edit icon down 1px. Removing this hides the top border for the button.
-    --tjs-editor-edit-top: 1px;
-  }
+        // Nudges the edit icon down 1px. Removing this hides the top border for the button.
+        --tjs-editor-edit-top: 1px;
+    }
 </style>
