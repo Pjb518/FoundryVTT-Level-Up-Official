7f2a117c1da8445b0f267ff846afe94859b7089b
Phil <me@phillip.best>
2023-02-25 17:19:38 +0000

    WIP! Adds maneuver summary.

---
+++ src/apps/components/itemSummaries/ManeuverSummary.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+
+    export let item;
+
+    const action = item.actions.values()[0];
+</script>
+
+{#if item.system.degree === 0}
+    <p class="summary">
+        {localize(CONFIG.A5E.maneuverDegrees[item.system.degree])}
+    </p>
+{:else if item.system.degree}
+    <p class="summary">
+        {localize(CONFIG.A5E.maneuverDegrees[item.system.degree])}
+
+        {#if item.system.tradition}
+            {localize(CONFIG.A5E.maneuverTraditions[item.system.tradition])}
+        {/if}
+
+        {#if item.system.exertionCost}
+            ({item.system.exertionCost}
+            {localize(
+                `A5E.ExertionPoint${
+                    item.system.exertionCost > 1 ? "Plural" : ""
+                }`
+            )})
+        {/if}
+    </p>
+{/if}
+
+<style lang="scss">
+    .summary {
+        font-style: italic;
+        margin-bottom: 0.5rem;
+    }
+</style>
