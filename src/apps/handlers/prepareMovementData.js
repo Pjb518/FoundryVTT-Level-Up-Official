afd8b84f21bfb98e68bd2df45f41bb12da37fcc6
JoshLaunchcode <holo.sf0@gmail.com>
2023-02-28 17:29:18 -0600

    adds hover functionality to actor

--- src/apps/handlers/prepareMovementData.js
+++ src/apps/handlers/prepareMovementData.js
@@ -1,10 +1,23 @@
 export default function getMovementData(data) {
   // eslint-disable-next-line no-unused-vars
-  const speeds = Object.entries(data.system.attributes.movement).filter(([_, speed]) => speed);
+  const hover = data.system.attributes.movement.traits.hover
+  const speeds = Object.entries(data.system.attributes.movement).filter(([key, speed]) => {
+    if(!speed && key === "fly" && hover){
+      return !speed
+    } else {
+      return speed
+    }
+  }).filter(([key]) => key !== "traits");
+  
   const unit = game.i18n.localize('A5E.MeasurementFeetAbbr');
+  const hoverAbbr = game.i18n.localize('A5E.MovementHoverAbbr');
 
   return speeds.map(([name, speed]) => {
     const label = game.i18n.localize(CONFIG.A5E.movement[name]);
-    return `${label} - ${speed} ${unit}`;
+    if(name == "fly" && hover){
+      return `${label} - ${speed || 0} ${unit} ${hoverAbbr}`;  
+    } else {
+      return `${label} - ${speed} ${unit}`;
+    }
   });
 }
