3eb97f9e77266f625dd7bfc0de57e38ffee7deec
JoshLaunchcode <holo.sf0@gmail.com>
2023-02-28 00:08:44 -0600

    adds properties to feats and knacks

--- src/apps/components/itemPropertiesConfig/FeatureConfiguration.svelte
+++ src/apps/components/itemPropertiesConfig/FeatureConfiguration.svelte
@@ -1,25 +1,26 @@
 <script>
-    import { getContext } from "svelte";
-    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+  import { getContext } from "svelte";
+  import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
 
-    import FormSection from "../FormSection.svelte";
-    import RadioGroup from "../RadioGroup.svelte";
+  import FormSection from "../FormSection.svelte";
+  import RadioGroup from "../RadioGroup.svelte";
 
-    const item = getContext("item");
+  const item = getContext("item");
+  const hasPrerequisite = ["knack", "feat"];
 
-    let editMode = false;
+  let editMode = false;
 
-    function toggleEditMode() {
-        editMode = !editMode;
-    }
+  function toggleEditMode() {
+    editMode = !editMode;
+  }
 
-    const featureTypes = CONFIG.A5E.featureTypes;
+  const featureTypes = CONFIG.A5E.featureTypes;
 </script>
 
 <section>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <header
-        class="
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <header
+    class="
             u-align-center
             u-flex
             u-font-serif
@@ -30,39 +31,49 @@
             u-text-lg
             u-w-fit
         "
-        on:click={toggleEditMode}
-    >
-        <h3>{localize("A5E.TabFeatureProperties")}</h3>
-        <i
-            class="u-text-sm fas"
-            class:fa-chevron-up={editMode}
-            class:fa-edit={!editMode}
-        />
-    </header>
+    on:click={toggleEditMode}
+  >
+    <h3>{localize("A5E.TabFeatureProperties")}</h3>
+    <i
+      class="u-text-sm fas"
+      class:fa-chevron-up={editMode}
+      class:fa-edit={!editMode}
+    />
+  </header>
 
-    {#if editMode}
-        <div class="u-flex u-flex-col u-gap-md">
-            <FormSection heading="A5E.FeatureTypePrompt">
-                <RadioGroup
-                    options={Object.entries(featureTypes)}
-                    selected={$item.system.featureType}
-                    name="system.featureType"
-                    document={item}
-                />
-            </FormSection>
+  {#if editMode}
+    <div class="u-flex u-flex-col u-gap-md">
+      <FormSection heading="A5E.FeatureTypePrompt">
+        <RadioGroup
+          options={Object.entries(featureTypes)}
+          selected={$item.system.featureType}
+          name="system.featureType"
+          document={item}
+        />
+      </FormSection>
+    </div>
+  {:else}
+    <dl class="a5e-box u-flex u-flex-col u-gap-sm u-m-0 u-p-md u-text-sm">
+      <div class="u-flex u-gap-md">
+        <dt class="u-text-bold">
+          {localize("A5E.FeatureTypePrompt")}:
+        </dt>
+        <dd class="u-m-0 u-p-0">
+          {localize(featureTypes[$item.system.featureType] ?? "A5E.None")}
+        </dd>
+      </div>
+    </dl>
+  {/if}
+
+  {#if hasPrerequisite.includes($item.system.featureType)}
+    <FormSection heading="Prerequisites:">
+      <div
+        class="u-align-center u-flex u-flex-wrap u-gap-md u-text-sm u-w-full"
+      >
+        <div class="u-align-center u-flex u-gap-md u-w-full">
+          <input class="u-pl-lg" type="text" name="Prerequisite" value="" />
         </div>
-    {:else}
-        <dl class="a5e-box u-flex u-flex-col u-gap-sm u-m-0 u-p-md u-text-sm">
-            <div class="u-flex u-gap-md">
-                <dt class="u-text-bold">
-                    {localize("A5E.FeatureTypePrompt")}:
-                </dt>
-                <dd class="u-m-0 u-p-0">
-                    {localize(
-                        featureTypes[$item.system.featureType] ?? "A5E.None"
-                    )}
-                </dd>
-            </div>
-        </dl>
-    {/if}
+      </div>
+    </FormSection>
+  {/if}
 </section>
