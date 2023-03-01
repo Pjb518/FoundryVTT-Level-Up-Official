59ba34e5d54c090c55574545b52ed15108caf12b
JoshLaunchcode <holo.sf0@gmail.com>
2023-03-01 08:53:03 -0600

    moves the hover checkbox into the Flying Speed declaration area

--- src/apps/dialogs/MovementConfigDialog.svelte
+++ src/apps/dialogs/MovementConfigDialog.svelte
@@ -1,5 +1,6 @@
 <script>
     import { getContext } from "svelte";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
     import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store";
 
     import FormSection from "../components/FormSection.svelte";
@@ -23,42 +24,59 @@
     <!-- TODO: Maybe make distance a number? -->
     {#each Object.entries($actor.system.attributes.movement) as [mode, distance]}
         {#if mode != "traits"}
-            <FormSection heading={headings[mode]} inline={true}>
-                <div class="u-w-20">
-                    <input
-                        class="a5e-input"
-                        type="number"
-                        name={`system.attributes.movement.${mode}`}
-                        value={distance || 0}
-                        on:change={({ target }) => {
-                            updateDocumentDataFromField(
-                                $actor,
-                                target.name,
-                                target.value
-                            );
-                        }}
-                    />
+            <FormSection>
+                <div class="outer-wrapper">
+                    <div class="field-wrapper">
+                        <h3 class="form-heading u-flex-shrink-0 u-w-30">
+                            {localize(headings[mode])}
+                        </h3>
+
+                        <div class="u-w-20">
+                            <input
+                                class="a5e-input"
+                                type="text"
+                                name={`system.attributes.movement.${mode}`}
+                                value={distance || ""}
+                                on:change={({ target }) => {
+                                    updateDocumentDataFromField(
+                                        $actor,
+                                        target.name,
+                                        target.value
+                                    );
+                                }}
+                            />
+                        </div>
+                    </div>
+
+                    {#if mode === "fly"}
+                        <div class="u-align-center u-flex u-gap-md">
+                            <input
+                                class="u-pointer"
+                                type="checkbox"
+                                name="system.attributes.movement.traits.hover"
+                                checked={$actor.system.attributes.movement
+                                    .traits.hover}
+                                on:change={({ target }) => {
+                                    updateDocumentDataFromField(
+                                        $actor,
+                                        target.name,
+                                        target.checked
+                                    );
+                                }}
+                            />
+
+                            <label
+                                for={`${appId}-${""}-hover`}
+                                class="u-pointer"
+                            >
+                                {localize("A5E.MovementHover")}
+                            </label>
+                        </div>
+                    {/if}
                 </div>
             </FormSection>
         {/if}
     {/each}
-    <FormSection heading="A5E.MovementHover" inline={true}>
-        <div class="u-w-20">
-            <input
-                class="u-pointer"
-                type="checkbox"
-                name="system.attributes.movement.traits.hover"
-                checked={$actor.system.attributes.movement.traits.hover}
-                on:change={({ target }) => {
-                    updateDocumentDataFromField(
-                        $actor,
-                        target.name,
-                        target.checked
-                    );
-                }}
-            />
-        </div>
-    </FormSection>
 </article>
 
 <style lang="scss">
@@ -70,5 +88,25 @@
         gap: 0.5rem;
         overflow: auto;
         background: rgba(246, 242, 235, 0.5);
+    }
+
+    .form-heading {
+        font-size: 0.833rem;
+        font-weight: bold;
+    }
+
+    .field-wrapper {
+        display: flex;
+        align-items: center;
+    }
+
+    .outer-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+
+    .u-pointer {
+        margin-left: 0px;
     }
 </style>
