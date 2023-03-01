f9ff916294af82002c5dfbcb6632888b6b1b5c74
JoshLaunchcode <holo.sf0@gmail.com>
2023-02-28 18:47:54 -0600

    makes adjustments based on PR feedback

--- src/apps/handlers/prepareMovementData.js
+++ src/apps/handlers/prepareMovementData.js
@@ -2,20 +2,18 @@ export default function getMovementData(data) {
   // eslint-disable-next-line no-unused-vars
   const hover = data.system.attributes.movement.traits.hover
   const speeds = Object.entries(data.system.attributes.movement).filter(([key, speed]) => {
-    if(!speed && key === "fly" && hover){
-      return !speed
-    } else {
-      return speed
-    }
-  }).filter(([key]) => key !== "traits");
-  
+    if (key === "fly" && !speed && hover) { return true; }
+    if (key === "traits") { return false; }
+    return speed
+  })
+
   const unit = game.i18n.localize('A5E.MeasurementFeetAbbr');
   const hoverAbbr = game.i18n.localize('A5E.MovementHoverAbbr');
 
   return speeds.map(([name, speed]) => {
     const label = game.i18n.localize(CONFIG.A5E.movement[name]);
-    if(name == "fly" && hover){
-      return `${label} - ${speed || 0} ${unit} ${hoverAbbr}`;  
+    if (name == "fly" && hover) {
+      return `${label} - ${speed || 0} ${unit} ${hoverAbbr}`;
     } else {
       return `${label} - ${speed} ${unit}`;
     }
