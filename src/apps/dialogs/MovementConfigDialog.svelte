648ff6361d8567ec52fac857ac3849041372001a
JoshLaunchcode <holo.sf0@gmail.com>
2023-03-01 07:12:50 -0600

    Fixes spacing for .svelte files

--- src/apps/dialogs/MovementConfigDialog.svelte
+++ src/apps/dialogs/MovementConfigDialog.svelte
@@ -1,66 +1,74 @@
 <script>
-  import { getContext } from "svelte";
-  import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store";
+    import { getContext } from "svelte";
+    import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store";
 
-  import FormSection from "../components/FormSection.svelte";
+    import FormSection from "../components/FormSection.svelte";
 
-  import updateDocumentDataFromField from "../utils/updateDocumentDataFromField";
+    import updateDocumentDataFromField from "../utils/updateDocumentDataFromField";
 
-  export let { actorDocument, appId } = getContext("#external").application;
+    export let { actorDocument, appId } = getContext("#external").application;
 
-  const actor = new TJSDocument(actorDocument);
+    const actor = new TJSDocument(actorDocument);
 
-  const headings = {
-    burrow: "A5E.MovementBurrowingSpeed",
-    climb: "A5E.MovementClimbingSpeed",
-    fly: "A5E.MovementFlyingSpeed",
-    swim: "A5E.MovementSwimmingSpeed",
-    walk: "A5E.MovementWalkingSpeed",
-  };
+    const headings = {
+        burrow: "A5E.MovementBurrowingSpeed",
+        climb: "A5E.MovementClimbingSpeed",
+        fly: "A5E.MovementFlyingSpeed",
+        swim: "A5E.MovementSwimmingSpeed",
+        walk: "A5E.MovementWalkingSpeed",
+    };
 </script>
 
 <article>
-  <!-- TODO: Maybe make distance a number? -->
-  {#each Object.entries($actor.system.attributes.movement) as [mode, distance]}
-    {#if mode != "traits"}
-      <FormSection heading={headings[mode]} inline={true}>
+    <!-- TODO: Maybe make distance a number? -->
+    {#each Object.entries($actor.system.attributes.movement) as [mode, distance]}
+        {#if mode != "traits"}
+            <FormSection heading={headings[mode]} inline={true}>
+                <div class="u-w-20">
+                    <input
+                        class="a5e-input"
+                        type="text"
+                        name={`system.attributes.movement.${mode}`}
+                        value={distance || ""}
+                        on:change={({ target }) => {
+                            updateDocumentDataFromField(
+                                $actor,
+                                target.name,
+                                target.value
+                            );
+                        }}
+                    />
+                </div>
+            </FormSection>
+        {/if}
+    {/each}
+    <FormSection heading="A5E.MovementHover" inline={true}>
         <div class="u-w-20">
-          <input
-            class="a5e-input"
-            type="text"
-            name={`system.attributes.movement.${mode}`}
-            value={distance || ""}
-            on:change={({ target }) => {
-              updateDocumentDataFromField($actor, target.name, target.value);
-            }}
-          />
+            <input
+                class="u-pointer"
+                type="checkbox"
+                name="system.attributes.movement.traits.hover"
+                checked={$actor.system.attributes.movement.traits.hover}
+                on:change={({ target }) => {
+                    updateDocumentDataFromField(
+                        $actor,
+                        target.name,
+                        target.checked
+                    );
+                }}
+            />
         </div>
-      </FormSection>
-    {/if}
-  {/each}
-  <FormSection heading="A5E.MovementHover" inline={true}>
-    <div class="u-w-20">
-      <input
-        class="u-pointer"
-        type="checkbox"
-        name="system.attributes.movement.traits.hover"
-        checked={$actor.system.attributes.movement.traits.hover}
-        on:change={({ target }) => {
-          updateDocumentDataFromField($actor, target.name, target.checked);
-        }}
-      />
-    </div>
-  </FormSection>
+    </FormSection>
 </article>
 
 <style lang="scss">
-  article {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    padding: 0.75rem;
-    gap: 0.5rem;
-    overflow: auto;
-    background: rgba(246, 242, 235, 0.5);
-  }
+    article {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        padding: 0.75rem;
+        gap: 0.5rem;
+        overflow: auto;
+        background: rgba(246, 242, 235, 0.5);
+    }
 </style>
