71a070ca2094ac8dbe24891130818f4ff6714933
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-02 10:59:32 -0800

    Adds missing handler functionality to death save overlay. This still needs a tint overlay on the image.

--- src/apps/components/actorSidebar/DeathSaveOverlay.svelte
+++ src/apps/components/actorSidebar/DeathSaveOverlay.svelte
@@ -1,6 +1,9 @@
 <script>
     import { getContext } from "svelte";
-    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+
+    import pressedKeysStore from "../../../stores/pressedKeysStore";
+
+    import getKeyPressAsOptions from "../../handlers/getKeyPressAsOptions";
     import updateDocumentDataFromField from "../../utils/updateDocumentDataFromField";
 
     const actor = getContext("actor");
@@ -9,7 +12,17 @@
 </script>
 
 <div class="death-saves">
-    <i class="fas fa-check" />
+    <button
+        class="death-saves__button"
+        on:click={() =>
+            updateDocumentDataFromField(
+                $actor,
+                "system.attributes.death.success",
+                death.success + 1
+            )}
+    >
+        <i class="fas fa-check" />
+    </button>
 
     <input
         class="death-saves__input"
@@ -17,6 +30,8 @@
         name="system.attributes.death.success"
         placeholder="0"
         min="0"
+        data-tooltip="A5E.DeathSuccess"
+        data-tooltip-direction="UP"
         value={death.success}
         on:change={({ target }) =>
             updateDocumentDataFromField(
@@ -29,15 +44,16 @@
     <div
         class="death-saves__icon u-align-center u-flex u-flex-col u-pos-relative"
     >
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <i
-            class="fas fa-skull a5e-js-roll-death-saving-throw"
-            on:click={$actor.rollDeathSavingThrow()}
-        />
-    </div>
-
-    <div class="a5e-tooltip a5e-tooltip--death-save">
-        {localize("A5E.DeathSavingThrowRoll")}
+        <button
+            class="death-saves__button"
+            data-tooltip="A5E.DeathSavingThrowRoll"
+            data-tooltip-direction="UP"
+            on:click={$actor.rollDeathSavingThrow(
+                getKeyPressAsOptions($pressedKeysStore)
+            )}
+        >
+            <i class="fas fa-skull a5e-js-roll-death-saving-throw" />
+        </button>
     </div>
 
     <input
@@ -47,6 +63,8 @@
         data-dtype="Number"
         placeholder="0"
         min="0"
+        data-tooltip="A5E.DeathFailure"
+        data-tooltip-direction="UP"
         value={death.failure}
         on:change={({ target }) =>
             updateDocumentDataFromField(
@@ -56,7 +74,17 @@
             )}
     />
 
-    <i class="fas fa-times" />
+    <button
+        class="death-saves__button"
+        on:click={() =>
+            updateDocumentDataFromField(
+                $actor,
+                "system.attributes.death.failure",
+                death.failure + 1
+            )}
+    >
+        <i class="fas fa-times" />
+    </button>
 </div>
 
 <style lang="scss">
@@ -71,10 +99,8 @@
         transform: translate(-50%, -50%);
         z-index: 5;
 
-        width: min-content;
-        aspect-ratio: 1;
+        border-radius: 5px;
         padding: 0.5rem;
-        border-radius: 100%;
 
         background: rgba(139, 37, 37, 0.95);
         color: rgba(255 255 255 / 0.75);
@@ -87,9 +113,17 @@
             &:hover {
                 color: #fff;
             }
+        }
 
-            &:hover > .a5e-tooltip {
-                display: block;
+        &__button {
+            padding: 0;
+            margin: 0;
+            background: transparent;
+            color: inherit;
+
+            &:hover,
+            &:focus {
+                box-shadow: none;
             }
         }
 
