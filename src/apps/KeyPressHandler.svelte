e5b49f861cdee55b3ea92829161a6af08c8c3f5d
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-02 09:06:51 -0800

    Adds resetKeys to reset pressedKeys on document visibility change.

--- src/apps/KeyPressHandler.svelte
+++ src/apps/KeyPressHandler.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { onDestroy } from "svelte";
+    import { onMount, onDestroy } from "svelte";
 
     import keyBindingStore from "../stores/pressedKeysStore";
 
@@ -18,10 +18,19 @@
         keyBindingStore.update(() => ({ ...pressedKeys }));
     }
 
+    function resetKeys() {
+        Object.keys(pressedKeys).forEach((key) => (pressedKeys[key] = false));
+        keyBindingStore.update(() => ({ ...pressedKeys }));
+    }
+
+    onMount(() => {
+        document.onvisibilitychange = () => resetKeys();
+    });
+
     onDestroy(unsubscribe);
 </script>
 
-<svelte:body
+<svelte:window
     on:keydown={(event) => logKey(event, true)}
     on:keyup={(event) => logKey(event, false)}
 />
