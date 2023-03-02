b9eb994dd948327c3875896bccc53fb2cc23c7a1
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-01 16:55:27 -0800

    Changes the secret description editor to the generic editor

--- src/apps/components/pages/ItemGmNotesTab.svelte
+++ src/apps/components/pages/ItemGmNotesTab.svelte
@@ -1,36 +1,10 @@
 <script>
     import { getContext } from "svelte";
-    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
-    import { TJSTinyMCE } from "@typhonjs-fvtt/svelte-standard/component";
+
+    import Editor from "../Editor.svelte";
 
     const item = getContext("item");
-
-    function updateSecretDescription(event) {
-        const { content } = event.detail;
-
-        $item.update({
-            "system.secretDescription": content === "<p></p>" ? "" : content,
-        });
-    }
+    $: content = $item.system.secretDescription;
 </script>
 
-<div class="editor">
-    <!-- svelte-ignore missing-declaration (TextEditor) -->
-    <TJSTinyMCE
-        content={$item.system.secretDescription ||
-            localize("A5E.NoSecretDescription")}
-        enrichedContent={TextEditor.enrichHTML($item.system.secretDescription, {
-            async: false,
-        })}
-        on:editor:save={(event) => updateSecretDescription(event)}
-    />
-</div>
-
-<style lang="scss">
-    .editor {
-        height: 100%;
-
-        // Nudges the edit icon down 1px. Removing this hides the top border for the button.
-        --tjs-editor-edit-top: 1px;
-    }
-</style>
+<Editor document={item} {content} updatePath="system.secretDescription" />
