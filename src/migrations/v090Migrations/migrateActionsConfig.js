1fb6134f5a2364d94fcf8ab291e478811ac6e502
JoshLaunchcode <holo.sf0@gmail.com>
2023-03-05 21:18:46 -0600

    updates migrateMovement to check if distance data is a number and act accordingly if so, makes same updates to migrateSenses

--- src/migrations/v090Migrations/migrateActionsConfig.js
+++ src/migrations/v090Migrations/migrateActionsConfig.js
@@ -1,7 +1,7 @@
 import migrate5ftRange from './migrate5ftRange';
 import migrateItemWeight from './migrateItemWeight';
-import migrateMovements from './migrateMovements';
 import migrateSenses from './migrateSenses';
+import migrateMovements from './migrateMovements';
 
 // FIXME: Remove
 window.testMigrate = {
--- src/migrations/v090Migrations/migrateMovements.js
+++ src/migrations/v090Migrations/migrateMovements.js
@@ -7,17 +7,21 @@ export default function migrateMovements(actorData, updateData) {
   };
 
   const newMovements = Object.entries(old).reduce((acc, [mode, distance]) => {
+    if (mode === 'traits') {
+      acc[mode] = { hover: distance.hover };
+      return acc;
+    }
     // eslint-disable-next-line no-restricted-syntax
     for (const [unit, strings] of Object.entries(unitMap)) {
-      if (strings.some((el) => distance.includes(el))) {
-        acc[mode] = { distance: (Number(parseInt(distance, 10))), unit };
+      if (Number.isNaN(distance) && strings.some((el) => distance.includes(el))) {
+        acc[mode] = { distance: (Number(parseInt(distance, 10)) || 0), unit };
         return acc;
       }
     }
-
+    acc[mode] = { distance: (Number(parseInt(distance, 10)) || 0), unit: 'feet' };
     return acc;
   }, {});
-  
+
   updateData['system.attributes.movement'] = newMovements;
 
   return updateData;
