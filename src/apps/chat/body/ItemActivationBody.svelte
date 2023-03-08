922c87401ec4dfaf9afbd3c4ac9fcab82f128e1b
Phil <me@phillip.best>
2023-03-08 01:40:00 +0000

    Adds ability check and skill check prompt buttons to the item activation cards.

--- src/apps/chat/body/ItemActivationBody.svelte
+++ src/apps/chat/body/ItemActivationBody.svelte
@@ -3,13 +3,21 @@
 
     export let message;
 
-    const { abilities } = CONFIG.A5E;
+    const { abilities, skills } = CONFIG.A5E;
 
     $: description = $message.flags?.a5e?.description;
     $: prompts = $message.flags?.a5e?.prompts;
 
+    $: abilityCheckPrompts = prompts?.filter(
+        ([_, prompt]) => prompt.type === "abilityCheck"
+    );
+
     $: savingThrowPrompts = prompts?.filter(
         ([_, prompt]) => prompt.type === "savingThrow"
+    );
+
+    $: skillCheckPrompts = prompts?.filter(
+        ([_, prompt]) => prompt.type === "skillCheck"
     );
 </script>
 
@@ -28,6 +36,34 @@
                         {localize("A5E.RollPromptSavingThrow", {
                             ability: localize(abilities[prompt.ability]),
                             dc: prompt.dc,
+                        })}
+                    </p>
+                </button>
+            {/each}
+        </section>
+    {/if}
+
+    {#if abilityCheckPrompts}
+        <section class="prompt-button-wrapper">
+            {#each abilityCheckPrompts as [key, prompt]}
+                <button>
+                    <p>
+                        {localize("A5E.AbilityCheckPrompt", {
+                            ability: localize(abilities[prompt.ability]),
+                        })}
+                    </p>
+                </button>
+            {/each}
+        </section>
+    {/if}
+
+    {#if skillCheckPrompts}
+        <section class="prompt-button-wrapper">
+            {#each skillCheckPrompts as [key, prompt]}
+                <button>
+                    <p>
+                        {localize("A5E.SkillCheckPrompt", {
+                            skill: localize(skills[prompt.skill]),
                         })}
                     </p>
                 </button>
