1c332d5de5170aaa4a2a1be18fb3adb9cd562790
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-08 13:15:59 -0800

    Adds basic styling for the GenericRollPromptButton

---
+++ src/apps/chat/body/promptButtons/GenericRollPromptButton.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+
+    import prepareSelectedTokenActors from "../../../dataPreparationHelpers/prepareSelectedTokenActors";
+
+    export let key;
+    export let prompt;
+
+    const { skills } = CONFIG.A5E;
+
+    function rollPrompt() {
+        const tokenActors = prepareSelectedTokenActors();
+
+        tokenActors.forEach((t) => {});
+    }
+</script>
+
+<button on:click={() => rollPrompt()}>
+    {localize("A5E.GenericRollPrompt", {
+        label: prompt.label,
+    })}
+</button>
