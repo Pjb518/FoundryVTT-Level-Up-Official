7ec0c0685d7dfff6d7161320f48460e45b404d66
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-08 12:06:39 -0800

    Adds handler to savingThrowPromptButton

--- src/apps/chat/body/promptButtons/SavingThrowPromptButton.svelte
+++ src/apps/chat/body/promptButtons/SavingThrowPromptButton.svelte
@@ -1,13 +1,23 @@
 <script>
     import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
 
+    import prepareSelectedTokenActors from "../../../dataPreparationHelpers/prepareSelectedTokenActors";
+
     export let key;
     export let prompt;
 
     const { abilities } = CONFIG.A5E;
+
+    function rollPrompt() {
+        const tokenActors = prepareSelectedTokenActors();
+
+        tokenActors.forEach((t) => {
+            t.rollSavingThrow(prompt.ability, { dc: prompt.dc });
+        });
+    }
 </script>
 
-<button>
+<button on:click={() => rollPrompt()}>
     {localize("A5E.RollPromptSavingThrow", {
         ability: localize(abilities[prompt.ability]),
         dc: prompt.dc,
