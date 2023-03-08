ecf1d80ac2dd776f1816748d58bf63019e9b11aa
Phil <me@phillip.best>
2023-03-08 17:20:58 +0000

    Adds logic for the tool check preparation helper.

--- src/apps/dataPreparationHelpers/prepareToolChecks.js
+++ src/apps/dataPreparationHelpers/prepareToolChecks.js
@@ -1,3 +1,23 @@
-export default function prepareToolChecks() {
-  return [];
+export default function prepareToolChecks(rolls) {
+  const counts = {};
+
+  // Flatten the tools config object and extract just the inner tools keys
+  const tools = Object.values(CONFIG.A5E.tools).reduce((acc, curr) => ({ ...acc, ...curr }), {});
+
+  if (!rolls.length) return [];
+
+  return rolls.map(([key, roll]) => {
+    if (roll.label) return [key, roll.label];
+
+    roll.tool ??= 'airVehicles';
+
+    const label = game.i18n.format('A5E.ToolCheckSpecific', {
+      tool: game.i18n.localize(tools[roll.tool])
+    });
+
+    counts[roll.tool] ??= 0;
+    counts[roll.tool] += 1;
+
+    return [key, `${label} #${counts[roll.tool]}`];
+  });
 }
