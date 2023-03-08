bd4a2a1bce036b2cc320c4549f230da8d727a7a0
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-08 11:53:14 -0800

    Adds handler for Ability Check Prompt

--- src/apps/chat/body/promptButtons/AbilityCheckPromptButton.svelte
+++ src/apps/chat/body/promptButtons/AbilityCheckPromptButton.svelte
@@ -1,13 +1,24 @@
 <script>
     import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
 
+    import prepareSelectedTokenActors from "../../../dataPreparationHelpers/prepareSelectedTokenActors";
+
     export let key;
     export let prompt;
 
     const { abilities } = CONFIG.A5E;
+
+    function rollPrompt() {
+        const tokenActors = prepareSelectedTokenActors();
+
+        // Roll Ability Check
+        tokenActors.forEach((a) => {
+            a.rollAbilityCheck(prompt.ability);
+        });
+    }
 </script>
 
-<button>
+<button on:click={() => rollPrompt()}>
     {localize("A5E.AbilityCheckPrompt", {
         ability: localize(abilities[prompt.ability]),
     })}
