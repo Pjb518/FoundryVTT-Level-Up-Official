28cca6051138a53a60a05245c9d4f1f8b11676f4
Phil <me@phillip.best>
2023-03-08 16:44:42 +0000

    Adds logic to the prepareHealingRolls helper.

--- src/apps/dataPreparationHelpers/prepareHealingRolls.js
+++ src/apps/dataPreparationHelpers/prepareHealingRolls.js
@@ -1,3 +1,18 @@
-export default function prepareHealingRolls() {
-  return [];
+export default function prepareHealingRolls(rolls) {
+  const counts = {};
+
+  if (!rolls.length) return [];
+
+  return rolls.map(([key, roll]) => {
+    if (roll.label) return [key, roll.label];
+
+    roll.healingType ??= 'healing';
+
+    const label = game.i18n.localize(CONFIG.A5E.healingTypes[roll.healingType] ?? '');
+
+    counts[roll.healingType] ??= 0;
+    counts[roll.healingType] += 1;
+
+    return [key, `${label} #${counts[roll.healingType]}`];
+  });
 }
