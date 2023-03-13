1fac120f55abeb0e285c6c0e73053f2ad8901f85
Phil <me@phillip.best>
2023-03-13 15:50:28 +0000

    Removes the requirement for template quantity to be explicitly set in the item data to pass template validation.

--- src/utils/measuredTemplates/validateTemplateData.js
+++ src/utils/measuredTemplates/validateTemplateData.js
@@ -6,22 +6,25 @@
  */
 export default function validateTemplateData(item, actionId) {
   const { area } = item.actions[actionId];
+  area.quantity ??= 1;
 
   if (!area?.shape) return false;
-  if (!area.quantity || area.quantity <= 0) return false;
+  if (area.quantity <= 0) return false;
 
   if (area.shape === 'cone') {
-    const length = Number(area?.length);
+    const length = parseInt(area?.length, 10);
     if (!length || length <= 0) return false;
   } else if (area.shape === 'cube') {
-    const size = Number(area?.width);
+    const size = parseInt(area?.width, 10);
     if (!size || size <= 0) return false;
   } else if (area.shape === 'cylinder' || area.shape === 'sphere') {
-    const radius = Number(area?.radius);
-    if (!radius || radius <= 0) return false;
+    const radius = parseInt(area?.radius, 10);
+    if (!radius || radius <= 0) {
+      return false;
+    }
   } else if (area.shape === 'line') {
-    const length = Number(area?.length);
-    const width = Number(area?.width);
+    const length = parseInt(area?.length, 10);
+    const width = parseInt(area?.width, 10);
 
     if (!length || !width) return false;
     if (length <= 0 || width <= 0) return false;
