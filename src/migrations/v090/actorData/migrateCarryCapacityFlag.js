56337f4b2dd1f2c6ee6e4e3ee8b915eaf049acdc
Phil <me@phillip.best>
2023-03-16 08:06:38 +0000

    Adds a fix for migrate carry capacity.

--- src/migrations/v090/actorData/migrateCarryCapacityFlag.js
+++ src/migrations/v090/actorData/migrateCarryCapacityFlag.js
@@ -2,6 +2,5 @@ export default function migrateCarryCapacityFlag(actorData, updateData) {
   const old = actorData?.flags?.a5e?.carryCapacityMultiplier;
   if (!old) return;
 
-  if (old > 1) updateData.flags.a5e.doubleCarryCapacity = true;
-  else updateData.flags.a5e.doubleCarryCapacity = false;
+  foundry.utils.setProperty(updateData, 'flags.a5e.doubleCarryCapacity', old > 1);
 }
