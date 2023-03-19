24e35fab10a80c5e20e8e63407ed893fe0f018f1
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-19 07:12:56 -0700

    Adds migration for spell attack type fix

---
+++ src/migrations/v090/itemData/migrateSpellAttackFix.js
@@ -0,0 +1,20 @@
+export default function migrateSpellAttack(itemData, updateData) {
+  if (!itemData.type === 'spell') return;
+
+  const actions = itemData.actions.entries();
+  if (!actions.length) return;
+
+  const [actionId, action] = actions;
+
+  // Replace attack type
+
+  const attackRolls = Object.entries(action?.rolls ?? {})
+    .filter(({ type }) => type === 'attack');
+
+  if (!attackRolls.length) return;
+  const [rollId, attackRoll] = attackRolls[0];
+
+  if (attackRoll.attackType) return;
+
+  updateData[`system.actions.${actionId}.rolls.${rollId}.attackType`] = 'rangedSpellAttack';
+}
