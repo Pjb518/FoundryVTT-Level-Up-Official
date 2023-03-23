45503b365b23ded24c0a8e198c6622598d2c20a1
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-23 12:59:18 -0700

    WIP - Refactor of the spell selection component

--- src/apps/components/activationDialog/SpellSection.svelte
+++ src/apps/components/activationDialog/SpellSection.svelte
@@ -3,8 +3,8 @@
 
     import FormSection from "../FormSection.svelte";
     import RadioGroup from "../RadioGroup.svelte";
+    import Tag from "../Tag.svelte";
 
-    export let actionId;
     export let consumers;
     export let item;
     export let spellData;
@@ -12,22 +12,85 @@
     let consumer = Object.values(consumers.spell ?? {})[0][1];
     let mode = consumer.mode;
 
+    const spellLevels = Object.entries(CONFIG.A5E.spellLevels).slice(1);
+    const consumeOptions = {
+        consumeSlot: "Consume Slot",
+        consumePoint: "Consume Points",
+        neither: "Don't Consume",
+    };
+
     $: availablePoints = $item.actor.system.spellResources.points.current;
+
     let availableSlots = Object.entries(
         $item.actor.system.spellResources.slots
     ).reduce((acc, [level, slot]) => {
-        if (slot.max > 0 && slot.current > 0)
-            acc.push([level, CONFIG.A5E.spellLevels[level]]);
+        if (slot.max > 0 && slot.current > 0) acc.push(level);
 
         return acc;
     }, []);
 
+    spellData.consumeSelection =
+        mode === "pointsOnly"
+            ? "consumePoint"
+            : availableSlots.length > 0
+            ? "consumeSlot"
+            : "consumePoint";
     spellData.selectedLevel = consumer.spellLevel;
-    spellData.points = consumer.points ?? 0;
+    spellData.points =
+        CONFIG.A5E.spellLevelCost[spellData.selectedLevel] ?? consumer.points;
     spellData.consumeSpellSlot = !availableSlots.length ? false : true;
     spellData.consumeSpellPoint =
         availablePoints > 0 && !consumeSpellSlot ? true : false;
 </script>
+
+{#if availableSlots.length > 0 || availablePoints > 0}
+    {#if ["variable", "slotsOnly"].includes(mode)}
+        <FormSection>
+            <section>
+                <h3 class="u-text-bold u-text-sm">
+                    {localize("A5E.SpellLevel")}
+                </h3>
+
+                <RadioGroup
+                    options={Object.entries(consumeOptions)}
+                    selected={spellData.consumeSelection}
+                    on:updateSelection={({ detail }) =>
+                        (spellData.consumeSelection = detail)}
+                />
+
+                <ul
+                    class=" u-flex u-flex-wrap u-gap-sm u-list-style-none u-m-0 u-p-0 u-text-xs u-w-full"
+                >
+                    {#each spellLevels as [value, label]}
+                        <Tag
+                            active={spellData.selectedLevel === value ||
+                                spellData.selectedLevel?.toString() === value}
+                            {value}
+                            {label}
+                            disabled={spellData.consumeSelection ===
+                                "consumeSlot" &&
+                                !availableSlots.includes(value)}
+                            on:tagToggle={({ detail }) =>
+                                (spellData.selectedLevel = Number(detail))}
+                        />
+                    {/each}
+                </ul>
+
+                {spellData.points}
+                {#if spellData.consumeSelection === "consumePoint" && spellData.points > availablePoints}
+                    <p class="hint" style="color: #8b6225;">
+                        <i class="fa-solid fa-circle-exclamation" />
+                        Insufficient spell points.
+                    </p>
+                {/if}
+            </section>
+        </FormSection>
+    {/if}
+
+    {#if mode === "pointsOnly"}
+        <!--  -->
+    {/if}
+{/if}
 
 <!-- {#if ["variable", "slotsOnly"].includes(mode)}
     <FormSection>
