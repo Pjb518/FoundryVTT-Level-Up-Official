8055576d55056b25e6651ed6cba1568f75c27af1
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-03-26 16:38:59 -0700

    Fixes locksheet toggle requiring double click on new actor

--- src/apps/handlers/toggleSheetLockedState.js
+++ src/apps/handlers/toggleSheetLockedState.js
@@ -1,4 +1,4 @@
 export default function toggleSheetLockedState(actor) {
-  const currentLockState = actor.getFlag('a5e', 'sheetIsLocked') ?? false;
+  const currentLockState = actor.getFlag('a5e', 'sheetIsLocked') ?? true;
   actor.update({ 'flags.a5e.sheetIsLocked': !currentLockState });
 }
