f222165911dac70b4b86f7a4fcf1819e97d7aaf8
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-04-09 22:17:30 -0700

    Changes method of aggregate export

--- src/migration/migrations/index.js
+++ src/migration/migrations/index.js
@@ -1,3 +1,3 @@
 /* eslint-disable import/export */
-export * from './001-migrate-to-aboleth';
-export * from './002-migrate-to-ooze';
+export { default as Migration001Aboleth } from './001-migrate-to-aboleth';
+export { default as Migration002Ooze } from './002-migrate-to-ooze';
