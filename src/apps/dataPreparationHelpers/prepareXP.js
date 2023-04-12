93ea76887a00ae90305a10a51578b3b20e03049a
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-04-11 18:50:28 -0700

    Fixes xp not being correct for creatures below cr 1

--- src/apps/dataPreparationHelpers/prepareXP.js
+++ src/apps/dataPreparationHelpers/prepareXP.js
@@ -1,6 +1,10 @@
 export default function prepareXP(data) {
   const cr = parseFloat(data.system.details.cr || 0);
-  const baseXp = CONFIG.A5E.CR_EXP_LEVELS[parseInt(cr, 10) > 30 ? 30 : cr];
+  let baseXp = 0;
+  if (cr === 0.125) baseXp = CONFIG.A5E.CR_EXP_LEVELS['1/8'];
+  else if (cr === 0.25) baseXp = CONFIG.A5E.CR_EXP_LEVELS['1/4'];
+  else if (cr === 0.5) baseXp = CONFIG.A5E.CR_EXP_LEVELS['1/2'];
+  else baseXp = CONFIG.A5E.CR_EXP_LEVELS[parseInt(cr, 10) > 30 ? 30 : cr];
 
   return data.system.details.elite ? baseXp * 2 : baseXp;
 }
