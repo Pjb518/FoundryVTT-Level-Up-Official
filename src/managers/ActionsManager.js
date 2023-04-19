4ebbb7f5da248a3a18a90b5a7dade9db3ff94921
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-04-19 05:51:31 -0700

    Adds static methods for creating rolls, prompts, and consumers.

--- src/managers/ActionsManager.js
+++ src/managers/ActionsManager.js
@@ -135,7 +135,7 @@ export default class ActionsManager extends DataProxy {
    * ************************************************ */
   /**
    *
-   * @param {ItemA5e} item
+   * @param {ItemA5E} item
    * @param {Object} data
    * @param {Boolean} update
    * @returns {Action}
@@ -166,6 +166,143 @@ export default class ActionsManager extends DataProxy {
     };
 
     if (update) await item.update(updateData);
+    return updateData;
+  }
+
+  /**
+   *
+   * @param {ItemA5E} item
+   * @param {Array<String, Action>} action
+   * @param {String} type
+   * @param {Object} data
+   * @param {Boolean} update
+   * @returns {Object}
+   */
+  static async addRoll(item, action, type, data = {}, update = true) {
+    const defaultData = {
+      type,
+      default: true
+    };
+
+    if (type === 'attack') {
+      // TODO: Throw error on multiple attack rolls
+      defaultData.attackType = 'meleeWeaponAttack';
+      defaultData.proficient = true;
+    }
+
+    if (type === 'abilityCheck' || type === 'savingThrow') {
+      defaultData.ability = 'str';
+    }
+
+    if (type === 'skillCheck') {
+      defaultData.skill = 'acr';
+      defaultData.ability = 'none';
+    }
+
+    if (type === 'toolCheck') {
+      defaultData.tool = 'airVehicles';
+      defaultData.ability = 'none';
+    }
+
+    const updateData = {
+      [`system.actions.${action[0]}.rolls`]: {
+        ...action[1].rolls,
+        [foundry.utils.randomID()]: foundry.utils.mergeObject(defaultData, data)
+      }
+    };
+
+    if (update) item.update(updateData);
+    return updateData;
+  }
+
+  /**
+   *
+   * @param {ItemA5E} item
+   * @param {Array<String, Action>} action
+   * @param {String} type
+   * @param {Object} data
+   * @param {Boolean} update
+   * @returns {Object}
+   */
+  static async addPrompt(item, action, type, data = {}, update = true) {
+    const defaultData = {
+      type,
+      default: true
+    };
+
+    if (type === 'savingThrow' || type === 'abilityCheck') {
+      defaultData.ability = 'str';
+    }
+
+    if (type === 'savingThrow') {
+      defaultData.saveDC = { type: 'spellcasting' };
+    }
+
+    if (type === 'skillCheck') {
+      defaultData.skill = 'acr';
+    }
+
+    const updateData = {
+      [`system.actions.${action[0]}.prompts`]: {
+        ...action[1].prompts,
+        [foundry.utils.randomID()]: foundry.utils.mergeObject(defaultData, data)
+      }
+    };
+
+    if (update) item.update(updateData);
+    return updateData;
+  }
+
+  /**
+   *
+   * @param {ItemA5E} item
+   * @param {Array<String, Action>} action
+   * @param {String} type
+   * @param {Object} data
+   * @param {Boolean} update
+   * @returns {Object}
+   */
+  static async addConsumer(item, action, type, data = {}, update = true) {
+    const defaultData = {
+      type,
+      default: true
+    };
+
+    if (type === 'ammunition') {
+      defaultData.itemId = '';
+      defaultData.quantity = 1;
+    }
+
+    if (type === 'hitDice') {
+      defaultData.quantity = 1;
+    }
+
+    if (type === 'quantity') {
+      defaultData.itemId = '';
+      defaultData.quantity = 1;
+    }
+
+    if (type === 'resource') {
+      defaultData.resource = '';
+      defaultData.quantity = 1;
+    }
+
+    if (type === 'spell') {
+      defaultData.mode = 'variable';
+      defaultData.spellLevel = item.system.level ?? 1;
+      defaultData.points = CONFIG.A5E.spellLevelCost?.[item.system.level] ?? 1;
+    }
+
+    if (['actionUses', 'itemUses'].includes(type)) data.quantity = 1;
+
+    const updateData = {
+      [`system.actions.${action[0]}.consumers`]: {
+        ...action[1].consumers,
+        [foundry.utils.randomID()]: foundry.utils.mergeObject(defaultData, data)
+      }
+    };
+
+    if (update) item.update(updateData);
     return updateData;
   }
 }
