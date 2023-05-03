7e7c85c09d7d2e4ae899b364e16d8f30b095cc1c
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-05-03 04:42:42 -0700

    Adds a tab for editting effects related effects.

---
+++ src/apps/settings/SettingsEffectsTab.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { getContext, setContext } from "svelte";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
+
+    import CheckboxGroup from "../components/CheckboxGroup.svelte";
+    import FormSection from "../components/FormSection.svelte";
+
+    const appId = getContext("appId");
+
+    // Conditions Automation
+    const conditions = [
+        "blinded",
+        "bloodied",
+        "encumbered",
+        // 'fatigue',
+        // 'frightened',
+        "grappled",
+        "invisible",
+        "paralyzed",
+        "petrified",
+        "poisoned",
+        "prone",
+        "rattled",
+        "restrained",
+        "slowed",
+        // 'strife',
+        "stunned",
+        "unconscious",
+    ].map((c) => [c, localize(`A5E.Condition${c.capitalize()}`)]);
+
+    $: selectedConditions = game.settings.get();
+</script>
+
+<!-- Condition Automation -->
+<FormSection heading="A5E.settings.automateConditions">
+    <CheckboxGroup options={conditions} />
+</FormSection>
---
+++ src/apps/settings/SystemSettings.svelte
@@ -0,0 +1,48 @@
+<svelte:options accessors={true} />
+
+<script>
+    import { getContext, setContext } from "svelte";
+    import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
+
+    import NavigationBar from "../components/navigation/NavigationBar.svelte";
+    import SettingsEffectsTab from "./SettingsEffectsTab.svelte";
+
+    export let { appId } = getContext("#external").application;
+    export let elementRoot;
+
+    function updateCurrentTab(event) {
+        currentTab = tabs[event.detail];
+    }
+
+    const tabs = [
+        {
+            name: "effects",
+            label: "A5E.settings.navigation.activeEffects",
+            component: SettingsEffectsTab,
+        },
+    ];
+
+    let currentTab = tabs[0];
+
+    setContext("appId", appId);
+</script>
+
+<ApplicationShell bind:elementRoot>
+    <main>
+        <NavigationBar {currentTab} {tabs} on:tab-change={updateCurrentTab} />
+
+        <svelte:component this={currentTab.component} />
+    </main>
+</ApplicationShell>
+
+<style lang="scss">
+    main {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        padding: 0.75rem;
+        gap: 0.5rem;
+
+        background: rgba(246, 242, 235, 0.5);
+    }
+</style>
