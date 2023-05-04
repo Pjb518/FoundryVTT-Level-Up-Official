ff4f5c844cc232d0cc5f29051b4780e85311d1ae
Phil <me@phillip.best>
2023-05-04 04:21:08 +0100

    Adds weight tracking for supply over your free limit.

--- src/apps/handlers/calculateInventoryWeight.js
+++ src/apps/handlers/calculateInventoryWeight.js
@@ -1,5 +1,6 @@
-export default function calculateInventoryWeight(actorData) {
-  const equippedItems = actorData.items.filter((item) => item.system.equipped);
+export default function calculateInventoryWeight(actor) {
+  const actorData = actor.system;
+  const equippedItems = actor.items.filter((item) => item.system.equipped);
 
   const totalItemWeight = equippedItems.reduce((acc, curr) => {
     const { quantity } = curr.system;
@@ -8,12 +9,18 @@ export default function calculateInventoryWeight(actorData) {
     return acc + (quantity ? itemWeight * quantity : itemWeight);
   }, 0);
 
-  const coinWeight = Object.values(actorData.system.currency).reduce(
+  const coinWeight = Object.values(actorData.currency).reduce(
     (acc, curr) => acc + Number(curr),
     0
   );
 
-  const trackCurrencyWeight = actorData.flags?.a5e?.trackCurrencyWeight ?? game.settings.get('a5e', 'currencyWeight');
+  const excessSupplyWeight = 2 * Math.abs(
+    Math.min(actorData.abilities.str.value - actorData.supply, 0)
+  );
 
-  return trackCurrencyWeight ? totalItemWeight + (coinWeight * 0.02) : totalItemWeight;
+  const trackCurrencyWeight = actor.flags?.a5e?.trackCurrencyWeight ?? game.settings.get('a5e', 'currencyWeight');
+
+  return trackCurrencyWeight
+    ? totalItemWeight + excessSupplyWeight + (coinWeight * 0.02)
+    : totalItemWeight + excessSupplyWeight;
 }
