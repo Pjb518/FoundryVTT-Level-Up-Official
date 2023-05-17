904d4915c976e039572804da72d9009aa2f3b13a
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-05-17 03:53:03 -0700

    Moves init hook to its own file

--- src/index.js
+++ src/index.js
@@ -2,148 +2,21 @@
 
 import './scss/main.scss';
 
-import ActorSheetA5e from './apps/ActorSheet';
-import ActiveEffectConfigA5e from './apps/ActiveEffectConfig';
-import ItemSheetA5e from './apps/ItemSheet';
-
 import A5eChatCard from './apps/chat/ChatCard.svelte';
 
-import A5E from './config';
-import ActiveEffectA5e from './documents/activeEffect';
-import ActorA5e from './documents/actor';
-import D20Roll from './dice/d20Roll';
-import ItemA5e from './documents/item';
-import TokenA5e from './documents/token';
-import TokenDocumentA5e from './documents/tokenDocument';
 import TokenHUDA5e from './documents/tokenHUD';
 
-import EffectOptions from './activeEffects/EffectOptions';
-
-import _onCombatControl from './combat/_onCombatControl';
-import _onCombatantControl from './combat/_onCombatantControl';
-import getInitiativeFormula from './combat/getInitiativeFormula';
-import getInitiativeRoll from './combat/getInitiativeRoll';
 import measureDistances from './pixi/measureDistances';
-import preloadHandlebarsTemplates from './templates';
-import performPreLocalization from './utils/localization/performLocalization';
 import registerSystemSettings from './settings';
-import rollCombatantInitiative from './combat/rollCombatantInitiative';
-import rollInitiative from './combat/rollInitiative';
 
 import createActor from './hooks/createActor';
 import createToken from './hooks/createToken';
+import init from './hooks/init';
 import ready from './hooks/ready';
 
 import setupConditions from './activeEffects/conditions';
 
-// Macros
-import activateActionMacro from './macros/activateActionMacro';
-import activateItemMacro from './macros/activateItemMacro';
-import createMacro from './macros/createMacro';
-
-// Managers
-import ActionsManager from './managers/ActionsManager';
-import ResourceConsumptionManager from './managers/ResourceConsumptionManager';
-import RestManager from './managers/RestManager';
-import TemplatePreparationManager from './managers/TemplatePreparationManager';
-
-// Migrations
-import MigrationList from './migration/MigrationList';
-import MigrationRunner from './migration/MigrationRunner';
-import handleMigration from './migration/handleMigration';
-
-Hooks.once('init', () => {
-  game.a5e = {
-    applications: {
-      ActorSheetA5e,
-      ItemSheetA5e
-    },
-    activeEffects: {
-      EffectOptions
-    },
-    config: A5E,
-    dice: {
-      D20Roll
-    },
-    entities: {
-      ActorA5e,
-      ItemA5e,
-      TokenDocumentA5e,
-      TokenA5e
-    },
-    macros: {
-      activateActionMacro,
-      activateItemMacro,
-      createMacro
-    },
-    managers: {
-      ActionsManager,
-      ResourceConsumptionManager,
-      RestManager,
-      TemplatePreparationManager
-    },
-    migrations: {
-      MigrationList,
-      MigrationRunner,
-      handleMigration
-    }
-  };
-
-  CONFIG.A5E = A5E;
-  CONFIG.ActiveEffect.documentClass = ActiveEffectA5e;
-  CONFIG.Actor.documentClass = ActorA5e;
-  CONFIG.Item.documentClass = ItemA5e;
-  CONFIG.Token.documentClass = TokenDocumentA5e;
-  CONFIG.Token.objectClass = TokenA5e;
-
-  CONFIG.Dice.D20Roll = D20Roll;
-
-  CONFIG.Dice.rolls.push(D20Roll);
-
-  CONFIG.MeasuredTemplate.defaults.angle = 60;
-
-  Actors.unregisterSheet('core', ActorSheet);
-  Actors.registerSheet('a5e', ActorSheetA5e, {
-    types: ['character'],
-    makeDefault: true,
-    label: 'A5E.SheetClassCharacter'
-  });
-
-  Actors.registerSheet('a5e', ActorSheetA5e, {
-    types: ['npc'],
-    makeDefault: true,
-    label: 'A5E.SheetClassNPC'
-  });
-
-  Items.unregisterSheet('core', ItemSheet);
-  Items.registerSheet('a5e', ItemSheetA5e, {
-    makeDefault: true,
-    label: 'A5E.SheetClassItem'
-  });
-
-  DocumentSheetConfig.unregisterSheet(ActiveEffect, 'core', ActiveEffectConfig);
-  DocumentSheetConfig.registerSheet(ActiveEffect, 'a5e', ActiveEffectConfigA5e, {
-    makeDefault: true,
-    label: 'A5E.SheetClassActiveEffectConfig'
-  });
-
-  Combatant.prototype._getInitiativeFormula = getInitiativeFormula; // eslint-disable-line
-  Combatant.prototype.getInitiativeRoll = getInitiativeRoll;
-  Combatant.prototype.rollInitiative = rollCombatantInitiative;
-
-  Combat.prototype.rollInitiative = rollInitiative;
-
-  CombatTracker.prototype._onCombatantControl = _onCombatantControl;
-  CombatTracker.prototype._onCombatControl = _onCombatControl;
-
-  // Prelocalize any static strings once localization files become available.
-  Hooks.once('i18nInit', () => {
-    performPreLocalization(CONFIG.A5E);
-    game.a5e.activeEffects.EffectOptions.createOptions();
-  });
-
-  return preloadHandlebarsTemplates();
-});
+Hooks.once('init', init);
 
 // Once game object is ready initialize anything that requires the game object
 Hooks.once('setup', () => {
@@ -158,10 +31,6 @@ Hooks.on('canvasInit', () => {
   canvas.grid.diagonalRule = game.settings.get('a5e', 'diagonalRule');
   SquareGrid.prototype.measureDistances = measureDistances;
   game.canvas.hud.token = new TokenHUDA5e();
-});
-
-Hooks.on('init', () => {
-  TooltipManager.TOOLTIP_ACTIVATION_MS = 100;
 });
 
 Hooks.on('createActor', createActor);
