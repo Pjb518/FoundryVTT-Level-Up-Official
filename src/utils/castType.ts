9381df437f4537a941e4762ddbb4c1b2ed5dd9fe
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-05-23 00:08:19 -0700

    Adds utils for casting and determining actor field types.

---
+++ src/utils/castType.ts
@@ -0,0 +1,49 @@
+function parseOrString(raw: string): any {
+  try {
+    return JSON.parse(raw);
+  } catch (err) {
+    return raw;
+  }
+}
+
+function castArray(value: any): Array<any> {
+  let delta: Array<any>;
+  try {
+    delta = JSON.parse(value);
+    delta = delta instanceof Array ? delta : [delta];
+  } catch (err) {
+    delta = [value];
+  }
+
+  return delta;
+}
+
+function castSet(value: any): Set<any> {
+  let delta: Set<any>;
+  try {
+    delta = parseOrString(value);
+    delta = delta instanceof Set ? delta : new Set(delta);
+  } catch (err) {
+    delta = new Set([value]);
+  }
+  return delta;
+}
+
+export default function castType(value: any, targetType: any): any {
+  if (targetType === 'Array') return castArray(value);
+  if (targetType === 'Set') return castSet(value);
+
+  // TODO: Case to Object
+
+  if (targetType === 'boolean') return Boolean(parseOrString(value));
+
+  if (targetType === 'number') {
+    // @ts-ignore
+    const delta = Number.fromString(value);
+    return Number.isNaN(delta) ? 0 : delta;
+  }
+
+  if (targetType === 'string') return String(value);
+
+  return parseOrString(value);
+}
---
+++ src/utils/getCorrectedTypeValueFromKey.ts
@@ -0,0 +1,29 @@
+// @ts-nocheck
+import castType from './castType';
+
+export default function getCorrectedTypeValueFromKey(actor, key: string): any {
+  const model = game.model.Actor[actor.type] ?? {};
+  const actorValue = foundry.utils.getProperty(actor, key) ?? null;
+  if (actorValue === null || actorValue === undefined) return null;
+
+  const sampleValue = game.a5e.activeEffects.EffectOptions.options[document.type]
+    ?.allOptionsObj?.[key]?.sampleValue ?? foundry.utils.getProperty(model, key) ?? null;
+
+  if (!sampleValue) return actorValue;
+
+  const currentType = foundry.utils.getType(actorValue);
+  const targetType = foundry.utils.getType(sampleValue);
+
+  if (currentType === targetType) return actorValue;
+
+  // Cast actorValue to the correct type
+  try {
+    return castType(actorValue, targetType);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(
+      `Actor [${actor.id}] | Unable to parse active effect change for ${key}: "${actorValue}"`
+    );
+    return null;
+  }
+}
