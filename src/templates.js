d8a2e6f8804ff9096d8d37521567aac32cf79559
Phil <me@phillip.best>
2023-06-03 18:42:28 +0100

    Removes a bunch of legacy template code that is no longer being supported in 0.12.0.

--- src/templates.js
+++ src/templates.js
@@ -4,20 +4,6 @@
  * @returns {Promise}
  */
 export default async () => loadTemplates([
-  // Actor sheets
-  'systems/a5e/templates/sheet.html',
-  'systems/a5e/templates/limited-actor-sheet.hbs',
-
-  // Chat
-  'systems/a5e/templates/chat/ability-check.hbs',
-  'systems/a5e/templates/chat/item-card.hbs',
-
-  // Roll Tooltip
-  'systems/a5e/templates/chat/roll-tooltip.hbs',
-
-  // Announcements
-  'systems/a5e/templates/announcements/0.9.0.hbs',
-
   // Settings
   'systems/a5e/templates/conditionAutomationMenu.hbs',
 
