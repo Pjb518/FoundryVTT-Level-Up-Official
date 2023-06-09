d38ec9c95ea646fb58ace5fab5169dc9b5b0d7ff
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-06-09 16:27:49 -0700

    WIP - Adds some styling to the effect chat button.

--- src/apps/chat/body/promptButtons/ActiveEffectPromptButton.svelte
+++ src/apps/chat/body/promptButtons/ActiveEffectPromptButton.svelte
@@ -26,6 +26,31 @@
 
 {#if effect}
     <button on:click={() => applyEffect()}>
-        {effect.name}
+        <span>
+            <img class="effect-icon" src={effect.icon} alt="Icon Effect" />
+            {effect.name}
+        </span>
     </button>
 {/if}
+
+<style lang="scss">
+    button {
+        background-color: #425f65;
+        color: #f6f2eb;
+        border-radius: 4px;
+
+        span {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 0.25rem;
+            padding-block: 0.15rem;
+        }
+    }
+
+    .effect-icon {
+        width: 1rem;
+        border: none;
+        aspect-ratio: 1;
+    }
+</style>
