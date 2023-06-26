0ea55a848ebc33ef35b67ace9649ca9b241036e2
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-06-26 07:10:36 -0700

    Adds a button to submit. Adds the ability for settings to persist if settings weren't changed. Adds a warning for system refresh.

--- src/apps/settings/SettingsEffectsTab.svelte
+++ src/apps/settings/SettingsEffectsTab.svelte
@@ -5,8 +5,11 @@
     import CheckboxGroup from "../components/CheckboxGroup.svelte";
     import FormSection from "../components/FormSection.svelte";
 
+    export let reload;
+
     const appId = getContext("appId");
     const settings = getContext("settings");
+    const updates = getContext("updates");
 
     // Conditions Automation
     const conditions = [
@@ -30,13 +33,19 @@
     ].map((c) => [c, localize(`A5E.Condition${c.capitalize()}`)]);
 
     const automatedConditions = settings.getStore("automatedConditions");
+    let selectedConditions =
+        updates.get("automatedConditions") ?? $automatedConditions;
 </script>
 
 <!-- Condition Automation -->
 <FormSection heading="A5E.settings.automateConditions">
     <CheckboxGroup
         options={conditions}
-        selected={$automatedConditions}
-        on:updateSelection={({ detail }) => automatedConditions.set(detail)}
+        selected={selectedConditions}
+        on:updateSelection={({ detail }) => {
+            updates.set("automatedConditions", detail);
+            selectedConditions = detail;
+            reload = true;
+        }}
     />
 </FormSection>
--- src/apps/settings/SystemSettings.svelte
+++ src/apps/settings/SystemSettings.svelte
@@ -3,12 +3,25 @@
 <script>
     import { getContext, setContext } from "svelte";
     import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
+    import { localize } from "@typhonjs-fvtt/runtime/svelte/helper";
 
     import NavigationBar from "../components/navigation/NavigationBar.svelte";
     import SettingsEffectsTab from "./SettingsEffectsTab.svelte";
 
-    export let { appId, settings } = getContext("#external").application;
     export let elementRoot;
+    export let { appId, settings, dialog } =
+        getContext("#external").application;
+
+    function onSubmit() {
+        for (const [key, value] of updates) {
+            const setting = settings.getStore(key);
+            setting.set(value);
+        }
+
+        dialog.submit({
+            reload,
+        });
+    }
 
     function updateCurrentTab(event) {
         currentTab = tabs[event.detail];
@@ -23,16 +36,39 @@
     ];
 
     let currentTab = tabs[0];
+    let updates = new Map();
+    let reload = false;
 
     setContext("settings", settings);
     setContext("appId", appId);
+    setContext("updates", updates);
 </script>
 
 <ApplicationShell bind:elementRoot>
     <main>
-        <NavigationBar {currentTab} {tabs} on:tab-change={updateCurrentTab} />
+        <section>
+            <NavigationBar
+                {currentTab}
+                {tabs}
+                on:tab-change={updateCurrentTab}
+            />
 
-        <svelte:component this={currentTab.component} />
+            <svelte:component this={currentTab.component} bind:reload />
+        </section>
+
+        <button class="submit" on:click|preventDefault={() => onSubmit()}>
+            {#if reload}
+                <i
+                    class="fa-solid fa-circle-exclamation"
+                    style="color: #8b6225"
+                    data-tooltip={localize("A5E.settings.reload")}
+                    data-tooltip-direction="UP"
+                />
+            {:else}
+                <i class="fas fa-save" />
+            {/if}
+            {localize("A5E.SaveSubmit")}
+        </button>
     </main>
 </ApplicationShell>
 
@@ -45,5 +81,11 @@
         gap: 0.5rem;
 
         background: rgba(246, 242, 235, 0.5);
+    }
+
+    .submit {
+        margin-top: auto;
+        border-radius: 4px;
+        cursor: pointer;
     }
 </style>
