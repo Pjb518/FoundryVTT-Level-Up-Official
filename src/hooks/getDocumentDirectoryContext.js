66ab94cc58e9e2c2d131f6240cc901af9b9015d8
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-07-12 22:14:27 -0700

    Adds a check to see if the user is a GM for the getDocumentDirectoryContext

--- src/hooks/getDocumentDirectoryContext.js
+++ src/hooks/getDocumentDirectoryContext.js
@@ -4,6 +4,8 @@ import handleDocumentMigration from "../migration/handleDocumentMigration";
 import handlePackMigration from "../migration/handlePackMigration";
 
 export default function getDocumentDirectoryContext(dialog, html, data, type) {
+  if (!game.user.isGM) return;
+
   const menu = {
     name: localize('A5E.migration.migrateDocument', { type }),
     icon: '<i class="fa-solid fa-crow"></i>',
@@ -12,7 +14,6 @@ export default function getDocumentDirectoryContext(dialog, html, data, type) {
 
   html.push(menu);
 }
-
 
 function callMigration($li, docType) {
   if (docType === 'Pack') {
