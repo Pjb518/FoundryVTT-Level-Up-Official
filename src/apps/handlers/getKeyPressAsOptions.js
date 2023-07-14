f8f1615b410d6be2789312d909a958dd6a161972
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-07-14 06:32:39 -0700

    Adds option to reversr alt behaviour for all rolls.

--- src/apps/handlers/getKeyPressAsOptions.js
+++ src/apps/handlers/getKeyPressAsOptions.js
@@ -1,14 +1,19 @@
 export default function getKeyPressAsOptions(keyPressStore, { reverseAlt = false } = {}) {
   const { Shift, Control, Alt } = keyPressStore;
 
+  const altBehavior = game.settings.get('a5e', 'reverseAltBehavior');
+
   let rollMode = Shift
     ? CONFIG.A5E.ROLL_MODE.ADVANTAGE
     : CONFIG.A5E.ROLL_MODE.NORMAL;
 
   rollMode = Control ? CONFIG.A5E.ROLL_MODE.DISADVANTAGE : rollMode;
 
+  let skipRollDialog = altBehavior ? !Alt : Alt;
+  skipRollDialog = reverseAlt ? !skipRollDialog : skipRollDialog;
+
   return {
-    skipRollDialog: reverseAlt ? !Alt : Alt,
+    skipRollDialog,
     rollMode
   };
 }
