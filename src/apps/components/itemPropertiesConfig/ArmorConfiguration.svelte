300271e0d7dd93585ad0ca4f837cb021a09f2544
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-07-18 15:01:29 -0700

    Updates compendia for 0.006

--- src/apps/components/itemPropertiesConfig/ArmorConfiguration.svelte
+++ src/apps/components/itemPropertiesConfig/ArmorConfiguration.svelte
@@ -23,6 +23,7 @@
 
 <section>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
     <header
         class="
             u-align-center
