89bdfadc8355bee47578035376d287cb10952aa9
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-07-20 04:17:52 -0700

    Updates version ahead of minor release.

--- src/migration/handlePackMigration.js
+++ src/migration/handlePackMigration.js
@@ -10,7 +10,6 @@ export default async function handlePackMigration(pack, fullMigration = false) {
     MigrationList.constructFromVersion(null)
   );
 
-  console.log(migrationRunner);
   await migrationRunner.runCompendiumMigration(pack, fullMigration);
 
   ui.notifications.info(`Migrated ${pack.metadata.label} to the latest version.`, { permanent: true });
