204e9be421df6a4767ac78c5b20aa2e86ce933ee
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-08-17 11:52:19 -0700

    Re-adds template creation hook.

--- src/pixi/ItemMeasuredTemplate.js
+++ src/pixi/ItemMeasuredTemplate.js
@@ -88,6 +88,8 @@ export default class ItemMeasuredTemplate extends MeasuredTemplate {
     this.#events.resolve(
       canvas.scene.createEmbeddedDocuments('MeasuredTemplate', [this.document.toObject()])
     );
+
+    Hooks.callAll('a5e.measuredTemplatePlaced', this.item, this.document.toObject(), game.user.id);
   }
 
   async _onCancel(e) {
