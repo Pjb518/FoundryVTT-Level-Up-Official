30ff12d0949ff2c666f4ed641aa30ff6dbe5270b
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-08-31 01:46:48 -0700

    Removes linter warning.

--- src/apps/components/pages/ActorManueversPage.svelte
+++ src/apps/components/pages/ActorManueversPage.svelte
@@ -114,6 +114,7 @@
                 </h3>
 
                 <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <!-- svelte-ignore a11y-no-static-element-interactions -->
                 <i
                     class="fas fa-gear a5e-config-button"
                     on:click={() => $actor.configureManeuvers()}
