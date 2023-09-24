b21d3660895ea3444002eded5b163f7f87681e92
Phil <me@phillip.best>
2023-09-24 13:05:10 +0100

    Fixes an issue with how we add our partyViewer button to the Foundry interface that was clashing with the Minimal UI module.

--- src/hooks/renderApplication.js
+++ src/hooks/renderApplication.js
@@ -8,17 +8,15 @@ export default function renderApplication(application) {
 
   const sceneControls = document.querySelector('nav#controls .main-controls');
   const button = document.createElement('li');
+  const buttonIcon = document.createElement('i');
 
-  button.innerHTML = `
-    <li
-      id="a5e-js-toggle-party-viewer-visibility"
-      class="scene-control"
-      data-tooltip="Party Viewer"
-    >
-      <i class="fa-solid fa-users"></i>
-    </li>
-  `;
+  buttonIcon.classList.add('fa-solid', 'fa-users');
 
+  button.id = 'a5e-js-toggle-party-viewer-visibility';
+  button.classList.add('scene-control');
+  button.setAttribute('data-tooltip', 'Party Viewer');
   button.addEventListener('click', renderPartyViewer);
+  button.append(buttonIcon);
+
   sceneControls.append(button);
 }
