c76578e25da1923fb1db49714435337291d42aaa
Phil <me@phillip.best>
2023-09-25 00:04:05 +0100

    Fixes an issue with the Party Viewer keybinding registration, where the provided key was just a letter, rather than a proper key code.

--- src/hooks/renderPartyViewer.js
+++ src/hooks/renderPartyViewer.js
@@ -3,7 +3,7 @@ import PartyViewer from '../apps/PartyViewer';
 export default function renderPartyViewer(hotkey = false) {
   game.dialogs ??= {};
 
-  if (hotkey && game.dialogs.partyViewer) {
+  if (hotkey && game.dialogs.partyViewer?.rendered) {
     game.dialogs?.partyViewer?.close();
     return;
   }
--- src/keybindings.js
+++ src/keybindings.js
@@ -3,9 +3,9 @@ import renderPartyViewer from './hooks/renderPartyViewer';
 export default function registerKeybindings() {
   game.keybindings.register('a5e', 'party-sheet-open-close', {
     name: 'Open/Close Party Sheet',
-    editable: [{ key: 'p' }],
+    editable: [{ key: 'KeyP' }],
     onDown: () => {
-      if (!game.user.isGM && !(game.settings.get('a5e', 'playersCanAccessPartyViewer'))) {
+      if (!game.user.isGM && !game.settings.get('a5e', 'playersCanAccessPartyViewer')) {
         return;
       }
 
