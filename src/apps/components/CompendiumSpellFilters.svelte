89f94b1d18fbd65ee0dca5b8d18b45b4077c13c6
Phil <me@phillip.best>
2023-09-26 15:46:52 +0100

    Adds the checkboxes for the spell level filter.

--- src/apps/components/CompendiumSpellFilters.svelte
+++ src/apps/components/CompendiumSpellFilters.svelte
@@ -1,1 +1,32 @@
-Spell Filters!
+<script>
+    import Checkbox from "./Checkbox.svelte";
+    import FormSection from "./FormSection.svelte";
+
+    const { spellLevels } = CONFIG.A5E;
+</script>
+
+<FormSection
+    heading="Spell Levels"
+    --background="transparent"
+    --direction="column"
+    --padding="0"
+>
+    <ul class="filter-option-list">
+        {#each Object.entries(spellLevels) as [level, label]}
+            <li>
+                <Checkbox {label} />
+            </li>
+        {/each}
+    </ul>
+</FormSection>
+
+<style lang="scss">
+    .filter-option-list {
+        display: flex;
+        flex-direction: column;
+        gap: 0.25rem;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+</style>
