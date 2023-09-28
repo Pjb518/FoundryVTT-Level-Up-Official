ca3192c2debd70dc187288c8e5a6da7b92029d9b
Phil <me@phillip.best>
2023-09-28 02:36:36 +0100

    Temporarily removes the new spell compendium sheet so that we can put out a release.

--- src/hooks/setup.js
+++ src/hooks/setup.js
@@ -7,6 +7,9 @@ export default function setup() {
   registerSystemSettings();
   setupConditions();
 
+  // TODO: REMOVE THIS RETURN ONCE 0.15.3 IS OUT!
+  return;
+
   // Apply custom compendium styles to the SRD rules compendium.
   const spells = game.packs.get('a5e.a5e-spells');
   spells.applicationClass = SpellCompendiumSheet;
