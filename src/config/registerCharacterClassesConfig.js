4db069561f64a56d9474d6454d767e777257d859
Phil <me@phillip.best>
2023-10-01 22:57:09 +0100

    Reorganises the classes in the config file, moving the witch, elementalist, and wielder to the spellcasting classes section. Makes it so that the witch can be selected in the spellcasting classes config on spells.

--- src/config/registerCharacterClassesConfig.js
+++ src/config/registerCharacterClassesConfig.js
@@ -4,9 +4,12 @@ export default function registerCharacterClassesConfig(A5E) {
     bard: 'A5E.characterClasses.bard',
     cleric: 'A5E.characterClasses.cleric',
     druid: 'A5E.characterClasses.druid',
+    // elementalist: 'A5E.characterClasses.elementalist',
     herald: 'A5E.characterClasses.herald',
     sorcerer: 'A5E.characterClasses.sorcerer',
     warlock: 'A5E.characterClasses.warlock',
+    // wielder: 'A5E.characterClasses.wielder',
+    witch: 'A5E.characterClasses.witch',
     wizard: 'A5E.characterClasses.wizard'
   };
 
@@ -14,15 +17,12 @@ export default function registerCharacterClassesConfig(A5E) {
     ...A5E.spellcastingCharacterClasses,
     adept: 'A5E.characterClasses.adept',
     berserker: 'A5E.characterClasses.berserker',
-    // elementalist: 'A5E.characterClasses.elementalist',
-    // esper: 'A5E.characterClasses.esper',
+    esper: 'A5E.characterClasses.esper',
     fighter: 'A5E.characterClasses.fighter',
     marshal: 'A5E.characterClasses.marshal',
     ranger: 'A5E.characterClasses.ranger',
     rogue: 'A5E.characterClasses.rogue',
-    savant: 'A5E.characterClasses.savant'
-    // scholar: 'A5E.characterClasses.scholar',
-    // wielder: 'A5E.characterClasses.wielder',
-    // witch: 'A5E.characterClasses.witch',
+    savant: 'A5E.characterClasses.savant',
+    scholar: 'A5E.characterClasses.scholar'
   };
 }
