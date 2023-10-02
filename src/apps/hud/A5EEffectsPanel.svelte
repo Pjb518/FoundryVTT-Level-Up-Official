268906396f88fccc5d4a78bf03fa30cc03e78f4d
Phil <me@phillip.best>
2023-10-02 14:10:21 +0100

    Adds basic styles for the new effects panel.

--- src/apps/hud/A5EEffectsPanel.svelte
+++ src/apps/hud/A5EEffectsPanel.svelte
@@ -26,14 +26,17 @@
         ...($actor?.temporaryEffects || []),
         ...(token?.effects || []),
     ].sort((a, b) => a.name.localeCompare(b.name));
+
+    console.log($actor?.temporaryEffects);
 </script>
 
 {#if token && $actor && effects.length}
-    <article id="a5e-effects-panel">
-        <ul>
-            {#each effects as { name }}
-                <li>
-                    {name}
+    <article id="a5e-effects-panel" class="a5e-effects-panel">
+        <ul class="a5e-effect-list">
+            {#each effects as { name, icon }}
+                <li class="a5e-effect-item">
+                    <img class="a5e-effect-item__icon" src={icon} alt={name} />
+                    <!-- {name} -->
                 </li>
             {/each}
         </ul>
@@ -41,15 +44,36 @@
 {/if}
 
 <style lang="scss">
-    #a5e-effects-panel {
-        background-color: aliceblue;
-        pointer-events: initial;
-        padding: 0.75rem;
-
-        width: 8rem;
+    .a5e-effects-panel {
         position: absolute;
-        left: 0;
         top: 4rem;
+        left: -0.5rem;
+        pointer-events: initial;
         transform: translateX(-100%);
+    }
+
+    .a5e-effect-list {
+        display: flex;
+        flex-direction: column;
+        gap: 0.75rem;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    .a5e-effect-item {
+        display: flex;
+        position: relative;
+        border: 1px solid #e9d7a1;
+        border-radius: 50%;
+        cursor: pointer;
+
+        &__icon {
+            height: 2.5rem;
+            width: 2.5rem;
+            border: 1px solid black;
+            border-radius: 50%;
+            background-color: rgba(0, 0, 0, 0.6);
+        }
     }
 </style>
