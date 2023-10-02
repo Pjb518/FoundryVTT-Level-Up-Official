890194bbe30b2dac583769a4038306161baf5487
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-10-02 04:56:06 -0700

    Removes chat card header.

--- src/hooks/renderChatMessage.js
+++ src/hooks/renderChatMessage.js
@@ -2,13 +2,15 @@ import A5eChatCard from '../apps/chat/ChatCard.svelte';
 
 export default function renderChatMessage(message, html) {
   // Add svelte component
-  const target = $(html).find('.message-content article')[0];
+  // const target = $(html).find('.chat-message')[0];
+  const target = html[0];
 
   if (!target) return;
 
+  // target.innerHTML = '';
   if (CONFIG.A5E.chatCardTypes.includes(message.getFlag('a5e', 'cardType'))) {
     message._svelteComponent = new A5eChatCard({
-      target: $(html).find('.message-content article')[0],
+      target,
       props: { messageDocument: message }
     });
   }
