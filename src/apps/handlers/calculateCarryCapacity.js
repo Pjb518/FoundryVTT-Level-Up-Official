d1dceaef70c2f60152c18c97ccb889cc5e6022c5
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-10-20 12:04:57 -0700

    Defaults to medium size to calculate carry capacity if no size is avaliable on an actor.

--- src/apps/handlers/calculateCarryCapacity.js
+++ src/apps/handlers/calculateCarryCapacity.js
@@ -1,7 +1,8 @@
 export default function calculateCarryCapacity(actorData) {
+  console.log('Here');
   const strengthScore = actorData.system.abilities.str.value;
   const { size } = actorData.system.traits;
-  const baseCarryCapacityMultiplier = CONFIG.A5E.carryCapacityMultiplier[size];
+  const baseCarryCapacityMultiplier = CONFIG.A5E.carryCapacityMultiplier[size || 'med'];
   const carryCapacityMultiplier = actorData.flags.a5e?.doubleCarryCapacity ? 2 : 1;
 
   return strengthScore * baseCarryCapacityMultiplier * carryCapacityMultiplier * 15;
