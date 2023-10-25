9e1ff1f8e2d0db8aedcd1b5c60e2e3398823d735
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-10-24 18:53:03 -0700

    Adds a SubObjectManager Class

---
+++ src/managers/subItems/SubObjectManager.ts
@@ -0,0 +1,16 @@
+import SubItemManager from './SubItemManager';
+
+export default class SubObjectManager extends SubItemManager {
+  // constructor(doc: any, attribute: string, options: any) {
+  //   super(doc, attribute, options);
+  //   // TODO: Add custom options here
+  // }
+
+  /** ************************************************
+  *               Internal methods
+  * ************************************************ */
+
+  /** ************************************************
+  *               External methods
+  * ************************************************ */
+}
