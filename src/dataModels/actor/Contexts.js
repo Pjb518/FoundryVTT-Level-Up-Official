8609822108f653a416a3aea634f23979790d9149
NekroDarkmoon <nekrodarkmoon@gmail.com>
2023-10-25 21:09:07 -0700

    Adds damageTypes as an option for damage bonus context.

--- src/dataModels/actor/Contexts.js
+++ src/dataModels/actor/Contexts.js
@@ -5,11 +5,15 @@ export function getAttackBonusContext() {
 export function getDamageBonusContext() {
   const { fields } = foundry.data;
   return {
-    attackType: new fields.ArrayField(
+    attackTypes: new fields.ArrayField(
       new fields.StringField({ required: true, initial: '' }),
       { initial: ['all'] }
     ),
-    spellLevel: new fields.ArrayField(
+    damageTypes: new fields.ArrayField(
+      new fields.StringField({ required: true, initial: '' }),
+      { initial: [] }
+    ),
+    spellLevels: new fields.ArrayField(
       new fields.StringField({ required: true, initial: '' }),
       { initial: [] }
     )
@@ -19,11 +23,11 @@ export function getDamageBonusContext() {
 export function getHealingBonusData() {
   const { fields } = foundry.data;
   return {
-    healingType: new fields.ArrayField(
+    healingTypes: new fields.ArrayField(
       new fields.StringField({ required: true, initial: '' }),
       { initial: ['healing'] }
     ),
-    spellLevel: new fields.ArrayField(
+    spellLevels: new fields.ArrayField(
       new fields.StringField({ required: true, initial: '' }),
       { initial: [] }
     )
