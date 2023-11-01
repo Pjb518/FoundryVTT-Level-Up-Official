771f161a905547946dd942b0bf2d28bdf14162f5
Phil <me@phillip.best>
2023-11-01 11:12:43 +0000

    Adds a type to describe individual ability scores.

--- types/abilityScore.d.ts
+++ types/abilityScore.d.ts
@@ -1,1 +1,19 @@
 type AbilityScoreKey = 'str' | 'dex' | 'con' | 'int' | 'wis' | 'cha';
+
+type AbilityScore = {
+  mod: number,
+  value: number,
+  check: {
+    bonus: string,
+    deterministicBonus: number,
+    expertiseDice: number,
+    mod: number
+  },
+  save: {
+    bonus: string,
+    deterministicBonus: number,
+    expertiseDice: number,
+    mod: number,
+    proficient: boolean
+  }
+};
