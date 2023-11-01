85b6d3451282fb1c0ee3f549262897bc177b4184
Phil <me@phillip.best>
2023-11-01 15:23:08 +0000

    Updates localizeObject to Typescript.

--- src/utils/localization/localizeObject.ts
+++ src/utils/localization/localizeObject.ts
@@ -1,6 +1,7 @@
+// @ts-ignore
 import { localize } from '#runtime/svelte/helper';
 
-export default function localizeObject(obj) {
+export default function localizeObject(obj: { [key: string]: any }): void {
   Object.entries(obj).forEach(([key, value]) => {
     if (typeof value === 'string') {
       obj[key] = localize(value);
