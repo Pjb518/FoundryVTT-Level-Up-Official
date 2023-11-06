a3a54360a6afea40d8566900ddda5adb54a6bb3a
Phil <me@phillip.best>
2023-11-06 02:53:53 +0000

    Updates the validateTemplateData helper to account for emanations and walls.

--- src/utils/measuredTemplates/validateTemplateData.ts
+++ src/utils/measuredTemplates/validateTemplateData.ts
@@ -7,14 +7,12 @@ export default function validateTemplateData(area: TemplateArea): boolean {
   if (area.quantity <= 0) return false;
 
   if (area.shape === 'cone') {
-    let length: number;
-
-    if ((area as ConeArea).length) length = parseInt((area as ConeArea)?.length, 10);
+    const length = parseInt(area?.length, 10);
     if (!length || length <= 0) return false;
   } else if (area.shape === 'cube' || area.shape === 'square') {
     const size = parseInt(area?.width, 10);
     if (!size || size <= 0) return false;
-  } else if (['circle', 'cylinder', 'sphere'].includes(area.shape)) {
+  } else if (['circle', 'cylinder', 'emanation', 'sphere'].includes(area.shape)) {
     const radius = parseInt(area?.radius, 10);
     if (!radius || radius <= 0) return false;
   } else if (area.shape === 'line') {
@@ -23,6 +21,10 @@ export default function validateTemplateData(area: TemplateArea): boolean {
 
     if (!length || !width) return false;
     if (length <= 0 || width <= 0) return false;
+  } else if (area.shape === 'wall') {
+    const length = parseInt(area?.length, 10);
+
+    if (!length) return false;
   }
 
   return true;
