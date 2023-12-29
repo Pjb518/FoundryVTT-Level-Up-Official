19d7e1197f9985be00a7f443aabb5db79886ce4c
Phil <me@phillip.best>
2023-12-29 02:14:45 +0000

    Fixes a typo in the tool check roll prepartion logic that was preventing users from rolling tool checks.

--- src/managers/RollPreparationManager.js
+++ src/managers/RollPreparationManager.js
@@ -368,7 +368,7 @@ export default class RollPreparationManager {
     // Add Global Ability bonus
     modifiers.push({
       label: localize('A5E.AbilityCheckBonusGlobal'),
-      value: this.#actor.BonusesManger.getGlobalAbilityBonusesFormula('check')
+      value: this.#actor.BonusesManager.getGlobalAbilityBonusesFormula('check')
     });
 
     // Add Custom Bonus to Roll
