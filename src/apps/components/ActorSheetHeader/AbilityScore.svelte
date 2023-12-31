e67cd704e7522b3a60f5af5e5b5b0c6a9801a1c9
Phil <me@phillip.best>
2023-12-31 22:53:30 +0000

    Adds a new ability-scores stylessheet. Updates the ability scores section to use the new serif fonts.

--- src/apps/components/ActorSheetHeader/AbilityScore.svelte
+++ src/apps/components/ActorSheetHeader/AbilityScore.svelte
@@ -18,12 +18,12 @@
         : $actor.flags?.a5e?.sheetIsLocked ?? true;
 </script>
 
-<li class="ability-score">
-    <div class="ability-score__main">
-        <h3>{abilityLabel}</h3>
+<li class="a5e-ability-score">
+    <div class="a5e-ability-score__main">
+        <h3 class="a5e-ability-score__label">{abilityLabel}</h3>
 
         <input
-            class="ability-score__value"
+            class="a5e-ability-score__value"
             name="system.abilities.{abilityLabel}.value"
             type="number"
             value={ability.value}
@@ -40,7 +40,7 @@
     </div>
 
     <button
-        class="roll-button"
+        class="a5e-roll-button"
         data-tooltip="A5E.RollAbilityCheck"
         data-tooltip-direction="DOWN"
         on:click={() =>
@@ -49,15 +49,15 @@
                 getKeyPressAsOptions($pressedKeysStore),
             )}
     >
-        <h4 class="roll-button__label">Check</h4>
+        <h4 class="a5e-roll-button__label">Check</h4>
 
-        <div class="roll-button__value">
+        <div class="a5e-roll-button__value">
             {replaceHyphenWithMinusSign(ability.check.deterministicBonus)}
         </div>
     </button>
 
     <button
-        class="roll-button"
+        class="a5e-roll-button"
         data-tooltip="A5E.RollSavingThrow"
         data-tooltip-direction="DOWN"
         on:click={() =>
@@ -66,9 +66,9 @@
                 getKeyPressAsOptions($pressedKeysStore),
             )}
     >
-        <h4 class="roll-button__label">Save</h4>
+        <h4 class="a5e-roll-button__label">Save</h4>
 
-        <div class="roll-button__value">
+        <div class="a5e-roll-button__value">
             {replaceHyphenWithMinusSign(ability.save.deterministicBonus)}
         </div>
     </button>
@@ -81,79 +81,3 @@
         />
     {/if}
 </li>
-
-<style lang="scss">
-    .ability-score {
-        position: relative;
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        align-items: center;
-        width: 100%;
-        color: $color-light-text;
-        font-family: $font-primary;
-        border-radius: $border-radius-standard;
-        gap: 0.25rem;
-        overflow: hidden;
-
-        &__main {
-            grid-column: span 2;
-        }
-
-        &__value {
-            display: flex;
-            align-items: center;
-            height: 26px;
-            border: 0;
-            color: $color-dark-text;
-            margin-bottom: 0;
-            background: none;
-            font-size: $font-size-xl;
-            font-weight: inherit;
-            text-align: center;
-
-            &:focus {
-                box-shadow: none;
-            }
-
-            &::placeholder {
-                color: lighten($color-dark-text, 35%);
-            }
-        }
-    }
-
-    .ability-score__main,
-    .roll-button {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        padding: 0.125rem 0;
-        margin: 0;
-        font-family: $font-primary;
-        color: #7e7960;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        background: $color-light-background;
-        box-shadow: 0 0 5px #ccc inset;
-        line-height: unset;
-    }
-
-    .roll-button {
-        cursor: pointer;
-
-        &__label {
-            font-size: $font-size-xs;
-        }
-
-        &__value {
-            color: $color-dark-text;
-        }
-    }
-
-    .a5e-button--edit-config {
-        top: 0.25rem;
-        right: 0.25rem;
-    }
-</style>
