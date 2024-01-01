76ca27271bb5e00807103cef9939f844ff2701d4
Phil <me@phillip.best>
2024-01-01 14:23:58 +0000

    Updates the compendium section headings to the use the system's section-heading styles.

--- src/apps/components/compendiumSheets/CompendiumSubItemList.svelte
+++ src/apps/components/compendiumSheets/CompendiumSubItemList.svelte
@@ -5,28 +5,13 @@
 </script>
 
 {#if reducer.length}
-    <h3 class="a5efc-category-heading">
-        {name}
-    </h3>
+    <header class="a5e-section-header">
+        <h3 class="a5e-section-header__heading">
+            {name}
+        </h3>
+    </header>
 
     {#each [...$reducer] as document}
         <svelte:component this={ItemComponent} {document} />
     {/each}
 {/if}
-
-<style>
-    .a5efc-category-heading {
-        display: flex;
-        align-items: center;
-        margin-block: 0;
-        padding: 0.25rem 0.5rem;
-        font-family: Signika, sans-serif;
-        font-size: 0.833rem;
-        line-height: 1;
-        color: #f6f2eb;
-        background: #425f65;
-        border-bottom: none;
-        border-radius: 3px;
-        box-shadow: inset 0 0 10px #2e4246;
-    }
-</style>
