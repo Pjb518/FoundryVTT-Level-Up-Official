ae127109164f68108a8df87699029c5e44d3da4b
Phil <me@phillip.best>
2024-01-02 00:13:33 +0000

    Removes some defunct styles from the party viewer resources tab.

--- src/apps/components/partyViewer/PartyViewerResourceHeader.svelte
+++ src/apps/components/partyViewer/PartyViewerResourceHeader.svelte
@@ -86,22 +86,6 @@
         }
     }
 
-    .spell-slots {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 0.5rem;
-        width: 100%;
-        min-width: 7.5rem;
-
-        &__heading {
-            font-size: 0.833rem;
-            text-align: center;
-            font-weight: 700;
-            white-space: nowrap;
-        }
-    }
-
     .spell-levels {
         grid-area: spellSlots;
         display: flex;
