a91ad1d9eb67697c2e0ae6321f76caa8ea028b53
Phil <me@phillip.best>
2024-01-07 22:30:22 +0000

    Updates the item tabs to use the new a5e-page-wrapper class.

--- src/apps/components/pages/ItemActionsTab.svelte
+++ src/apps/components/pages/ItemActionsTab.svelte
@@ -37,60 +37,64 @@
     }
 </script>
 
-<Section
-    heading="A5E.TabActions"
-    headerButtons={[
-        {
-            classes: "add-button",
-            handler: addAction,
-            label: "A5E.ButtonAddAction",
-        },
-    ]}
-    --a5e-section-gap="0.125rem"
->
-    <ul class="a5e-item-list">
-        {#each Object.entries($item.system.actions ?? {}) as [id, action] (id)}
-            <li
-                class="a5e-item a5e-item--action"
-                data-action-id={id}
-                draggable="true"
-                on:dragstart={(event) => _onDragStart(event, id)}
-                on:auxclick={() => configureAction(id)}
-            >
-                <img
-                    class="a5e-item__image a5e-item__image--action"
-                    src={action?.img ?? $item.img ?? "icons/svg/item-bag.svg"}
-                    alt=""
-                />
-
-                {action?.name}
-
-                <div class="action-buttons">
-                    <button
-                        class="action-button fas fa-cog"
-                        data-tooltip="A5E.ButtonToolTipConfigure"
-                        data-tooltip-direction="UP"
-                        on:click={() => configureAction(id)}
+<div class="a5e-page-wrapper a5e-page-wrapper--scrollable">
+    <Section
+        heading="A5E.TabActions"
+        headerButtons={[
+            {
+                classes: "add-button",
+                handler: addAction,
+                label: "A5E.ButtonAddAction",
+            },
+        ]}
+        --a5e-section-gap="0.125rem"
+    >
+        <ul class="a5e-item-list">
+            {#each Object.entries($item.system.actions ?? {}) as [id, action] (id)}
+                <li
+                    class="a5e-item a5e-item--action"
+                    data-action-id={id}
+                    draggable="true"
+                    on:dragstart={(event) => _onDragStart(event, id)}
+                    on:auxclick={() => configureAction(id)}
+                >
+                    <img
+                        class="a5e-item__image a5e-item__image--action"
+                        src={action?.img ??
+                            $item.img ??
+                            "icons/svg/item-bag.svg"}
+                        alt=""
                     />
 
-                    <button
-                        class="action-button fa-solid fa-clone"
-                        data-tooltip="A5E.ButtonToolTipDuplicate"
-                        data-tooltip-direction="UP"
-                        on:click={() => duplicateAction(id)}
-                    />
+                    {action?.name}
 
-                    <button
-                        class="action-button delete-button fas fa-trash"
-                        data-tooltip="A5E.ButtonToolTipDelete"
-                        data-tooltip-direction="UP"
-                        on:click={() => deleteAction(id)}
-                    />
-                </div>
-            </li>
-        {/each}
-    </ul>
-</Section>
+                    <div class="action-buttons">
+                        <button
+                            class="action-button fas fa-cog"
+                            data-tooltip="A5E.ButtonToolTipConfigure"
+                            data-tooltip-direction="UP"
+                            on:click={() => configureAction(id)}
+                        />
+
+                        <button
+                            class="action-button fa-solid fa-clone"
+                            data-tooltip="A5E.ButtonToolTipDuplicate"
+                            data-tooltip-direction="UP"
+                            on:click={() => duplicateAction(id)}
+                        />
+
+                        <button
+                            class="action-button delete-button fas fa-trash"
+                            data-tooltip="A5E.ButtonToolTipDelete"
+                            data-tooltip-direction="UP"
+                            on:click={() => deleteAction(id)}
+                        />
+                    </div>
+                </li>
+            {/each}
+        </ul>
+    </Section>
+</div>
 
 <style lang="scss">
     .action-button {
