6ab22d11f0fff940ed2f0c55f6be8cbcd7ab86f7
Phil <me@phillip.best>
2024-01-07 17:14:28 +0000

    Updates the ResourceConsumer to use the new form components.

--- src/apps/components/itemActionsConfig/ResourceConsumer.svelte
+++ src/apps/components/itemActionsConfig/ResourceConsumer.svelte
@@ -3,11 +3,13 @@
     import { localize } from "#runtime/svelte/helper";
 
     import Checkbox from "../Checkbox.svelte";
+    import FieldWrapper from "../FieldWrapper.svelte";
 
     import updateDocumentDataFromField from "../../../utils/updateDocumentDataFromField";
 
     export let consumer;
     export let consumerId;
+    export let deleteConsumer;
 
     const item = getContext("item");
     const actionId = getContext("actionId");
@@ -17,7 +19,7 @@
         updateDocumentDataFromField(
             $item,
             `system.actions.${actionId}.consumers.${consumerId}.resource`,
-            selectedResource
+            selectedResource,
         );
     }
 
@@ -25,77 +27,74 @@
     $: selectedResource, updateResourceSelection();
 </script>
 
-<section class="action-config__wrapper">
-    <div class="a5e-field-group a5e-field-group--label">
-        <label for="{actionId}-{consumerId}-label">
-            {localize("A5E.Label")}
-        </label>
+<FieldWrapper
+    heading="A5E.Label"
+    buttons={[
+        {
+            classes: "fa-solid fa-trash",
+            handler: () => deleteConsumer(actionId, consumerId),
+        },
+    ]}
+    --a5e-header-button-color="rgba(0, 0, 0, 0.2)"
+    --a5e-header-button-color-hover="#555"
+>
+    <input
+        type="text"
+        value={consumer.label ?? ""}
+        on:change={() =>
+            updateDocumentDataFromField(
+                $item,
+                `system.actions.${actionId}.consumers.${consumerId}.label`,
+            )}
+    />
+</FieldWrapper>
 
-        <input
-            id="{actionId}-{consumerId}-label"
-            name="{actionId}-{consumerId}-label"
-            type="text"
-            value={consumer.label ?? ""}
-            on:change={() =>
-                updateDocumentDataFromField(
-                    $item,
-                    `system.actions.${actionId}.consumers.${consumerId}.label`
-                )}
-        />
-    </div>
+<div class="a5e-field-group u-flex-row u-gap-md">
+    <FieldWrapper heading="A5E.Resource">
+        <select
+            name="{actionId}-{consumerId}-item-id"
+            class="u-w-fit"
+            bind:value={selectedResource}
+        >
+            <option value="" />
 
-    <div class="a5e-field-group u-flex-row u-gap-md">
-        <div class="u-flex u-flex-col u-gap-sm">
-            <h3 class="a5e-field-group__heading">
-                {localize("A5E.Resource")}
-            </h3>
+            {#each Object.entries(A5E.resourceConsumerConfig) as [value, { label }] (value)}
+                <option {value}>
+                    {localize(label)}
+                </option>
+            {/each}
+        </select>
+    </FieldWrapper>
 
-            <select
-                name="{actionId}-{consumerId}-item-id"
-                class="u-w-fit"
-                bind:value={selectedResource}
-            >
-                <option value="" />
-
-                {#each Object.entries(A5E.resourceConsumerConfig) as [value, { label }] (value)}
-                    <option {value}>
-                        {localize(label)}
-                    </option>
-                {/each}
-            </select>
-        </div>
-
-        {#if A5E.resourceConsumerConfig?.[selectedResource]?.type === "value"}
-            <div class="u-flex u-flex-col u-gap-sm u-w-30">
-                <h3 class="a5e-field-group__heading">
-                    {localize("A5E.ConsumeValue")}
-                </h3>
-
-                <input
-                    type="number"
-                    d-type="Number"
-                    value={consumer.quantity ?? 1}
-                    on:change={({ target }) =>
-                        updateDocumentDataFromField(
-                            $item,
-                            `system.actions.${actionId}.consumers.${consumerId}.quantity`,
-                            Number(target.value)
-                        )}
-                />
-            </div>
-        {/if}
-    </div>
-
-    {#if A5E.resourceConsumerConfig?.[selectedResource]?.type === "boolean"}
-        <Checkbox
-            label="A5E.ConsumerRestoreResourceOnUse"
-            checked={consumer.restore ?? false}
-            on:updateSelection={({ detail }) =>
-                updateDocumentDataFromField(
-                    $item,
-                    `system.actions.${actionId}.consumers.${consumerId}.restore`,
-                    detail
-                )}
-        />
+    {#if A5E.resourceConsumerConfig?.[selectedResource]?.type === "value"}
+        <FieldWrapper
+            heading="A5E.ConsumeValue"
+            --a5e-field-wrapper-width="7.5rem"
+        >
+            <input
+                type="number"
+                d-type="Number"
+                value={consumer.quantity ?? 1}
+                on:change={({ target }) =>
+                    updateDocumentDataFromField(
+                        $item,
+                        `system.actions.${actionId}.consumers.${consumerId}.quantity`,
+                        Number(target.value),
+                    )}
+            />
+        </FieldWrapper>
     {/if}
-</section>
+</div>
+
+{#if A5E.resourceConsumerConfig?.[selectedResource]?.type === "boolean"}
+    <Checkbox
+        label="A5E.ConsumerRestoreResourceOnUse"
+        checked={consumer.restore ?? false}
+        on:updateSelection={({ detail }) =>
+            updateDocumentDataFromField(
+                $item,
+                `system.actions.${actionId}.consumers.${consumerId}.restore`,
+                detail,
+            )}
+    />
+{/if}
