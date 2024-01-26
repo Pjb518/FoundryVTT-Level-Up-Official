568a7a4e27839de2f1239d53457974060a8c44cb
Phil <me@phillip.best>
2024-01-26 10:29:13 +0000

    Removes the grant progress bar. Adds all grants from a single drop to the same interface.

--- src/apps/dialogs/GrantApplicationDialog.svelte
+++ src/apps/dialogs/GrantApplicationDialog.svelte
@@ -2,8 +2,6 @@
 
 <script lang="ts">
     import { getContext } from "svelte";
-    import { TJSDocument } from "#runtime/svelte/store/fvtt/document";
-    import { localize } from "#runtime/svelte/helper";
 
     import prepareApplyData from "../../utils/prepareApplyData";
 
@@ -13,42 +11,6 @@
     export let { allGrants, optionalGrants, actor } =
         // @ts-ignore
         getContext("#external").application;
-
-    function nextGrant() {
-        console.log(applyData);
-
-        if (state === 0) {
-            state = 1;
-            return;
-        }
-
-        if (state === 1 && progress === total - 1) {
-            state = 2;
-            return;
-        }
-
-        if (progress === total - 1) return;
-
-        progress = progress + 1;
-        currentGrant = configurableGrants[progress];
-    }
-
-    function previousGrant() {
-        if (progress === total - 1 && state === 2) {
-            state = 1;
-            return;
-        }
-
-        if (progress === 0 && state === 1) {
-            state = 0;
-            return;
-        }
-
-        if (progress === 0) return;
-
-        progress = progress - 1;
-        currentGrant = configurableGrants[progress];
-    }
 
     // Create a list of grants to show based on selected optional
     // grants and those grants that have configurable properties
@@ -79,28 +41,10 @@
     }
 
     let applyData = new Map<string, any>();
-    let progress = 0;
     let selectedOptionalGrants: string[] = [];
-    let state = optionalGrants.length ? 0 : 1;
 
     $: grants = getApplicableGrants(selectedOptionalGrants);
     $: configurableGrants = grants.filter((grant) => grant.requiresConfig);
-    $: totalApplicable = grants.length;
-    $: total = configurableGrants.length;
-
-    $: currentGrant = configurableGrants[progress];
-    $: CurrentComponent = currentGrant?.grant?.getSelectionComponent?.();
-    $: currentComponentProps =
-        currentGrant?.grant?.getSelectionComponentProps?.();
-
-    $: progressDisplay =
-        state === 0 ? 0 : state === 1 ? progress : progress + 1;
-    $: progressTooltip = `
-        Total Applicable Grants: ${totalApplicable}
-        Total Configurable Grants: ${total}
-        Current Grant: ${currentGrant?.grant?.label}
-        Current Grant Progress: ${progress + 1} / ${total}
-    `;
 </script>
 
 <article>
@@ -113,13 +57,14 @@
         />
     </Section>
 
-    <svelte:component
-        this={CurrentComponent}
-        {...currentComponentProps}
-        grant={currentGrant.grant}
-        on:updateSelection={({ detail }) =>
-            applyData.set(currentGrant.id, detail)}
-    />
+    {#each configurableGrants as { grant, id }}
+        <svelte:component
+            this={grant.getSelectionComponent?.()}
+            {...grant.getSelectionComponentProps?.()}
+            {grant}
+            on:updateSelection={({ detail }) => applyData.set(id, detail)}
+        />
+    {/each}
 
     <Section heading="Summary">
         <ul>
@@ -133,37 +78,6 @@
     </Section>
 
     <button> Submit </button>
-
-    <!-- Progress Bar -->
-    <div class="progress-bar-wrapper">
-        <button
-            class="nav-button"
-            disabled={state === 0 && progress === 0}
-            on:click={previousGrant}
-            data-tooltip="Previous"
-            data-tooltip-direction="UP"
-        >
-            <i class="fa-solid fa-chevron-left" />
-        </button>
-
-        <div
-            class="progress-bar"
-            data-tooltip={progressTooltip}
-            data-tooltip-direction="UP"
-        >
-            {progressDisplay} / {total}
-        </div>
-
-        <button
-            class="nav-button"
-            disabled={state === 2 && progress >= total - 1}
-            on:click={nextGrant}
-            data-tooltip="Next"
-            data-tooltip-direction="UP"
-        >
-            <i class="fa-solid fa-chevron-right" />
-        </button>
-    </div>
 </article>
 
 <style lang="scss">
@@ -172,49 +86,5 @@
         flex-direction: column;
         gap: 0.75rem;
         padding: 0.75rem;
-    }
-
-    .progress-bar-wrapper {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 0.5rem;
-        padding-top: 0.75rem;
-        border-top: 1px solid #ccc;
-    }
-
-    .progress-bar {
-        display: flex;
-        flex-grow: 1;
-        justify-content: center;
-        align-items: center;
-        gap: 0.5rem;
-        border-radius: 0.3rem;
-        height: 1.5rem;
-        border: 1px solid #aaa;
-        background-color: var(--color-primary);
-        color: var(--color-primary-text);
-    }
-
-    .nav-button {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 0.25rem;
-        width: 4rem;
-        padding: 0.25rem;
-        font-size: 0.833rem;
-        color: #f6f2eb;
-        background: #425f65;
-        border: 1px solid #3e4240;
-        border-radius: 3px;
-        line-height: 1;
-        box-shadow: 0 0 10px #2e4246 inset;
-        transition: all 0.15s ease-in-out;
-
-        &:hover,
-        &:focus {
-            box-shadow: 0 0 10px #2e4246 inset;
-        }
     }
 </style>
