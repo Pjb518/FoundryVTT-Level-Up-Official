044f309a43348b4eb8b502a66ac2761a909a7143
Phil <me@phillip.best>
2024-01-29 17:48:44 +0000

    Updates the premmium content tab to use Section

--- src/apps/dialogs/PremiumContentListPremium.svelte
+++ src/apps/dialogs/PremiumContentListPremium.svelte
@@ -1,3 +1,7 @@
+<script>
+    import Section from "../components/Section.svelte";
+</script>
+
 <p>
     Below you'll find a list of premium content modules organized by publisher.
     Some of these modules are included for free when you purchase the a PDF or
@@ -12,24 +16,24 @@
 </p>
 
 {#each Object.values(CONFIG.A5E.premiumContent) as { name, releases }}
-    <h3>{name}</h3>
-
-    <ul class="content-list">
-        {#each releases as { title, url }}
-            <li class="content-list__item">
-                <a href={url} target="_blank">{title}</a>
-            </li>
-        {/each}
-    </ul>
+    <Section heading={name}>
+        <ul class="content-list">
+            {#each releases as { title, url }}
+                <li class="content-list__item">
+                    <a href={url} target="_blank">{title}</a>
+                </li>
+            {/each}
+        </ul>
+    </Section>
 {/each}
 
 <style lang="scss">
     .a5e-content-disclaimer {
         padding: 0.5rem 0.75rem;
         margin-block: 0.75rem;
-        background: #425f65;
+        background: rgba(84, 84, 84, 0.8);
         color: #f6f2eb;
-        box-shadow: 0 0 10px #2e4246 inset;
+        box-shadow: 0 0 10px rgba(59, 59, 59, 0.8) inset;
         border-radius: 4px;
         text-wrap: pretty;
     }
