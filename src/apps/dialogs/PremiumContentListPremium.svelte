e380970369ab99e1175f4fbcf1975e3c3c385c7d
Phil <me@phillip.best>
2024-01-29 17:43:40 +0000

    Updates styles for premium content disclaimer.

--- src/apps/dialogs/PremiumContentListPremium.svelte
+++ src/apps/dialogs/PremiumContentListPremium.svelte
@@ -1,16 +1,14 @@
-<p style="margin-bottom: 0.75rem;">
+<p>
     Below you'll find a list of premium content modules organized by publisher.
     Some of these modules are included for free when you purchase the a PDF or
     physical book. Others modules are sold as standalone products.
 </p>
 
-<p>
-    <i>
-        Disclaimer: This page contains some affiliate links. If you choose to
-        make a purchase after clicking a link, we may receive a small amount of
-        kickback from DriveThruRPG that will go towards developing and
-        maintaining the A5e Foundry system and its modules.
-    </i>
+<p class="a5e-content-disclaimer">
+    <b>Disclaimer.</b> This page contains affiliate links. If you choose to make
+    a purchase after clicking a link, we may receive a small amount of kickback from
+    DriveThruRPG that will go towards developing and maintaining the A5e Foundry
+    system and its modules.
 </p>
 
 {#each Object.values(CONFIG.A5E.premiumContent) as { name, releases }}
@@ -26,6 +24,16 @@
 {/each}
 
 <style lang="scss">
+    .a5e-content-disclaimer {
+        padding: 0.5rem 0.75rem;
+        margin-block: 0.75rem;
+        background: #425f65;
+        color: #f6f2eb;
+        box-shadow: 0 0 10px #2e4246 inset;
+        border-radius: 4px;
+        text-wrap: pretty;
+    }
+
     .content-list {
         display: flex;
         flex-direction: column;
