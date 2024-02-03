fc200bac435cf1b86f2b35495726a0050b582b0b
Phil <me@phillip.best>
2024-02-03 14:44:04 +0000

    Updates the prepareXP utility to localize numbers to an appropriate regional format.

--- src/apps/dataPreparationHelpers/prepareXP.js
+++ src/apps/dataPreparationHelpers/prepareXP.js
@@ -1,10 +1,13 @@
-export default function prepareXP(data) {
-  const cr = parseFloat(data.system.details.cr || 0);
+export default function prepareXP(actor) {
+  const cr = parseFloat(actor?.system?.details?.cr || 0);
   let baseXp = 0;
+
   if (cr === 0.125) baseXp = CONFIG.A5E.CR_EXP_LEVELS['1/8'];
   else if (cr === 0.25) baseXp = CONFIG.A5E.CR_EXP_LEVELS['1/4'];
   else if (cr === 0.5) baseXp = CONFIG.A5E.CR_EXP_LEVELS['1/2'];
   else baseXp = CONFIG.A5E.CR_EXP_LEVELS[parseInt(cr, 10) > 30 ? 30 : cr];
 
-  return data.system.details.elite ? baseXp * 2 : baseXp;
+  const xp = actor?.system?.details?.elite ? baseXp * 2 : baseXp;
+
+  return xp.toLocaleString();
 }
