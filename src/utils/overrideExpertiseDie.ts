184f2aeddba35b4f200b6125ced2af61a55cef0c
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-02-08 02:54:03 -0800

    Updates flanking automation override to account for correct token target.

--- src/utils/overrideExpertiseDie.ts
+++ src/utils/overrideExpertiseDie.ts
@@ -1,16 +1,19 @@
-/**
- *
- * @param actor
- * @param expertiseDie
- */
-export default function overrideExpertiseDie(
-  actor: { getFlag: (arg0: string, arg1: string) => any; },
-  dieCount: number
-): number {
+function getFlankingBonus(actor: typeof Actor): boolean {
+  const targets = [...game.user.targets].map((t) => t.id);
+  if (targets.length !== 1) return false;
+
+  const targetId: string = actor.getFlag('a5e', 'flanking');
+  if (!targetId) return false;
+
+  const isFlanking: boolean = targetId === targets[0];
+  return isFlanking;
+}
+
+export default function overrideExpertiseDie(actor: typeof Actor, dieCount: number): number {
   let addDie = 0;
 
   // Account for flanking
-  const isFlanking: boolean = actor.getFlag('a5e', 'flanking');
+  const isFlanking: boolean = getFlankingBonus(actor);
   if (isFlanking) addDie += 1;
 
   const flag: number | undefined = actor.getFlag('a5e', 'effects.expertiseDie');
