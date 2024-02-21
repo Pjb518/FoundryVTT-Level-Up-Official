32e59eda3fa1ff364083ebc11e9a47b9f5703d60
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-02-21 02:19:39 -0800

    WIP - RollOverrides. - Changes init to use raw effect data instead of flags.

--- src/managers/RollOverrideManager.ts
+++ src/managers/RollOverrideManager.ts
@@ -1,8 +1,17 @@
 type OverrideType = 'rollMode' | 'expertiseDie';
 
-export interface RollOverride {
+type Change = {
+  key: string;
   value: number;
+  mode: number;
+  name: string;
+  type: OverrideType;
+};
+
+export interface RollOverride {
   overrideType: OverrideType;
+  source: string;
+  value: number;
 }
 
 export default class RollOverrideManager {
@@ -42,8 +51,6 @@ export default class RollOverrideManager {
   }
 
   initialize() {
-    const overrideTypes = ['rollMode', 'expertiseDie'] as OverrideType[];
-
     // Register all overrides from items
     this.actor.items.forEach((item: typeof Item) => {
       if (item.type !== 'object' && item.system?.objectType !== 'armor') return;
@@ -53,64 +60,81 @@ export default class RollOverrideManager {
         item.system.equippedState === CONFIG.A5E.ARMOR_MODES.OVERRIDE
         && item.system.ac.grantsDisadvantage
       ) {
-        this.overrides.get('system.skills.ste')?.push({ value: -1, overrideType: 'rollMode' });
+        this.overrides.get('system.skills.ste')?.push({ value: -1, overrideType: 'rollMode', source: item.name });
       }
     });
 
     // Register all overrides from effects
-    // TODO: Add source data for these effects
-    overrideTypes.forEach((overrideType) => {
-      const overrideFlags: Record<string, any> = foundry.utils.flattenObject(
-        foundry.utils.getProperty(this.actor, `flags.a5e.effects.${overrideType}`) ?? {}
-      );
+    const changes = this.actor.effects.reduce((acc: Change[], effect: typeof ActiveEffect) => {
+      if (effect.disabled) return acc;
+      if (effect.changes.length === 0) return acc;
 
-      Object.entries(overrideFlags).forEach(([key, value]) => {
-        if (['concentration', 'deathSave', 'initiative'].includes(key)) {
-          this.overrides.get(key)?.push({ value, overrideType });
-          return;
-        }
+      const data: any[] = [];
+      effect.changes.forEach((change) => {
+        if (!change.key.startsWith('flags.a5e.effects.rollMode') && !change.key.startsWith('flags.a5e.effects.expertiseDie')) return;
+        let key: string = change.key.replace('flags.a5e.effects.', '');
+        const type = key.split('.').shift();
+        if (type === 'rollMode') key = key.replace('rollMode.', '');
+        else key = key.replace('expertiseDie.', '');
 
-        if (key === 'abilityCheck.all') {
-          this.overrides.get('abilityCheck.all')?.push({ value, overrideType });
-          return;
-        }
-
-        if (key === 'abilitySave.all') {
-          this.overrides.get('abilitySave.all')?.push({ value, overrideType });
-          return;
-        }
-
-        if (key === 'skillCheck.all') {
-          this.overrides.get('skillCheck.all')?.push({ value, overrideType });
-          return;
-        }
-
-        if (key === 'attack.all') {
-          this.overrides.get('attack.all')?.push({ value, overrideType });
-        }
-
-        const [propertyType, propertyKey] = key.split('.');
-        if (['abilityCheck', 'abilitySave'].includes(propertyType)) {
-          const type = propertyType === 'abilityCheck' ? 'check' : 'save';
-          this.overrides.get(`system.abilities.${propertyKey}.${type}`)?.push({ value, overrideType });
-          return;
-        }
-
-        if (propertyType === 'skillCheck') {
-          this.overrides.get(`system.skills.${propertyKey}`)?.push({ value, overrideType });
-          return;
-        }
-
-        if (propertyType === 'attack') {
-          this.overrides.get(`attackTypes.${propertyKey}`)?.push({ value, overrideType });
-          return;
-        }
-
-        console.log(this.overrides);
+        data.push({
+          key,
+          value: parseInt(change.value, 10) || 0,
+          mode: change.mode,
+          name: effect.name,
+          type
+        });
       });
-    });
 
-    // const expertiseDieFlags: Record<string, any> = foundry.utils.getProperty(this.actor, 'flags.a5e.effects.expertiseDie') ?? {};
+      if (data.length) acc.push(...data);
+      return acc;
+    }, []);
+
+    changes.forEach((change: Change) => {
+      const {
+        key, value, name: source, type: overrideType
+      } = change;
+
+      if (['concentration', 'deathSave', 'initiative'].includes(key)) {
+        this.overrides.get(key)?.push({ value, overrideType, source });
+        return;
+      }
+
+      if (key === 'abilityCheck.all') {
+        this.overrides.get('abilityCheck.all')?.push({ value, overrideType, source });
+        return;
+      }
+
+      if (key === 'abilitySave.all') {
+        this.overrides.get('abilitySave.all')?.push({ value, overrideType, source });
+        return;
+      }
+
+      if (key === 'skillCheck.all') {
+        this.overrides.get('skillCheck.all')?.push({ value, overrideType, source });
+        return;
+      }
+
+      if (key === 'attack.all') {
+        this.overrides.get('attack.all')?.push({ value, overrideType, source });
+      }
+
+      const [propertyType, propertyKey] = key.split('.');
+      if (['abilityCheck', 'abilitySave'].includes(propertyType)) {
+        const type = propertyType === 'abilityCheck' ? 'check' : 'save';
+        this.overrides.get(`system.abilities.${propertyKey}.${type}`)?.push({ value, overrideType, source });
+        return;
+      }
+
+      if (propertyType === 'skillCheck') {
+        this.overrides.get(`system.skills.${propertyKey}`)?.push({ value, overrideType, source });
+        return;
+      }
+
+      if (propertyType === 'attack') {
+        this.overrides.get(`attackTypes.${propertyKey}`)?.push({ value, overrideType, source });
+      }
+    });
 
     this.ready = true;
   }
