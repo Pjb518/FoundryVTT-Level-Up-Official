532d4e9a615511fb2cbf4c8c114dd669b8aaa5d9
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-03-18 20:43:56 -0700

    Updates search to optioanlly take in a reducer.

--- src/documents/item/spell.js
+++ src/documents/item/spell.js
@@ -7,7 +7,7 @@ export default class SpellItemA5e extends ItemA5e {
 
   // eslint-disable-next-line consistent-return
   _preCreate(data, options, user) {
-    if (!data.system.spellBook) {
+    if (!data.system.spellBook && this.parent?.documentName === 'Actor') {
       ui.notifications.error('You must select a spell book to create a spell.');
       return false;
     }
