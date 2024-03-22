c96848136b945c6a3e51b00151dc04523122f0ec
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-03-22 00:26:24 -0700

    Adds missing padding to generic actor resource config dialog.

--- src/apps/dialogs/GenericActorResourceConfigDialog.svelte
+++ src/apps/dialogs/GenericActorResourceConfigDialog.svelte
@@ -3,7 +3,6 @@
 
     import Checkbox from "../components/Checkbox.svelte";
     import FieldWrapper from "../components/FieldWrapper.svelte";
-    import FormSection from "../components/LegacyFormSection.svelte";
     import RadioGroup from "../components/RadioGroup.svelte";
     import Section from "../components/Section.svelte";
 
@@ -73,7 +72,7 @@
 <Section
     heading="Recovery Configuration"
     --a5e-section-body-gap="0.75rem"
-    --a5e-section-padding="0 0.75rem"
+    --a5e-section-padding="0 0.75rem 0.75rem 0.75rem"
 >
     <RadioGroup
         heading="A5E.RecoverResourceAt"
