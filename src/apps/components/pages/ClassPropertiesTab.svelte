2b2be729361f14bb96e9de22d96dea3e480a9bbb
Phil <me@phillip.best>
2024-04-24 16:04:49 +0100

    Hides the used hit dice section and hp per level fields on class sheets.

--- src/apps/components/pages/ClassPropertiesTab.svelte
+++ src/apps/components/pages/ClassPropertiesTab.svelte
@@ -11,6 +11,7 @@
     const item = getContext("item");
     const abilities = { none: "None", ...CONFIG.A5E.abilities };
     const casterTypes = CONFIG.A5E.casterTypes;
+
     const hitDiceSize = [
         [6, "d6"],
         [8, "d8"],
@@ -53,51 +54,56 @@
                 )}
         />
 
-        <FieldWrapper heading="Hit Dice Used">
-            <input
-                class="a5e-input a5e-input--small a5e-input--slim"
-                type="number"
-                value={$item.system.hp.hitDiceUsed}
-                on:change={({ target }) =>
-                    updateDocumentDataFromField(
-                        $item,
-                        "system.hp.hitDiceUsed",
-                        Number(target.value),
-                    )}
-            />
-        </FieldWrapper>
+        {#if $item.actor}
+            <FieldWrapper heading="Hit Dice Used">
+                <input
+                    class="a5e-input a5e-input--small a5e-input--slim"
+                    type="number"
+                    value={$item.system.hp.hitDiceUsed}
+                    on:change={({ target }) =>
+                        updateDocumentDataFromField(
+                            $item,
+                            "system.hp.hitDiceUsed",
+                            Number(target.value),
+                        )}
+                />
+            </FieldWrapper>
+        {/if}
     </Section>
 
-    <Section heading="Hit Points" --a5e-section-body-gap="0.75rem">
-        <div class="class-hit-point-container">
-            {#each Object.entries($item.system.hp.levels) as [level, hp]}
-                {#if level <= classLevel}
-                    <!-- {#if true} -->
-                    <input
-                        class="a5e-input a5e-input--small a5e-input--slim"
-                        type="number"
-                        value={hp ?? 0}
-                        on:change={({ target }) =>
-                            updateDocumentDataFromField(
-                                $item,
-                                `system.hp.levels.${level}`,
-                                Number(target.value),
-                            )}
-                    />
-                {/if}
-            {/each}
-        </div>
+    {#if $item.actor}
+        <Section heading="Hit Points" --a5e-section-body-gap="0.75rem">
+            <div class="class-hit-point-container">
+                {#each Object.entries($item.system.hp.levels) as [level, hp]}
+                    {#if level <= classLevel}
+                        <input
+                            class="a5e-input a5e-input--small a5e-input--slim"
+                            type="number"
+                            value={hp ?? 0}
+                            min="0"
+                            on:change={({ target }) =>
+                                updateDocumentDataFromField(
+                                    $item,
+                                    `system.hp.levels.${level}`,
+                                    Number(target.value),
+                                )}
+                        />
+                    {/if}
+                {/each}
+            </div>
 
-        <hr class="a5e-rule" />
+            <hr class="a5e-rule" />
 
-        <span> {totalHp}</span>
-    </Section>
+            <span>{totalHp}</span>
+        </Section>
+    {/if}
 
     <Section heading="Combat Maneuvers" --a5e-section-body-gap="0.75rem">
         <FieldWrapper heading="Starting Manuevers Count">
             <input
                 class="a5e-input a5e-input--small a5e-input--slim"
                 type="number"
+                min="0"
                 value={$item.system.combatManeuvers.startingManeuvers}
                 on:change={({ target }) =>
                     updateDocumentDataFromField(
