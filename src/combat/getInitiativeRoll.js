77fed97a2594ac965ceeb3721fac7544d58a569c
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-04-29 00:48:52 -0700

    Fixes initiative not rolling.

--- src/combat/getInitiativeRoll.js
+++ src/combat/getInitiativeRoll.js
@@ -7,5 +7,6 @@ export default async function getInitiativeRoll(options) {
   const formula = await this._getInitiativeFormula(options);
   const roll = Roll.create(formula);
 
-  return roll.evaluateSync();
+  const result = await roll.evaluate();
+  return result;
 }
