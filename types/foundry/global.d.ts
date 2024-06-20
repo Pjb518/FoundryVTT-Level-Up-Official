8cf041c601095cec26d6050a83a93fcbe83e096a
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-06-20 01:27:53 -0700

    Adds league types to repo.

--- types/foundry/global.d.ts
+++ types/foundry/global.d.ts
@@ -5,38 +5,38 @@
 // import * as documents from './common/documents/_module.d.mts';
 // import * as utils from './common/utils/module.d.mts';
 
-declare const CONFIG: any;
-declare const CONST: any;
+// declare const CONFIG: any;
+// declare const CONST: any;
 
-declare const Actor: any;
-declare const ActiveEffect: any;
-declare const ChatMessage: any;
-declare const Combat: any;
-declare const Combatant: any;
-declare const CompendiumCollection: any;
-declare const Hooks: any;
-declare const Item: any;
-declare const Roll: any;
-declare const Token: any;
+// declare const Actor: any;
+// declare const ActiveEffect: any;
+// declare const ChatMessage: any;
+// declare const Combat: any;
+// declare const Combatant: any;
+// declare const CompendiumCollection: any;
+// declare const Hooks: any;
+// declare const Item: any;
+// declare const Roll: any;
+// declare const Token: any;
 
-declare const DocumentSheetConfig: any;
-declare const TokenDocument: any;
-declare const TextEditor: any;
+// declare const DocumentSheetConfig: any;
+// declare const TokenDocument: any;
+// declare const TextEditor: any;
 
-declare const foundry: {
-  CONST: any;
-  abstract: any;
-  config: any;
-  data: any;
-  documents: any;
-  packages: any;
-  utils: Utils;
-};
+// declare const foundry: {
+//   CONST: any;
+//   abstract: any;
+//   config: any;
+//   data: any;
+//   documents: any;
+//   packages: any;
+//   utils: Utils;
+// };
 
-declare const canvas: any;
-declare const game: any;
-declare const ui: any;
-declare const warpgate: any;
+// declare const canvas: any;
+// declare const game: any;
+// declare const ui: any;
+// declare const warpgate: any;
 
-declare function fromUuid(uuid: string, options: any = {}): Promise<any>;
-declare function fromUuidSync(uuid: string, options: any = {}): any;
+// declare function fromUuid(uuid: string, options: any = {}): Promise<any>;
+// declare function fromUuidSync(uuid: string, options: any = {}): any;
