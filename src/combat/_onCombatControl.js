b50ce3105b114b11b47f6218cd1239b8995d16d0
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-06-24 14:53:03 -0700

    Fixes roll all initiative not respecting system settings.

--- src/combat/_onCombatControl.js
+++ src/combat/_onCombatControl.js
@@ -14,16 +14,19 @@ export default async function _onCombatControl(event) {
     ? CONFIG.A5E.ROLL_MODE.ADVANTAGE
     : CONFIG.A5E.ROLL_MODE.NORMAL;
 
+  const reverseAlt = game.settings.get('a5e', 'reverseInitiativeAltBehavior');
+  const defaultAltBehavior = game.settings.get('a5e', 'reverseAltBehavior');
+
+  let skipRollDialog = defaultAltBehavior ? !event.altKey : event.altKey;
+  skipRollDialog = reverseAlt ? !skipRollDialog : skipRollDialog;
+
   rollMode = (event.metaKey || event.ctrlKey) ? CONFIG.A5E.ROLL_MODE.DISADVANTAGE : rollMode;
 
   if (fn) {
     await fn.bind(combat)({
       rollOptions: {
         rollMode,
-        skipRollDialog: game.settings.get(
-          'a5e',
-          'reverseInitiativeAltBehavior'
-        ) ? !event.altKey : event.altKey
+        skipRollDialog
       }
     });
   }
