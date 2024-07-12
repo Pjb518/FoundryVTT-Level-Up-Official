a36555ee6c11fa160df75c57fd65fbb1c45f9e28
Jenn <chrystamofdem@gmail.com>
2024-07-11 23:38:02 -0700

    Adds custom lighten and darken function

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,10 @@ const config = {
     svelte({
       preprocess: preprocess({
         scss: {
-          prependData: '@import "src/scss//old/base/_variables.scss";'
+          prependData: `@import "src/scss//old/base/_variables.scss";
+                        @import "src/scss/base/_variables.scss";
+                        @import "src/scss/base/_functions.scss";
+                        `
         },
         typescript: {
           tsconfigFile: './tsconfig.json'
