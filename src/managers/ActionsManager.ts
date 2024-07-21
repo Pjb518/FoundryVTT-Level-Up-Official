8de6bdcb17a0993542c1042c5cbf57c0aa73bdca
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-07-21 13:46:10 -0700

    WIP - Actions Retouch. - Starts working on data model for actions.

---
+++ src/managers/ActionsManager.ts
@@ -0,0 +1,12 @@
+import type { Action } from 'types/action';
+import type { BaseItemA5e } from '../documents/item/base';
+
+export class ActionsManager extends Map<string, Action> {
+  #item: BaseItemA5e;
+
+  constructor(item: BaseItemA5e) {
+    super();
+
+    this.#item = item;
+  }
+}
