a6e5c28b74b7f4f79272f77c9e624f8b8dc432f1
Jenn <chrystamofdem@gmail.com>
2024-07-28 19:56:51 -0700

    Fixed issues with parsing. - renamed config to args - added warnings and errors for mandatory argument issues

--- src/managers/A5eEnricherManager.ts
+++ src/managers/A5eEnricherManager.ts
@@ -14,10 +14,10 @@ class A5eEnricherManager {
     const enricherTypes = ['check', 'save'];
 
     CONFIG.TextEditor.enrichers.push({
-      // pattern: \[\[\/(?<type>\w+)(?<options>( +\w+=([\w\d]+|"[\w\d ]+"))*)\]\]
+      // pattern: \[\[\/(?<type>\w+)(?<argString>( +\w+=([\w\d]+|"[\w\d ]+"))*)\]\]
       // matches: [[/type arg1=val1 arg2=val2 arg3="val 3"]]
-      pattern: new RegExp(`\\[\\[\\/(?<type>${enricherTypes.join('|')})(?<config>( +\\w+=([\\w\\d]+|"[\\w\\d ]+"))*)\\]\\]`, 'gi'),
-      enricher: this.parseEnricherInput
+      pattern: new RegExp(`\\[\\[\\/(?<type>${enricherTypes.join('|')})(?<argString>( +\\w+=([\\w\\d]+|"[\\w\\d ]+"))*)\\]\\]`, 'gi'),
+      enricher: this.parseEnricherInput.bind(this)
     });
   }
 
@@ -29,64 +29,100 @@ class A5eEnricherManager {
    *                                       null to indicate that no replacement should be made.
   */
   parseEnricherInput(match, options) {
-    const { type, config } = match.groups;
+    const { type, argString } = match.groups;
 
-    const configArray = this.parseConfig(config);
+    const args = this.parseArguments(argString);
 
     if (type.toLowerCase() === 'check') {
-      console.log('CheckTest');
-      return this.enrichCheck(configArray, options);
+      return this.enrichCheck(args, options);
     }
     if (type.toLowerCase() === 'save') {
-      console.log('SaveTest');
-      return this.enrichSave(configArray, options);
+      return this.enrichSave(args, options);
     }
     return null;
   }
 
   /**
-   * Parses the configuration into array format and changes numbers to Number
-   * @param {string} config       The raw config string
-   * @returns {indexedArray}  An indexed array of config item tuples [arg, val]
+   * Parses the arguments into record format and parses into int if possible.
+   * @param {string} argString       The raw arguments string
+   * @returns {Record}  An indexed array of config item tuples [arg, val]
   */
-  parseConfig(config: string) {
-    const configItems = config.toLowerCase().split(' ').filter((item) => item);
-    const configArray: { [key: string]: string | number } = {};
-    for (const item of configItems) {
-      const configItem: (string | number)[] = item.split('=');
-      if (Number.isNumeric(configItem[1])) {
-        configItem[1] = Number(configItem[1]);
-      }
-      [configArray[configItem[0]]] = configItem;
-    }
-    return configArray;
+  parseArguments(argString: string) {
+    const args = argString.toLowerCase().split(' ').filter((item) => item);
+    const structured: Record<string, string | number> = {};
+
+    args.forEach((arg) => {
+      const [key, value] = arg.split('=').map((a) => a.trim());
+      structured[key] = Number.isNumeric(value) ? parseInt(value, 10) : value;
+    });
+
+    return structured;
   }
 
-  enrichCheck(configArray, options) {
-    if (!('skill' in configArray && configArray.skill in CONFIG.A5E.skills)
-      && !('ability' in configArray && configArray.ability in CONFIG.A5E.abilities)) {
-      // TODO: throw an error
-      console.log('Error: No valid skill or ability defined.');
+  enrichCheck(args, options) {
+    if (!args.skill && !args.ability) {
+      ui.notifications?.error('Enricher is missing both skill and ability.');
       return null;
     }
-    // TODO: roll
+    // Checks for a properly defined skill
+    if (args.skill) {
+      if (!Object.keys(CONFIG.A5E.skills).includes(args.skill)) {
+        ui.notifications?.error('Invalid skill name.');
+        return null;
+      }
+      if (args.ability && !Object.keys(CONFIG.A5E.abilities).includes(args.ability)) {
+        ui.notifications?.error('Invalid ability name.');
+        return null;
+      }
+      // If not using default ability
+      if (args.ability) {
+        return null;
+      }
+
+      // using default ability for skill
+      // TODO: Do Stuff
+      return null;
+    }
+    // Check if the ability is proper
+    if (!Object.keys(CONFIG.A5E.abilities).includes(args.ability)) {
+      ui.notifications?.error('Invalid ability name.');
+      return null;
+    }
+
+    // This means only the ability save is left.
+    // TODO: Do Stuff
     return null;
   }
 
-  enrichSave(configArray, options) {
-    const saveTypes = ['death', 'concentration'];
-    if (!('type' in configArray && (configArray.type in saveTypes || configArray.type in CONFIG.A5E.abilities))
-      && !('ability' in configArray && configArray.ability in CONFIG.A5E.abilities)) {
-      // TODO: throw an error
-      console.log('Error: No valid type or ability defined.');
+  enrichSave(args, options) {
+    const saveTypes: string[] = ['death', 'concentration'];
+
+    if (!args.type && !args.ability) {
+      ui.notifications?.error('Enricher is missing both type and ability.');
       return null;
     }
-    if (('type' in configArray) && ('ability' in configArray)) {
-      // TODO: throw a warning
-      console.log('Warning: Unnecessary ability defined.');
+
+    // Is either concentration or death save
+    if (args.type) {
+      if (!saveTypes.includes(args.type)) {
+        ui.notifications?.error('Invalid save type.');
+        return null;
+      }
+      if (args.ability) {
+        ui.notifications?.warn('Unnecessary ability argument provided.');
+      }
+      // TODO: Do Stuff
       return null;
     }
-    // TODO: roll
+
+    // Check if the ability is proper
+    if (!Object.keys(CONFIG.A5E.abilities).includes(args.ability)) {
+      ui.notifications?.error('Invalid ability name.');
+      return null;
+    }
+
+    // This means only the ability save is left.
+    // TODO: Do Stuff
     return null;
   }
 }
