c5612ced46c5f6f3c5056e9fa3152ce07c77a1f5
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-08-12 13:58:17 -0700

    Updates chat card background color.

--- src/apps/chat/ItemCard.svelte
+++ src/apps/chat/ItemCard.svelte
@@ -322,6 +322,48 @@
         }
     }
 
+    function reevaluateCritMode() {
+        const isCrit = zip($message.rolls, $message.system.rollData).some(
+            ([roll, rollData]) => {
+                if (rollData.type !== "attack") return false;
+
+                const d20Roll = roll.terms.find((term) => term.faces === 20);
+
+                if (!d20Roll) return false;
+
+                return d20Roll.results.some(
+                    ({ result, active }) =>
+                        active && result >= (rollData.critThreshold ?? 20),
+                );
+            },
+        );
+
+        if (isCrit === undefined || isCrit === null) return;
+
+        toggleCriticalDamage(isCrit ? 1 : 0);
+    }
+
+    function toggleCriticalDamage(newCritMode) {
+        const rolls = zip($message.rolls, $message.system.rollData).map(
+            ([roll, rollData]) => {
+                if (rollData.type !== "damage") return roll;
+                if (!rollData.canCrit ?? true) return roll;
+                if (!rollData.critRoll || !rollData.baseRoll) return roll;
+
+                if (newCritMode === 1) return Roll.fromData(rollData.critRoll);
+                if (newCritMode === 0) return Roll.fromData(rollData.baseRoll);
+
+                if (rollData.baseRoll.formula === roll.formula) {
+                    return Roll.fromData(rollData.critRoll);
+                }
+
+                return Roll.fromData(rollData.baseRoll);
+            },
+        );
+
+        $message.update({ rolls });
+    }
+
     const message = new TJSDocument(messageDocument);
     const { system } = $message;
 
@@ -343,9 +385,8 @@
     const hasRolls = rolls.length;
     const item = fromUuidSync($message.system.itemId ?? "");
     const dispatch = createEventDispatcher();
-    const hideDescription = false; // TODO: Update this
-
-    console.log(prompts);
+    let hideDescription =
+        game.settings.get("a5e", "hideChatDescriptionsByDefault") ?? false;
 
     setContext("message", message);
 
