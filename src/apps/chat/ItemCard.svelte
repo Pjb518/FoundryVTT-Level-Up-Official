27561240aa9678c6adadf45fd40bd4d7b62033e1
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-08-16 02:33:56 -0700

    Adds repeat roll for roll chat cards. Moves dice reevaluation methods to their own files.

--- src/apps/chat/ItemCard.svelte
+++ src/apps/chat/ItemCard.svelte
@@ -9,13 +9,14 @@
 
     import constructRollFormula from "../../dice/constructRollFormula";
     import getKeyPressAsOptions from "../handlers/getKeyPressAsOptions";
-    import getExpertiseDieSize from "../../utils/getExpertiseDieSize";
     import getPromptTitle from "../dataPreparationHelpers/cardPrompts/getPromptTitle";
     import getPromptSubtitle from "../dataPreparationHelpers/cardPrompts/getPromptSubtitle";
     import preparePrompts from "../dataPreparationHelpers/cardPrompts/preparePrompts";
     import prepareRolls from "../dataPreparationHelpers/cardRolls/prepareRolls";
     import prepareSelectedTokenActors from "../dataPreparationHelpers/prepareSelectedTokenActors";
     import pressedKeysStore from "../../stores/pressedKeysStore";
+    import toggleExpertiseDice from "../dataPreparationHelpers/cardRolls/toggleExpertiseDice";
+    import toggleRollMode from "../dataPreparationHelpers/cardRolls/toggleRollMode";
     import zip from "../../utils/zip";
 
     import ItemSummary from "../components/itemSummaries/ItemSummary.svelte";
@@ -26,7 +27,6 @@
     export let messageDocument;
 
     function getSubtitle(name, actionName) {
-        console.log(name, actionName);
         if (!actionName || typeof actionName !== "string") return null;
         if (name.trim() === actionName.trim()) return null;
 
@@ -62,207 +62,13 @@
         return null;
     }
 
-    async function toggleExpertiseDice(rollIndex, expertiseDice) {
-        const { rollData } = $message?.system ?? {};
-        const [originalRoll, originalRollData] = rolls[rollIndex];
-        const originalExpertiseDice = originalRollData.expertiseDice;
-
-        if (originalExpertiseDice === expertiseDice) return;
-
-        const newRoll = new Roll(
-            originalRoll.formula,
-            { ...originalRoll.data },
-            { ...originalRoll.options },
-        );
-
-        newRoll.terms = [...originalRoll.terms];
-
-        const expertiseDieIndex = newRoll.terms.findIndex(
-            (term) => term.options?.flavor === "Expertise Die",
-        );
-
-        const expertiseDie =
-            expertiseDieIndex !== -1 ? newRoll.terms[expertiseDieIndex] : null;
-
-        // If the current roll already has an expertise die as part of the roll, log the
-        // result so that we can reuse it if the expertise die is changed back to the
-        // original type.
-        if (expertiseDie) {
-            rollData[rollIndex].expertiseDiceResults ??= {};
-            rollData[rollIndex].expertiseDiceResults[originalExpertiseDice] ??=
-                newRoll.terms[expertiseDieIndex]?.results[0]?.result;
-        }
-
-        // If the user is removing expertise dice from the roll, remove both the die and
-        // the preceding operator term.
-        if (!expertiseDice) {
-            newRoll.terms.splice(expertiseDieIndex - 1, 2);
-        }
-
-        // If the current roll has no expertise dice, append a new expertise die to the
-        // roll terms alone with a + operator; Otherwise, replace the current expertise
-        // die roll with a new one, using previously stored results if available.
-        else {
-            const newExpertiseDieRoll = new foundry.dice.terms.Die({
-                number: 1,
-                faces: parseInt(getExpertiseDieSize(expertiseDice).slice(2), 10),
-                options: {
-                    flavor: "Expertise Die",
-                },
-            });
-
-            const pastResults = rollData[rollIndex].expertiseDiceResults ?? {};
-
-            if (pastResults[expertiseDice]) {
-                newExpertiseDieRoll.results.push({
-                    active: true,
-                    result: pastResults[expertiseDice],
-                });
-
-                newExpertiseDieRoll._evaluated = true;
-            } else {
-                await newExpertiseDieRoll.evaluate();
-            }
-
-            if (!originalExpertiseDice) {
-                newRoll.terms.push(
-                    new foundry.dice.terms.OperatorTerm({ operator: "+" }),
-                    newExpertiseDieRoll,
-                );
-            } else {
-                newRoll.terms.splice(expertiseDieIndex, 1, newExpertiseDieRoll);
-            }
-
-            if (game.modules?.get("dice-so-nice")?.active) {
-                // @ts-expect-error
-                game.dice3d.showForRoll(
-                    Roll.fromTerms([newExpertiseDieRoll]),
-                    game.users.get($message.user.id),
-                    true,
-                    $message.whisper,
-                    $message.blind,
-                    null,
-                    $message.speaker,
-                );
-            }
-        }
-
-        // Update the formula and total information for the new roll and evaluate the roll
-        newRoll._formula = Roll.getFormula(newRoll.terms);
-        newRoll._total = newRoll._evaluateTotal();
-        await newRoll.evaluate();
-
-        $message.rolls.splice(rollIndex, 1, newRoll);
-        rollData[rollIndex].expertiseDice = expertiseDice;
-
-        await $message.update({
-            rolls: $message.rolls,
-            "system.rollData": rollData,
-        });
+    async function _toggleExpertiseDice(rollIndex, expertiseDice) {
+        toggleExpertiseDice($message, rolls, rollIndex, expertiseDice);
     }
 
-    async function toggleRollMode(rollIndex, rollMode) {
-        const [originalRoll, originalRollData] = rolls[rollIndex];
-        const originalRollMode = originalRollData.rollMode;
-
-        if (originalRollMode === rollMode) return;
-
-        const newRoll = new Roll(
-            originalRoll.formula,
-            { ...originalRoll.data },
-            { ...originalRoll.options },
-        );
-
-        newRoll.terms = [...originalRoll.terms];
-
-        const d20Term = newRoll.terms[0];
-        const originalResultsLength = d20Term.results.length;
-
-        // If there isn't a secondary d20 result in the message rollData, store the
-        // secondary die result for the current die if available.
-        if (d20Term.number === 2) {
-            const secondDieResult = d20Term.results[1].result;
-            $message.system.rollData[rollIndex].secondDieResult ??= secondDieResult;
-        }
-
-        // Remove kh and kl modifiers
-        d20Term.modifiers = d20Term.modifiers.filter(
-            (modifier) => !["kh", "kl"].includes(modifier),
-        );
-
-        if (!rollMode) {
-            // Set the number of dice to 1 and keep only the first die
-            d20Term.number = 1;
-            d20Term.results = [d20Term.results.shift()];
-        } else {
-            // Add a kh modifier for advantage and a kl modifier for disadvantage
-            d20Term.modifiers.push(rollMode === 1 ? "kh" : "kl");
-
-            // Add a second die if there isn't one already
-            if (!originalRollMode) {
-                d20Term.number = 2;
-
-                const { secondDieResult } = $message?.system?.rollData[rollIndex];
-
-                // If a secondary d20 roll exists in the rollData, use that; otherwise,
-                // generate a new result.
-                if (secondDieResult) {
-                    d20Term.results.push({ result: secondDieResult });
-                } else {
-                    await d20Term.roll();
-                }
-            }
-        }
-
-        // Reset all the metadata for the new d20Term results
-        d20Term.results.forEach((term) => {
-            term.active = true;
-            delete term.discarded;
-            delete term.indexThrow;
-        });
-
-        d20Term._evaluateModifiers();
-
-        // Update the formula and total information for the new roll and evaluate the roll
-        newRoll._formula = Roll.getFormula(newRoll.terms);
-        newRoll._total = newRoll._evaluateTotal();
-        await newRoll.evaluate();
-
-        if (
-            game.modules.get("dice-so-nice")?.active &&
-            d20Term.results.length > originalResultsLength
-        ) {
-            const fakeD20Roll = Roll.fromTerms([
-                foundry.dice.terms.RollTerm.fromData(d20Term.toJSON()),
-            ]);
-
-            fakeD20Roll.terms[0].number = fakeD20Roll.terms[0].results.length;
-            fakeD20Roll.terms[0].results = fakeD20Roll.terms[0].results.filter(
-                (_, index) => index > 0,
-            );
-
-            game.dice3d.showForRoll(
-                fakeD20Roll,
-                game.users.get($message.user.id),
-                true,
-                $message.whisper,
-                $message.blind,
-                null,
-                $message.speaker,
-            );
-        }
-
-        // Replace the old roll with the new one in the message rolls array
-        $message.rolls.splice(rollIndex, 1, newRoll);
-
-        // Update the corresponding rollData object
-        $message.system.rollData[rollIndex].rollMode = rollMode;
-
-        // Permanently update the message with the new data
-        await $message.update({
-            rolls: $message.rolls,
-            "system.rollData": $message.system.rollData,
-        });
+    async function _toggleRollMode(rollIndex, rollMode) {
+        const [, originalRollData] = rolls[rollIndex];
+        toggleRollMode($message, rolls, rollIndex, rollMode);
 
         // Use the new attack roll information to determine whether to display crit damage
         if (originalRollData.type === "attack") reevaluateCritMode();
@@ -458,9 +264,9 @@
                         {roll}
                         {rollData}
                         --a5e-roll-color={prepareRollColor(rollData)}
-                        on:toggleRollMode={({ detail }) => toggleRollMode(i, detail)}
+                        on:toggleRollMode={({ detail }) => _toggleRollMode(i, detail)}
                         on:toggleExpertiseDice={({ detail }) =>
-                            toggleExpertiseDice(i, detail)}
+                            _toggleExpertiseDice(i, detail)}
                     />
 
                     {#if rolls.length > 1 && rollData.type === "attack"}
--- src/apps/chat/RollCard.svelte
+++ src/apps/chat/RollCard.svelte
@@ -1,17 +1,53 @@
 <svelte:options accessors={true} />
 
-<script lang="ts">
+<script>
     import { TJSDocument } from "#runtime/svelte/store/fvtt/document";
     import { setContext } from "svelte";
 
     import prepareRolls from "../dataPreparationHelpers/cardRolls/prepareRolls";
+    import toggleExpertiseDice from "../dataPreparationHelpers/cardRolls/toggleExpertiseDice";
+    import toggleRollMode from "../dataPreparationHelpers/cardRolls/toggleRollMode";
 
     import RollCardHeader from "./RollCardHeader.svelte";
     import RollSummary from "./body/RollSummary.svelte";
 
     export let messageDocument;
 
-    function repeatRoll() {}
+    async function repeatRoll() {
+        const newRolls = await Promise.all(
+            $message.rolls.map((r) => new Roll(r.formula).roll()),
+        );
+
+        const chatData = {
+            author: $message?.author,
+            speaker: $message.speaker,
+            sound: $message.sound,
+            rolls: newRolls,
+            rollMode: $message.rollMode,
+            system: {
+                actorId: $message.system.actorId,
+                actorName: $message.system.actorName,
+                img: $message.system.img,
+                rollData: foundry.utils.duplicate($message.system.rollData),
+                rollType: $message.system.rollType,
+            },
+            type: "roll",
+        };
+
+        // @ts-expect-error
+        ChatMessage.applyRollMode(chatData, $message.rollMode);
+        // @ts-expect-error
+        const chatCard = await ChatMessage.create(chatData);
+        return chatCard;
+    }
+
+    function _toggleExpertiseDice(rollIndex, expertiseDice) {
+        toggleExpertiseDice($message, rolls, rollIndex, expertiseDice);
+    }
+
+    async function _toggleRollMode(rollIndex, rollMode) {
+        toggleRollMode($message, rolls, rollIndex, rollMode);
+    }
 
     const message = new TJSDocument(messageDocument);
     const { system } = $message;
@@ -32,7 +68,12 @@
 <article class="a5e-chat-card__body">
     <section class="rolls">
         {#each rolls ?? [] as [roll, rollData], i}
-            <RollSummary {roll} {rollData} />
+            <RollSummary
+                {roll}
+                {rollData}
+                on:toggleRollMode={({ detail }) => _toggleRollMode(i, detail)}
+                on:toggleExpertiseDice={({ detail }) => _toggleExpertiseDice(i, detail)}
+            />
         {/each}
     </section>
 </article>
