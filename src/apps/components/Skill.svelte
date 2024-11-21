ff325c12996c2ecd33c2d1718492663efdd6cecf
nchezrony <nchezrony@gmail.com>
2024-11-21 08:38:01 -0600

    Corrects skill sheet calculation.

--- src/apps/components/Skill.svelte
+++ src/apps/components/Skill.svelte
@@ -71,7 +71,7 @@
     let showDeterministicBonus =
         $actor.flags?.a5e?.includeAbilityModifiersForSkills ?? true;
 
-    $: abilityBonus = $actor.system.abilities[skill.ability].check.deterministicBonus;
+    $: abilityBonus = $actor.system.abilities[skill.ability].check.mod;
 
     $: skillBonus = getSkillBonus($actor, showDeterministicBonus);
     $: proficiencyLevel = getProficiencyLevel($actor, skill);
@@ -123,7 +123,9 @@
 
     <div class="skill__mod-wrapper">
         <span class="skill__mod">
-            {replaceHyphenWithMinusSign(skillBonus)}
+            {replaceHyphenWithMinusSign(
+                showDeterministicBonus ? skillBonus + abilityBonus : skillBonus
+            )}
         </span>
 
         {#if $actor.flags.a5e?.showPassiveScores ?? true}
