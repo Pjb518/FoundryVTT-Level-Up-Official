b808242da31304e39171ca482f7089c7df2ff84f
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-12-11 11:52:22 -0800

    Fixes getSavingThrowLabel

--- src/utils/summaries/getSavingThrowLabel.ts
+++ src/utils/summaries/getSavingThrowLabel.ts
@@ -6,23 +6,23 @@ export default function getSavingThrowLabel(action: Action) {
 	if (foundry.utils.isEmpty(action)) return '';
 
 	const prompts = action?.prompts;
+	if (foundry.utils.isEmpty(prompts) || Object.keys(prompts).length > 1) return null;
 
-	if (foundry.utils.isEmpty(prompts) || Object.keys(prompts).length > 1 ) return null;
+	const savingThrowLabel = Object.values(prompts ?? {})
+		.reduce((acc, prompt) => {
+			if (prompt.type !== 'savingThrow') return acc;
 
-	const promptID = Object.keys(prompts);
-	const ability = prompts[promptID]?.ability;
-	const type = prompts[promptID]?.type;
-	const onSave = prompts[promptID]?.onSave;
+			// @ts-expect-error
+			const onSave = prompt.onSave;
+			// @ts-expect-error
+			const ability = prompt.ability;
 
-	let savingThrowLabel = '';
+			if (onSave) acc.push(`${localize(CONFIG.A5E.abilities[ability])} (${onSave})`);
+			else acc.push(`${localize(CONFIG.A5E.abilities[ability])}`);
 
-	if(type === "savingThrow"){
-		if (onSave) {
-			savingThrowLabel = `${localize(CONFIG.A5E.abilities[ability])} (${onSave})`;
-		} else {
-			savingThrowLabel = `${localize(CONFIG.A5E.abilities[ability])}`;
-		}
-	}
+			return acc;
+		}, [] as string[])
+		.join(', ');
 
 	return savingThrowLabel;
 }
