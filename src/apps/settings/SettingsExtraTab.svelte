76b0b190d73a13c285160d076f41909e73f8aa93
NekroDarkmoon <nekrodarkmoon@gmail.com>
2024-12-16 06:23:15 -0800

    WIP - Flips settings.

--- src/apps/settings/SettingsExtraTab.svelte
+++ src/apps/settings/SettingsExtraTab.svelte
@@ -17,101 +17,87 @@
     let showVRCSpecialties = settings.getStore("showVRCSpecialties");
     let showVRCTechLevel = settings.getStore("showVRCTechLevel");
     let useCredits = settings.getStore("useCredits");
-    
 </script>
 
-<Section
-    heading="A5E.settings.sectionHeader.vrc"
-    --a5e-section-body-gap="0.5rem"
->
-        <FieldWrapper hint="A5E.settings.hints.showVRCImplants">
-            <Checkbox
-                label="A5E.settings.showVRCImplants"
-                checked={updates.get("showVRCImplants") ??
-                    $showVRCImplants ??
-                    true}
-                on:updateSelection={({ detail }) => {
-                    updates.set("showVRCImplants", detail);
-                    reload = true;
-                }}
-            />
-        </FieldWrapper>
+<Section heading="A5E.settings.sectionHeader.vrc" --a5e-section-body-gap="0.5rem">
+    <FieldWrapper hint="A5E.settings.hints.showVRCImplants">
+        <Checkbox
+            label="A5E.settings.showVRCImplants"
+            checked={updates.get("showVRCImplants") ?? $showVRCImplants ?? false}
+            on:updateSelection={({ detail }) => {
+                updates.set("showVRCImplants", detail);
+                reload = true;
+            }}
+        />
+    </FieldWrapper>
 
-        <FieldWrapper hint="A5E.settings.hints.showVRCProficiencies">
-            <Checkbox
-                label="A5E.settings.showVRCProficiencies"
-                checked={updates.get("showVRCProficiencies") ??
-                    $showVRCProficiencies ??
-                    true}
-                on:updateSelection={({ detail }) => {
-                    updates.set("showVRCProficiencies", detail);
-                    reload = true;
-                }}
-            />
-        </FieldWrapper>
+    <FieldWrapper hint="A5E.settings.hints.showVRCProficiencies">
+        <Checkbox
+            label="A5E.settings.showVRCProficiencies"
+            checked={updates.get("showVRCProficiencies") ??
+                $showVRCProficiencies ??
+                false}
+            on:updateSelection={({ detail }) => {
+                updates.set("showVRCProficiencies", detail);
+                reload = true;
+            }}
+        />
+    </FieldWrapper>
 
-        <FieldWrapper hint="A5E.settings.hints.showVRCPsionicDisciplines">
-            <Checkbox
-                label="A5E.settings.showVRCPsionicDisciplines"
-                checked={updates.get("showVRCPsionicDisciplines") ??
-                    $showVRCPsionicDisciplines ??
-                    true}
-                on:updateSelection={({ detail }) => {
-                    updates.set("showVRCPsionicDisciplines", detail);
-                    reload = true;
-                }}
-            />
-        </FieldWrapper>
+    <FieldWrapper hint="A5E.settings.hints.showVRCPsionicDisciplines">
+        <Checkbox
+            label="A5E.settings.showVRCPsionicDisciplines"
+            checked={updates.get("showVRCPsionicDisciplines") ??
+                $showVRCPsionicDisciplines ??
+                false}
+            on:updateSelection={({ detail }) => {
+                updates.set("showVRCPsionicDisciplines", detail);
+                reload = true;
+            }}
+        />
+    </FieldWrapper>
 
-        <FieldWrapper hint="A5E.settings.hints.showVRCSkills">
-            <Checkbox
-                label="A5E.settings.showVRCSkills"
-                checked={updates.get("showVRCSkills") ??
-                    $showVRCSkills ??
-                    true}
-                on:updateSelection={({ detail }) => {
-                    updates.set("showVRCSkills", detail);
-                    reload = true;
-                }}
-            />
-        </FieldWrapper>
+    <FieldWrapper hint="A5E.settings.hints.showVRCSkills">
+        <Checkbox
+            label="A5E.settings.showVRCSkills"
+            checked={updates.get("showVRCSkills") ?? $showVRCSkills ?? false}
+            on:updateSelection={({ detail }) => {
+                updates.set("showVRCSkills", detail);
+                reload = true;
+            }}
+        />
+    </FieldWrapper>
 
-        <FieldWrapper hint="A5E.settings.hints.showVRCSpecialties">
-            <Checkbox
-                label="A5E.settings.showVRCSpecialties"
-                checked={updates.get("showVRCSpecialties") ??
-                    $showVRCSpecialties ??
-                    true}
-                on:updateSelection={({ detail }) => {
-                    updates.set("showVRCSpecialties", detail);
-                    reload = true;
-                }}
-            />
-        </FieldWrapper>
+    <FieldWrapper hint="A5E.settings.hints.showVRCSpecialties">
+        <Checkbox
+            label="A5E.settings.showVRCSpecialties"
+            checked={updates.get("showVRCSpecialties") ?? $showVRCSpecialties ?? false}
+            on:updateSelection={({ detail }) => {
+                updates.set("showVRCSpecialties", detail);
+                reload = true;
+            }}
+        />
+    </FieldWrapper>
 
-        <FieldWrapper hint="A5E.settings.hints.showVRCTechLevel">
-            <Checkbox
-                label="A5E.settings.showVRCTechLevel"
-                checked={updates.get("showVRCTechLevel") ??
-                    $showVRCTechLevel ??
-                    true}
-                on:updateSelection={({ detail }) => {
-                    updates.set("showVRCTechLevel", detail);
-                    reload = true;
-                }}
-            />
-        </FieldWrapper>
+    <FieldWrapper hint="A5E.settings.hints.showVRCTechLevel">
+        <Checkbox
+            label="A5E.settings.showVRCTechLevel"
+            checked={updates.get("showVRCTechLevel") ?? $showVRCTechLevel ?? false}
+            on:updateSelection={({ detail }) => {
+                updates.set("showVRCTechLevel", detail);
+                reload = true;
+            }}
+        />
+    </FieldWrapper>
 
-        <FieldWrapper hint="A5E.settings.hints.useCredits">
-            <Checkbox
-                label="A5E.settings.useCredits"
-                checked={updates.get("useCredits") ??
-                    $useCredits ??
-                    true}
-                on:updateSelection={({ detail }) => {
-                    updates.set("useCredits", detail);
-                    reload = true;
-                }}
-            />
-        </FieldWrapper>
+    <FieldWrapper hint="A5E.settings.hints.useCredits">
+        <Checkbox
+            label="A5E.settings.useCredits"
+            checked={updates.get("useCredits") ?? $useCredits ?? false}
+            on:updateSelection={({ detail }) => {
+                updates.set("useCredits", detail);
+                reload = true;
+            }}
+        />
+    </FieldWrapper>
 </Section>
