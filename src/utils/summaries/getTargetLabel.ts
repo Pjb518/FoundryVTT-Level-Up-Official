a59c6dea9412865bfe42fac73a5e1ebe6234a215
nchezrony <nchezrony@gmail.com>
2025-01-29 06:15:03 -0600

    Adds text box when action target is set to Other. Adds heard and seen checkboxes for targeting.

--- src/utils/summaries/getTargetLabel.ts
+++ src/utils/summaries/getTargetLabel.ts
@@ -7,12 +7,19 @@ export default function getTargetLabel(action: Action) {
 
 	if (foundry.utils.isEmpty(target) || !target.type || target.type === 'none') return '';
 
+	let heardOrSeen = '';
+
+	if (target.heard && !target.seen) heardOrSeen = ` (${localize('A5E.TargetHeard')})`;
+	else if (target.seen && !target.heard) heardOrSeen = ` (${localize('A5E.TargetSeen')})`;
+	else if (target.heard && target.seen) heardOrSeen = ` (${localize('A5E.TargetHeard')} or ${localize('A5E.TargetSeen')})`;
+
 	if (target.type === 'self') return localize('A5E.TargetSelf');
-	if (target.type === 'other') return localize('A5E.TargetOther');
+	if (target.type === 'other'  && target.otherText === '') return localize('A5E.TargetOther') + heardOrSeen;
+	if (target.type === 'other'  && target.otherText !== '') return target.otherText + heardOrSeen;
 
 	if (target.quantity === 0 || target.quantity > 1) {
-		return `${target.quantity} ${CONFIG.A5E.targetTypesPlural[target.type]}`;
+		return `${target.quantity} ${CONFIG.A5E.targetTypesPlural[target.type]}` + heardOrSeen;
 	}
 
-	return `${target.quantity || 1} ${CONFIG.A5E.targetTypes[target.type]}`;
+	return `${target.quantity || 1} ${CONFIG.A5E.targetTypes[target.type]}` + heardOrSeen;
 }
