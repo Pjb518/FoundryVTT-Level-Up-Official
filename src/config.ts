ee3c39dfdb6bb8718fb64299599033130314a13b
nchezrony <nchezrony@gmail.com>
2025-01-30 20:19:34 -0600

    VRC Archetypes and Backgrounds (#514)
    * Adds trooper class doc and features.
    
    * Removes incomplete file.
    
    * Adds psion archetype docs and features.
    
    * Adds trooper archetype docs and features from VRC.
    
    * Adds psyknight archetype features and docs from VRC.
    
    * Adds additional Spacefaring ports and Strength to material properties.
    
    * Updates Scientist class doc and Scrutnize feature to use class resources. Adds Scientist archetype docs and features.
    
    * Adds Scout archetype docs and features.
    
    * Fixes Keytar name in proficiencies. Adds Shawm and Viol to gear.
    
    * Adds backgrounds and features from VRC.

--- src/config.ts
+++ src/config.ts
@@ -613,6 +613,7 @@ const knackTypes = {
 	scientist: 'Scientific Praxes',
 	scout: 'Clever Trick',
 	sorcerer: 'Arcane Innovation',
+  trooper: 'Drill',
 	warlock: 'Secret of Arcana',
 	wielder: 'Artifact Whisper',
 	witch: 'Magical Mystery',
@@ -707,6 +708,7 @@ const materialProperties = {
 	rust: 'A5E.MaterialPropertyRust',
 	silvered: 'A5E.MaterialPropertySilvered',
 	spacefaring: 'A5E.MaterialPropertySpacefaring',
+	strength: 'A5E.MaterialPropertyStrength',
 	underarmor: 'A5E.MaterialPropertyUnderarmor',
 	weighty: 'A5E.MaterialPropertyWeighty',
 	wild: 'A5E.MaterialPropertyWild',
@@ -726,6 +728,8 @@ const modPorts = {
 	4: '4',
 	5: '5',
 	6: '6',
+	7: '7',
+	8: '8'
 };
 
 /**
@@ -856,6 +860,7 @@ const repairTools = {
 	notRepairable: 'A5E.repairTools.notRepairable',
 	sewingKit: 'A5E.repairTools.sewingKit',
 	smithsTools: 'A5E.repairTools.smithsTools',
+	tinkersTools: 'A5E.repairTools.tinkersTools',
 };
 
 const resourceRecoveryOptions = {
