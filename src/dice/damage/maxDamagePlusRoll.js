ac88cf48c242a0d8922cd564574e8bf57df12fff
NekroDarkmoon <nekrodarkmoon@gmail.com>
2025-02-01 19:35:35 -0800

    Fixes doubling of modifiers in maxDicePlusRoll dice setting.

--- src/dice/damage/maxDamagePlusRoll.js
+++ src/dice/damage/maxDamagePlusRoll.js
@@ -1,9 +1,31 @@
 export default async function maxDamage(baseRoll) {
 	const maxRoll = baseRoll.clone();
-	await maxRoll.evaluate({ maximize: true });
+	// await maxRoll.evaluate({ maximize: true });
+
+	const newTerms = [];
+	let wasDie = false;
+	maxRoll.terms.forEach((term, idx) => {
+		if (term instanceof foundry.dice.terms.Die) {
+			newTerms.push(term);
+			wasDie = true;
+			return;
+		}
+
+		if (term instanceof foundry.dice.terms.OperatorTerm && wasDie === true) {
+			const nextTerm = maxRoll.terms.at(idx + 1);
+
+			if (nextTerm && nextTerm instanceof foundry.dice.terms.Die) {
+				newTerms.push(term);
+				wasDie = false;
+			}
+		}
+	});
+
+	const newRoll = Roll.fromTerms(newTerms);
+	await newRoll.evaluate({ maximize: true });
 
 	return [
-		...maxRoll.terms,
+		...newRoll.terms,
 		await new foundry.dice.terms.OperatorTerm({ operator: '+' }).evaluate(),
 		...baseRoll.terms,
 	];
