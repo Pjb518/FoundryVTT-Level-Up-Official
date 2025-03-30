82fc0accba426974218821b8098b171436bffeb1
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2025-03-30 13:59:49 -0700

    Disables Compendium sheets temporarily.

--- src/hooks/setupFancySheets.ts
+++ src/hooks/setupFancySheets.ts
@@ -1,80 +1,86 @@
-import * as indexFunctions from '../utils/createIndexes';
+import * as indexFunctions from "../utils/createIndexes";
 
 const fancyPackData = {
-	archetypes: {
-		packs: ['a5e.a5e-archetypes'],
-		func: indexFunctions.createArchetypeLikeIndex,
-	},
-	classFeatures: {
-		packs: ['a5e.a5e-class-features'],
-		func: indexFunctions.createClassFeatureLikeIndex,
-	},
-	manuevers: {
-		packs: ['a5e.a5e-maneuvers'],
-		func: indexFunctions.createManeuverLikeIndex,
-	},
-	monsters: {
-		packs: ['a5e.a5e-monsters'],
-		func: indexFunctions.createMonsterLikeIndex,
-	},
-	spells: {
-		packs: ['a5e.a5e-spells'],
-		func: indexFunctions.createSpellLikeIndex,
-	},
-	dnd5eMonsters: {
-		packs: ['a5e.dnd5e-monsters'],
-		func: indexFunctions.create5eMonsterLikeIndex,
-	},
-	dnd5eSpells: {
-		packs: ['a5e.dnd5e-spells'],
-		func: indexFunctions.create5eSpellLikeIndex,
-	},
-	objects: {
-		packs: ['a5e.a5e-adventuring-gear', 'a5e.dnd5e-items'],
-		func: indexFunctions.createObjectLikeIndex,
-	},
+  archetypes: {
+    packs: ["a5e.a5e-archetypes"],
+    func: indexFunctions.createArchetypeLikeIndex,
+  },
+  classFeatures: {
+    packs: ["a5e.a5e-class-features"],
+    func: indexFunctions.createClassFeatureLikeIndex,
+  },
+  manuevers: {
+    packs: ["a5e.a5e-maneuvers"],
+    func: indexFunctions.createManeuverLikeIndex,
+  },
+  monsters: {
+    packs: ["a5e.a5e-monsters"],
+    func: indexFunctions.createMonsterLikeIndex,
+  },
+  spells: {
+    packs: ["a5e.a5e-spells"],
+    func: indexFunctions.createSpellLikeIndex,
+  },
+  dnd5eMonsters: {
+    packs: ["a5e.dnd5e-monsters"],
+    func: indexFunctions.create5eMonsterLikeIndex,
+  },
+  dnd5eSpells: {
+    packs: ["a5e.dnd5e-spells"],
+    func: indexFunctions.create5eSpellLikeIndex,
+  },
+  objects: {
+    packs: ["a5e.a5e-adventuring-gear", "a5e.dnd5e-items"],
+    func: indexFunctions.createObjectLikeIndex,
+  },
 };
 
 const autoMappingConfig = {
-	npc: indexFunctions.createMonsterLikeIndex,
-	spell: indexFunctions.createSpellLikeIndex,
-	object: indexFunctions.createObjectLikeIndex,
-	feature: indexFunctions.createClassFeatureLikeIndex,
-	archetype: indexFunctions.createArchetypeLikeIndex,
+  npc: indexFunctions.createMonsterLikeIndex,
+  spell: indexFunctions.createSpellLikeIndex,
+  object: indexFunctions.createObjectLikeIndex,
+  feature: indexFunctions.createClassFeatureLikeIndex,
+  archetype: indexFunctions.createArchetypeLikeIndex,
 };
 
 export default function setupFancySheets() {
-	const definedPacks = new Set();
+  const definedPacks = new Set();
 
-	// Setup defined packs
-	Object.values(fancyPackData).forEach(({ packs, func }) => {
-		packs.forEach((pack) => {
-			func(pack);
-			definedPacks.add(pack);
-		});
-	});
+  // TODO: Fix this before v13 release
+  return;
 
-	// Setup undefined packs
-	// @ts-expect-error
-	if (!game.settings.storage.get('world').getItem('a5e.autoApplyFancySheets')) return;
+  // Setup defined packs
+  Object.values(fancyPackData).forEach(({ packs, func }) => {
+    packs.forEach((pack) => {
+      func(pack);
+      definedPacks.add(pack);
+    });
+  });
 
-	for (const pack of game.packs) {
-		const id = pack.metadata.id || pack.collection;
+  // Setup undefined packs
+  // @ts-expect-error
+  if (!game.settings.storage.get("world").getItem("a5e.autoApplyFancySheets"))
+    return;
 
-		if (!id) continue;
-		// if (definedPacks.has(id)) continue;
+  for (const pack of game.packs) {
+    const id = pack.metadata.id || pack.collection;
 
-		const documentType = pack.metadata.type;
-		if (!documentType) continue;
+    if (!id) continue;
+    // if (definedPacks.has(id)) continue;
 
-		// @ts-expect-error
-		const indexTypes: string[] = [...pack.index].map((index) => index.type).filter(Boolean);
-		if (!indexTypes.every((type: string) => indexTypes[0] === type)) continue;
+    const documentType = pack.metadata.type;
+    if (!documentType) continue;
 
-		const indexType = indexTypes[0];
-		const applyFunc = autoMappingConfig[indexType];
-		if (!applyFunc) continue;
+    // @ts-expect-error
+    const indexTypes: string[] = [...pack.index]
+      .map((index) => index.type)
+      .filter(Boolean);
+    if (!indexTypes.every((type: string) => indexTypes[0] === type)) continue;
 
-		applyFunc(id);
-	}
+    const indexType = indexTypes[0];
+    const applyFunc = autoMappingConfig[indexType];
+    if (!applyFunc) continue;
+
+    applyFunc(id);
+  }
 }
