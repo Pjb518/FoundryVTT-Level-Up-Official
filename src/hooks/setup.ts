4e45277f0c1069cfd75fb3c08d5eb37dd6d8aa5b
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2025-05-04 14:41:34 -0700

    Re-adds registration of settings.

--- src/hooks/setup.ts
+++ src/hooks/setup.ts
@@ -1,13 +1,13 @@
-import registerSystemSettings from '../settings';
-import registerConditionsConfig from '../config/registerConditionsConfig';
-import registerExtraContentConfig from '../config/registerExtraContentConfig';
-import setupFancySheets from './setupFancySheets';
-import updateGMTitle from './updateGMTitle';
+import registerSystemSettings from "../settings.ts";
+import registerConditionsConfig from "../config/registerConditionsConfig";
+import registerExtraContentConfig from "../config/registerExtraContentConfig";
+import setupFancySheets from "./setupFancySheets";
+import updateGMTitle from "./updateGMTitle";
 
 export default function setup() {
-	registerSystemSettings();
-	registerConditionsConfig();
-	registerExtraContentConfig();
-	setupFancySheets();
-	updateGMTitle();
+  registerSystemSettings();
+  registerConditionsConfig();
+  registerExtraContentConfig();
+  setupFancySheets();
+  updateGMTitle();
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,19 @@
 // import SettingsShim from './settings/SettingsShim';
 
 // import { gameSettings } from './settings/SettingsStore';
+import { settings } from "./settings/settings.ts";
+
+function parseAndRegisterSettings() {
+  for (const setting of settings) {
+    // @ts-ignore
+    game.settings.register(setting.namespace, setting.key, setting.options);
+  }
+}
 
 export default function registerSystemSettings() {
+  // Parse and register settings
+  parseAndRegisterSettings();
+
   // gameSettings.init();
   // game.settings.registerMenu('a5e', 'SystemSettings', {
   // 	name: 'System Settings',
