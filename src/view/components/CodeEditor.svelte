667ef34c6823982271bb2c5695bfab1bdab7b631
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2025-07-18 17:40:45 -0700

    WIP - On Macro tab.

---
+++ src/view/components/CodeEditor.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+    import { getContext, onMount } from "svelte";
+
+    type Props = {
+        applicationType?: string;
+        content: string;
+        document: any;
+        field: string;
+        onSave?: () => void;
+        [key: string]: any;
+    };
+
+    function handleSave() {
+        console.log(field, content);
+        document.update({ [`system.${field}`]: content });
+        onSave?.();
+    }
+
+    let {
+        applicationType = "sheet",
+        content,
+        document,
+        field,
+        onSave,
+        ...rest
+    }: Props = $props();
+
+    let codeMirrorContainerEl: HTMLElement;
+    let elem: any;
+
+    let application: any = getContext(applicationType);
+
+    const config = {
+        value: content,
+        language: "javascript",
+        indent: 2,
+    };
+
+    // Create Editor element and put it in the contents element.
+    onMount(async () => {
+        elem =
+            foundry.applications.elements.HTMLCodeMirrorElement.create(config);
+
+        codeMirrorContainerEl.innerHTML = elem.outerHTML;
+    });
+</script>
+
+<section class="a5e-macro-page">
+    <div class="a5e-code-editor">
+        <div
+            id="a5e-code-mirror-macro"
+            style="display: contents;"
+            class={rest.class ?? ""}
+            bind:this={codeMirrorContainerEl}
+        ></div>
+    </div>
+
+    <button
+        onclick={(e) => {
+            e.preventDefault();
+            handleSave();
+        }}
+    >
+        Save Macro
+    </button>
+</section>
+
+<style lang="scss">
+    .a5e-macro-page {
+        display: grid;
+        grid-template-rows: minmax(0, 1fr) min-content;
+        gap: 0.5rem;
+        overflow: hidden;
+    }
+
+    .a5e-code-editor {
+        height: 25rem;
+    }
+</style>
---
+++ src/view/sheets/pages/item/ItemMacroPage.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { getContext } from "svelte";
+
+    import Section from "#view/snippets/Section.svelte";
+    import CodeEditor from "#view/components/CodeEditor.svelte";
+
+    let item: any = getContext("item");
+    let actionId: string | null = getContext("actionId");
+
+    let field = actionId ? `actions.${actionId}.macro` : "macro";
+    let content = $derived(
+        foundry.utils.getProperty(item.reactive.system, field),
+    );
+</script>
+
+{#key field}
+    <CodeEditor document={item} {field} {content}></CodeEditor>
+{/key}
