c81e1074263c888227c60d4a2b52d89e1f1c9128
nchezrony <nchezrony@gmail.com>
2025-12-23 18:10:08 -0600

    Updates version ahead of minor release.

--- src/documents/item/class.ts
+++ src/documents/item/class.ts
@@ -229,8 +229,8 @@ export default class ClassItemA5e extends OriginItemA5e {
 		if (keys.includes('system.hp.hitDiceUsed')) {
 			const used = foundry.utils.getProperty(changed, 'system.hp.hitDiceUsed');
 			const max = this.totalHitDice;
-			// @ts-expect-error
-			await this.updateSource({ 'system.hp.hitDiceUsed': Math.clamp(used, 0, max) });
+
+			changed.system.hp.hitDiceUsed = Math.clamp(used, 0, max);
 		}
 
 		if (this.parent?.documentName === 'Actor' && keys.includes('system.classLevels')) {
