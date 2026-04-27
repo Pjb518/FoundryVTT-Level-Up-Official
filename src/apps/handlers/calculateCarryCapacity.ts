30b1867ad9164fb0d3c32e16b0d365e9712bd106
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-04-26 18:15:08 -0700

    Removes more old code.

--- src/apps/handlers/calculateCarryCapacity.ts
+++
@@ -1,8 +0,0 @@
-export default function calculateCarryCapacity(actorData) {
-	const strengthScore = actorData.system.abilities.str.value;
-	const { size } = actorData.system.traits;
-	const baseCarryCapacityMultiplier = CONFIG.A5E.carryCapacityMultiplier[size || 'med'];
-	const carryCapacityMultiplier = actorData.flags.a5e?.doubleCarryCapacity ? 2 : 1;
-
-	return strengthScore * baseCarryCapacityMultiplier * carryCapacityMultiplier * 15;
-}
--- src/apps/handlers/toggleSheetLockedState.js
+++
@@ -1,4 +0,0 @@
-export default function toggleSheetLockedState(actor) {
-	const currentLockState = actor.getFlag('a5e', 'sheetIsLocked') ?? true;
-	actor.update({ 'flags.a5e.sheetIsLocked': !currentLockState });
-}
