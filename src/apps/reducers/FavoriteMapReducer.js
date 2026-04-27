1174770c18f1b0028190aba20cd0be78b50588cc
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-04-26 18:04:50 -0700

    Removes some old apps files.

--- src/apps/reducers/FavoriteMapReducer.js
+++
@@ -1,7 +0,0 @@
-// import { DynMapReducer } from '#runtime/svelte/store/reducer';
-
-export default class FavoriteMapReducer extends DynMapReducer {
-  initialize() {
-    this.filters.add((item) => item.system.favorite);
-  }
-}
