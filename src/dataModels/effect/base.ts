a941860e6610d007bd1e7cba858cb14af4aff9ee
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-04-29 22:36:01 -0700

    Adds changes to base effect schema.

--- src/dataModels/effect/base.ts
+++ src/dataModels/effect/base.ts
@@ -1,31 +1,68 @@
-import { migrationData } from '../common';
+import { migrationData } from "../common.ts";
 
 const { fields } = foundry.data;
 
 const baseSchema = () => ({
-	applyToSelf: new fields.BooleanField({ required: true, nullable: false, initial: false }),
-	effectType: new fields.StringField({ required: true, nullable: false, initial: 'passive' }),
-	default: new fields.BooleanField({ required: true, nullable: false, initial: true }),
+  applyToSelf: new fields.BooleanField({
+    required: true,
+    nullable: false,
+    initial: false,
+  }),
+  changes: new fields.ArrayField(
+    new fields.SchemaField({
+      key: new fields.StringField({ required: true }),
+      // We can use this to get our custom types in
+      type: new fields.StringField({
+        required: true,
+        blank: false,
+        initial: "add",
+      }),
+      value: new fields.AnyField({
+        required: true,
+        nullable: true,
+        serializable: true,
+        initial: "",
+      }),
+      phase: new fields.StringField({
+        required: true,
+        blank: false,
+        initial: "initial",
+      }),
+      priority: new fields.NumberField(),
+    }),
+  ),
+  effectType: new fields.StringField({
+    required: true,
+    nullable: false,
+    initial: "passive",
+  }),
+  default: new fields.BooleanField({
+    required: true,
+    nullable: false,
+    initial: true,
+  }),
 });
 
 declare namespace A5EBaseActiveEffectData {
-	type Schema = DataSchema & ReturnType<typeof baseSchema> & ReturnType<typeof migrationData>;
-	type BaseData = Record<string, unknown>;
-	type DerivedData = Record<string, unknown>;
+  type Schema = DataSchema &
+    ReturnType<typeof baseSchema> &
+    ReturnType<typeof migrationData>;
+  type BaseData = Record<string, unknown>;
+  type DerivedData = Record<string, unknown>;
 }
 
 class A5EBaseActiveEffectData extends foundry.abstract.TypeDataModel<
-	A5EBaseActiveEffectData.Schema,
-	ActiveEffect.ConfiguredInstance,
-	A5EBaseActiveEffectData.BaseData,
-	A5EBaseActiveEffectData.DerivedData
+  A5EBaseActiveEffectData.Schema,
+  ActiveEffect.ConfiguredInstance,
+  A5EBaseActiveEffectData.BaseData,
+  A5EBaseActiveEffectData.DerivedData
 > {
-	static override defineSchema(): A5EBaseActiveEffectData.Schema {
-		return {
-			...baseSchema(),
-			...migrationData(),
-		};
-	}
+  static override defineSchema(): A5EBaseActiveEffectData.Schema {
+    return {
+      ...baseSchema(),
+      ...migrationData(),
+    };
+  }
 }
 
 export { A5EBaseActiveEffectData };
