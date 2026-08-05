931d61863039f4c3ecfde32b4b33311c08e11465
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-08-05 11:18:53 -0700

    Fixes darkvision being in color.

--- src/documents/tokenDocument.ts
+++ src/documents/tokenDocument.ts
@@ -3,154 +3,152 @@
  * @extends {TokenDocument}
  */
 export default class TokenDocumentA5e extends TokenDocument {
-  declare automateVision: boolean;
+	declare automateVision: boolean;
 
-  declare charOnlyVisionAutomation: boolean;
+	declare charOnlyVisionAutomation: boolean;
 
-  get scene() {
-    return this.parent;
-  }
+	get scene() {
+		return this.parent;
+	}
 
-  override prepareBaseData() {
-    this.updateTokenSize();
-    super.prepareBaseData();
-  }
+	override prepareBaseData() {
+		this.updateTokenSize();
+		super.prepareBaseData();
+	}
 
-  // TODO: Fix this
-  async _onOverrideSize(changes) {
-    const width = changes.width || this.width;
-    const height = changes.height || this.height;
-    this.update({ width, height });
-  }
+	// TODO: Fix this
+	async _onOverrideSize(changes) {
+		const width = changes.width || this.width;
+		const height = changes.height || this.height;
+		this.update({ width, height });
+	}
 
-  /* ----------------------------------------
+	/* ----------------------------------------
     Detection Mode
   ------------------------------------------- */
-  override _prepareDetectionModes() {
+	override _prepareDetectionModes() {
+		this.automateVision ??=
+			game.settings.storage.get('world').getItem('a5e.automateVisionRules') ?? false;
 
-    this.automateVision ??=
-      game.settings.storage.get("world").getItem("a5e.automateVisionRules") ??
-      false;
+		this.charOnlyVisionAutomation ??=
+			game.settings.storage.get('world').getItem('a5e.visionRulesApplyToCharactersOnly') ?? true;
 
-    this.charOnlyVisionAutomation ??=
-      game.settings.storage
-        .get("world")
-        .getItem("a5e.visionRulesApplyToCharactersOnly") ?? true;
+		const { actor, scene } = this;
+		if (!this.automateVision || !scene || !actor) {
+			super._prepareDetectionModes();
+			return;
+		}
 
-    const { actor, scene } = this;
-    if (!this.automateVision || !scene || !actor) {
-      super._prepareDetectionModes();
-      return;
-    }
+		if (actor.type === 'npc' && this.charOnlyVisionAutomation) {
+			super._prepareDetectionModes();
+			return;
+		}
 
-    if (actor.type === "npc" && this.charOnlyVisionAutomation) {
-      super._prepareDetectionModes();
-      return;
-    }
+		const { visionData } = actor;
+		const lightPerception = { enabled: true, range: Infinity };
+		const basicSight = { enabled: true, range: 0 };
+		this.detectionModes = { lightPerception, basicSight };
 
-    const { visionData } = actor;
-    const lightPerception = { enabled: true, range: Infinity };
-    const basicSight = { enabled: true, range: 0 };
-    this.detectionModes = { lightPerception, basicSight };
+		const visionMode = visionData.hasDarkvision ? 'darkvision' : 'basic';
+		this.sight.enabled = true;
+		this.sight.attenuation = 0.1;
+		this.sight.brightness = 0;
+		this.sight.contrast = 0;
+		this.sight.range = 0;
+		this.sight.saturation = 0;
+		this.sight.visionMode = visionMode;
 
-    const visionMode = visionData.hasDarkvision ? "darkvision" : "basic";
-    this.sight.enabled = true;
-    this.sight.attenuation = 0.1;
-    this.sight.brightness = 0;
-    this.sight.contrast = 0;
-    this.sight.range = 0;
-    this.sight.saturation = 0;
-    this.sight.visionMode = visionMode;
+		const visionModeDefaults = CONFIG.Canvas.visionModes[visionMode].vision.defaults;
+		this.sight.brightness = visionModeDefaults.brightness ?? 0;
+		this.sight.saturation = visionModeDefaults.saturation ?? 0;
 
-    const visionModeDefaults = CONFIG.Canvas.visionModes[visionMode].vision.defaults;
-    this.sight.brightness = visionModeDefaults.brightness ?? 0;
-    this.sight.saturation = visionModeDefaults.saturation ?? 0;
+		if (visionMode === 'darkvision') {
+			this.sight.range = basicSight.range = visionData.senses.darkvision.distance;
+			// TODO: Add support for color darkvision
+			this.sight.saturation = -1;
+		}
 
-    if (visionMode === "darkvision") {
-      this.sight.range = basicSight.range = visionData.senses.darkvision.distance;
-      // TODO: Add support for color darkvision
-      this.sight.saturation = 0;
-    }
+		if (visionData.hasBlindsight) {
+			this.detectionModes.blindsight = {
+				enabled: true,
+				range: visionData.senses.blindsight.distance ?? 0,
+			};
+		}
 
-    if (visionData.hasBlindsight) {
-      this.detectionModes.blindsight = {
-        enabled: true, range: visionData.senses.blindsight.distance ?? 0
-      };
-    }
+		if (visionData.hasTremorsense) {
+			this.detectionModes.feelTremor = {
+				enabled: true,
+				range: visionData.senses.tremorsense.distance ?? 0,
+			};
+		}
 
-    if (visionData.hasTremorsense) {
-      this.detectionModes.feelTremor = {
-        enabled: true, range: visionData.senses.tremorsense.distance ?? 0
-      };
-    }
+		if (visionData.hasTruesight) {
+			this.detectionModes.seeAll = {
+				enabled: true,
+				range: visionData.senses.truesight.distance ?? 0,
+			};
+		}
 
-    if (visionData.hasTruesight) {
-      this.detectionModes.seeAll = {
-        enabled: true, range: visionData.senses.truesight.distance ?? 0
-      }
-    }
+		if (!actor.statuses.has('deafened')) {
+			this.detectionModes.hearing = { enabled: true, range: Infinity };
+		}
+	}
 
-    if (!actor.statuses.has("deafened")) {
-      this.detectionModes.hearing = { enabled: true, range: Infinity };
-    }
+	updateTokenSize() {
+		const { actor } = this;
+		if (!actor) return;
 
-  }
+		const { size } = actor.system.traits;
+		const numericalSize = CONFIG.A5E.tokenDimensions[size];
+		this.width = numericalSize ?? this.width ?? 1;
+		this.height = numericalSize ?? this.height ?? 1;
+	}
 
-  updateTokenSize() {
-    const { actor } = this;
-    if (!actor) return;
+	/**
+	 * Overrides base functionality and doesn't update unlinked tokens.
+	 * @override
+	 * */
+	_onUpdateBaseActor(update = {}, options = {}) {
+		// Update synthetic Actor data
+		if (!this.isLinked && this.delta) {
+			this.delta.updateSyntheticActor();
+			// eslint-disable-next-line no-restricted-syntax
+			for (const collection of Object.values(this.delta.collections)) {
+				collection.initialize({ full: true });
+			}
+		}
 
-    const { size } = actor.system.traits;
-    const numericalSize = CONFIG.A5E.tokenDimensions[size];
-    this.width = numericalSize ?? this.width ?? 1;
-    this.height = numericalSize ?? this.height ?? 1;
-  }
+		this._onRelatedUpdate(update, options);
+		this._updateCanvas(update);
+	}
 
-  /**
-   * Overrides base functionality and doesn't update unlinked tokens.
-   * @override
-   * */
-  _onUpdateBaseActor(update = {}, options = {}) {
-    // Update synthetic Actor data
-    if (!this.isLinked && this.delta) {
-      this.delta.updateSyntheticActor();
-      // eslint-disable-next-line no-restricted-syntax
-      for (const collection of Object.values(this.delta.collections)) {
-        collection.initialize({ full: true });
-      }
-    }
+	// Update canvas if there are changes that affect the canvas
+	_updateCanvas(updates) {
+		if (!this.scene?.isInFocus && !this.scene?.isView) return;
+		if (!this.automateVision || !this.sight.enabled) return;
 
-    this._onRelatedUpdate(update, options);
-    this._updateCanvas(update);
-  }
+		const keys = Object.keys(foundry.utils.flattenObject(updates));
+		if (keys.some((k) => k.startsWith('system.attributes.senses'))) {
+			canvas.perception.update({ initializeVision: true }, true);
+			this.reset();
+		}
+	}
 
-  // Update canvas if there are changes that affect the canvas
-  _updateCanvas(updates) {
-    if (!this.scene?.isInFocus && !this.scene?.isView) return;
-    if (!this.automateVision || !this.sight.enabled) return;
+	/** @inheritdoc */
+	getBarAttribute(barName, { alternative } = {}) {
+		const data = super.getBarAttribute(barName, { alternative });
 
-    const keys = Object.keys(foundry.utils.flattenObject(updates));
-    if (keys.some((k) => k.startsWith("system.attributes.senses"))) {
-      canvas.perception.update({ initializeVision: true }, true);
-      this.reset();
-    }
-  }
+		if (data && data.attribute === 'attributes.hp') {
+			data.value += parseInt(
+				foundry.utils.getProperty(this.actor.system, 'attributes.hp.temp') || 0,
+				10,
+			);
+			data.max += parseInt(
+				foundry.utils.getProperty(this.actor.system, 'attributes.hp.temp') || 0,
+				10,
+			);
+		}
 
-  /** @inheritdoc */
-  getBarAttribute(barName, { alternative } = {}) {
-    const data = super.getBarAttribute(barName, { alternative });
-
-    if (data && data.attribute === "attributes.hp") {
-      data.value += parseInt(
-        foundry.utils.getProperty(this.actor.system, "attributes.hp.temp") || 0,
-        10,
-      );
-      data.max += parseInt(
-        foundry.utils.getProperty(this.actor.system, "attributes.hp.temp") || 0,
-        10,
-      );
-    }
-
-    return data;
-  }
+		return data;
+	}
 }
