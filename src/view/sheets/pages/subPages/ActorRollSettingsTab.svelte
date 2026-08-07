1d009421be8c8a74c0795f5f90126260236f94e7
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-08-06 19:09:24 -0700

    Adds logic for actor level critical thresholds.

--- src/view/sheets/pages/subPages/ActorRollSettingsTab.svelte
+++ src/view/sheets/pages/subPages/ActorRollSettingsTab.svelte
@@ -68,28 +68,28 @@
     </FieldWrapper>
 
     <FieldWrapper
-        hint="A5E.settings.hints.criticalHitThresholdMelee"
+        hint="A5E.settings.hints.criticalHitThresholdWeapon"
         --a5e-field-wrapper-direction="row"
         --a5e-field-wrapper-item-alignment="center"
     >
         <input
-            id="{actor.id}-critical-hit-threshold-melee"
+            id="{actor.id}-critical-hit-threshold-weapon"
             class="a5e-input a5e-input--small a5e-input--slim"
             type="number"
             min="0"
             max="20"
-            value={flags?.criticalHitThresholdMelee ?? 20}
+            value={flags?.criticalHitThresholdWeapon ?? 20}
             onchange={({ currentTarget }) => {
                 updateDocumentDataFromField(
                     actor,
-                    "flags.a5e.criticalHitThresholdMelee",
+                    "flags.a5e.criticalHitThresholdWeapon",
                     parseInt(currentTarget.value, 20),
                 );
             }}
         />
 
-        <label for="{actor.id}-critical-hit-threshold-melee">
-            {localize("A5E.settings.criticalHitThresholdMelee")}
+        <label for="{actor.id}-critical-hit-threshold-weapon">
+            {localize("A5E.settings.criticalHitThresholdWeapon")}
         </label>
     </FieldWrapper>
 
