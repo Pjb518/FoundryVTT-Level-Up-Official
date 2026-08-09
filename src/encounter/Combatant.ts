99ec12a0d6272c4a1a71db72a5c7f418988cefd7
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-08-08 23:07:13 -0700

    WIP - Combatant

--- src/encounter/Combatant.ts
+++ src/encounter/Combatant.ts
@@ -2,6 +2,29 @@ class CombatantA5e extends Combatant {
 	get encounter() {
 		return this.parent;
 	}
+
+	async onStartTurn(): Promise<void> {
+		const { actor, encounter } = this;
+		if (!actor || !encounter) return;
+
+		const eventType = 'turn-start';
+		await this.#performActorUpdates(eventType);
+	}
+
+	async onEndTurn(): Promise<void> {
+		const { actor, encounter } = this;
+		if (!actor || !encounter) return;
+
+		const eventType = 'turn-end';
+		await this.#performActorUpdates(eventType);
+	}
+
+	async #performActorUpdates(event: 'turn-start' | 'turn-end'): Promise<void> {
+		const { actor } = this;
+		if (!actor) return;
+
+		// Recharge turn and round based durations
+	}
 }
 
 export { CombatantA5e };
