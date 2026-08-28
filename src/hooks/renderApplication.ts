211cc4fdddea8750b0ba6c595840508089173713
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-08-28 14:56:37 -0700

    Adds custom hook for tools scene control bar.

--- src/hooks/renderApplication.ts
+++ src/hooks/renderApplication.ts
@@ -1,39 +1,38 @@
-import renderPartyViewer from "./renderPartyViewer.ts";
+import renderPartyViewer from './renderPartyViewer.ts';
 
 export default function setupPartyViewerButton() {
-  Hooks.on("getSceneControlButtons", (controls) => {
-    if (
-      !game.settings.get("a5e", "playersCanAccessPartyViewer") &&
-      !game.user.isGM
-    ) {
-      return;
-    }
+	Hooks.on('getSceneControlButtons', (controls) => {
+		if (!game.settings.get('a5e', 'playersCanAccessPartyViewer') && !game.user.isGM) {
+			return;
+		}
 
-    // Initialize if it doesn't exist
-    if (!controls.a5eTools) {
-      controls.a5eTools = {
-        name: "a5eTools",
-        title: "A5E Tools",
-        icon: "fa-solid fa-toolbox",
-        layer: "a5eTools", // Now this custom layer exists
-        tools: {},
-      };
-    }
+		// Initialize if it doesn't exist
+		if (!controls.a5eTools) {
+			controls.a5eTools = {
+				name: 'a5eTools',
+				title: 'A5E Tools',
+				icon: 'fa-solid fa-toolbox',
+				layer: 'a5eTools', // Now this custom layer exists
+				tools: {},
+			};
+		}
 
-    // Add party viewer as a tool
-    controls.a5eTools.tools.partyViewer = {
-      name: "partyViewer",
-      title: "Party Viewer",
-      icon: "fa-solid fa-users",
-      button: true,
-      onClick: () => {
-        renderPartyViewer();
-      },
-    };
+		// Add party viewer as a tool
+		controls.a5eTools.tools.partyViewer = {
+			name: 'partyViewer',
+			title: 'Party Viewer',
+			icon: 'fa-solid fa-users',
+			button: true,
+			onClick: () => {
+				renderPartyViewer();
+			},
+		};
 
-    // Set activeTool if not already set
-    if (!controls.a5eTools.activeTool) {
-      controls.a5eTools.activeTool = "partyViewer";
-    }
-  });
+		// Set activeTool if not already set
+		// if (!controls.a5eTools.activeTool) {
+		//   controls.a5eTools.activeTool = "partyViewer";
+		// }
+		//
+		Hooks.callAll('a5e.getA5EToolsSceneControlButtons', controls);
+	});
 }
