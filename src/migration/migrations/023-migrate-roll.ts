a37d4d6ab64450d08254b200247612e189e6a052
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-09-03 19:12:27 -0700

    Fixes some bugs with migration

--- src/migration/migrations/023-migrate-roll.ts
+++ src/migration/migrations/023-migrate-roll.ts
@@ -1,53 +1,62 @@
-import { A5E } from '../../config.ts';
 import { MigrationBase } from '../MigrationBase.ts';
 
-const KEY_MAP = {
-	// Attacks
-	'flags.a5e.effects.rollMode.attack.meleeWeaponAttack':
-		'system.rolls.attack.outgoing.meleeWeaponAttack.rollMode',
-
-	'flags.a5e.effects.rollMode.attack.rangedWeaponAttack':
-		'system.rolls.attack.outgoing.rangedWeaponAttack.rollMode',
-
-	'flags.a5e.effects.rollMode.attack.meleeSpellAttack':
-		'system.rolls.attack.outgoing.meleeSpellAttack.rollMode',
-
-	'flags.a5e.effects.rollMode.attack.rangedSpellAttack':
-		'system.rolls.attack.outgoing.rangedSpellAttack.rollMode',
-
-	// Abilities
-	...Object.keys(A5E.abilities).reduce(
-		(acc, a) => {
-			acc[`flags.a5e.effects.rollMode.abilityCheck.${a}`] = `system.abilities.${a}.check.rollMode`;
-			acc[`flags.a5e.effects.rollMode.abilitySave.${a}`] = `system.abilities.${a}.save.rollMode`;
-			return acc;
-		},
-		{} as Record<string, string>,
-	),
-
-	// Skills
-	...Object.keys(A5E.skills).reduce(
-		(acc, s) => {
-			acc[`flags.a5e.effects.rollMode.skillCheck.${s}`] = `system.skills.${s}.check.rollMode`;
-			return acc;
-		},
-		{} as Record<string, string>,
-	),
-
-	// Other
-	'flags.a5e.effects.rollMode.concentration': 'system.attributes.concentration.roll.rollMode',
-	'flags.a5e.effects.rollMode.deathSave': 'system.rolls.death.rollMode',
-	'flags.a5e.effects.rollMode.initiative': 'system.attributes.initiative.rollMode',
-};
-
-const KEY_MAP_KEYS = new Set(Object.keys(KEY_MAP));
-const ITEM_TYPES = new Set(['base', 'feature', 'interaction', 'maneuver', 'object', 'spell']);
-
-export class Migration23MigrateRoll extends MigrationBase {
+export class Migration023MigrateRoll extends MigrationBase {
 	static override version = 0.023;
 
+	/** ================================================= */
+	// Keys
+	/** ================================================= */
+
+	KEY_MAP = {
+		// Attacks
+		'flags.a5e.effects.rollMode.attack.meleeWeaponAttack':
+			'system.rolls.attack.outgoing.meleeWeaponAttack.rollMode',
+
+		'flags.a5e.effects.rollMode.attack.rangedWeaponAttack':
+			'system.rolls.attack.outgoing.rangedWeaponAttack.rollMode',
+
+		'flags.a5e.effects.rollMode.attack.meleeSpellAttack':
+			'system.rolls.attack.outgoing.meleeSpellAttack.rollMode',
+
+		'flags.a5e.effects.rollMode.attack.rangedSpellAttack':
+			'system.rolls.attack.outgoing.rangedSpellAttack.rollMode',
+
+		// Abilities
+		...Object.keys(CONFIG.A5E.abilities).reduce(
+			(acc, a) => {
+				acc[`flags.a5e.effects.rollMode.abilityCheck.${a}`] =
+					`system.abilities.${a}.check.rollMode`;
+				acc[`flags.a5e.effects.rollMode.abilitySave.${a}`] = `system.abilities.${a}.save.rollMode`;
+				return acc;
+			},
+			{} as Record<string, string>,
+		),
+
+		// Skills
+		...Object.keys(CONFIG.A5E.skills).reduce(
+			(acc, s) => {
+				acc[`flags.a5e.effects.rollMode.skillCheck.${s}`] = `system.skills.${s}.check.rollMode`;
+				return acc;
+			},
+			{} as Record<string, string>,
+		),
+
+		// Other
+		'flags.a5e.effects.rollMode.concentration': 'system.attributes.concentration.roll.rollMode',
+		'flags.a5e.effects.rollMode.deathSave': 'system.rolls.death.rollMode',
+		'flags.a5e.effects.rollMode.initiative': 'system.attributes.initiative.rollMode',
+	};
+
+	KEY_MAP_KEYS = new Set(Object.keys(this.KEY_MAP));
+
+	ITEM_TYPES = new Set(['base', 'feature', 'interaction', 'maneuver', 'object', 'spell']);
+
+	/** ================================================= */
+	// Migration Methods
+	/** ================================================= */
+
 	override async updateItem(source: Item): Promise<void> {
-		if (!ITEM_TYPES.has(source.type)) return;
+		if (!this.ITEM_TYPES.has(source.type)) return;
 
 		// Update scaling values
 		const actions = Object.entries(source.system.actions ?? {});
@@ -78,8 +87,8 @@ export class Migration23MigrateRoll extends MigrationBase {
 	override async updateEffect(source: ActiveEffect): Promise<void> {
 		source.system.changes.forEach((change, idx) => {
 			// Update roll mode keys
-			if (KEY_MAP_KEYS.has(change.key)) {
-				source.system.changes[idx].key = KEY_MAP_KEYS[change.key] || change.key;
+			if (this.KEY_MAP_KEYS.has(change.key)) {
+				source.system.changes[idx].key = this.KEY_MAP[change.key] || change.key;
 			}
 		});
 	}
