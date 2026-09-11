89b694c47b65853d0f4a5c2dcb7c1f2680fd8b51
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-09-11 00:06:20 -0700

    Updates localizations for time

--- src/utils/getFormattedTimeFromSeconds.ts
+++ src/utils/getFormattedTimeFromSeconds.ts
@@ -3,27 +3,27 @@ import { localize } from '#utils/localization/localize.ts';
 export default function getFormattedTimeFromSeconds(seconds: number): string {
 	if (seconds >= 63_072_000) {
 		// two years
-		return `${Math.floor(seconds / 31_536_000)} ${localize('A5E.DurationYearPlural')}`;
+		return `${Math.floor(seconds / 31_536_000)} ${localize('A5E.durations.yearPlural')}`;
 	}
 	if (seconds >= 31_536_000) {
 		// one year
-		return `1 ${localize('A5E.DurationYear')}`;
+		return `1 ${localize('A5E.durations.year')}`;
 	}
 	if (seconds >= 1_209_600) {
 		// two weeks
-		return `${Math.floor(seconds / 604_800)} ${localize('A5E.DurationWeekPlural')}`;
+		return `${Math.floor(seconds / 604_800)} ${localize('A5E.durations.weekPlural')}`;
 	}
 	if (seconds > 604_800) {
 		// one week
-		return `1 ${localize('A5E.DurationWeek')}`;
+		return `1 ${localize('A5E.durations.week')}`;
 	}
 	if (seconds >= 172_800) {
 		// two days
-		return `${Math.floor(seconds / 86_400)} ${localize('A5E.DurationDayPlural')}`;
+		return `${Math.floor(seconds / 86_400)} ${localize('A5E.durations.dayPlural')}`;
 	}
 	if (seconds > 7_200) {
 		// two hours
-		return `${Math.floor(seconds / 3_600)} ${localize('A5E.DurationHourPlural')}`;
+		return `${Math.floor(seconds / 3_600)} ${localize('A5E.durations.hourPlural')}`;
 	}
 	if (seconds > 120) {
 		// two minutes
@@ -35,15 +35,15 @@ export default function getFormattedTimeFromSeconds(seconds: number): string {
 	}
 	if (seconds >= 6) {
 		// one round
-		return `1 ${localize('A5E.DurationRound')}`;
+		return `1 ${localize('A5E.durations.Round')}`;
 	}
 	if (seconds >= 2) {
 		// two seconds
-		return `${seconds} ${localize('A5E.DurationSecondPlural')}`;
+		return `${seconds} ${localize('A5E.durations.secondPlural')}`;
 	}
 	if (seconds === 1) {
 		// one second
-		return `1 ${localize('A5E.DurationSecond')}`;
+		return `1 ${localize('A5E.durations.second')}`;
 	}
 
 	return 'Error';
