4005ed66269045608453e37250b3d7970c7de68c
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-09-20 18:41:13 -0700

    Adds supports for spell scroll and blocks all other item types other than objects.

--- src/documents/sheets/PartySheet.svelte.ts
+++ src/documents/sheets/PartySheet.svelte.ts
@@ -55,6 +55,104 @@ class PartySheetA5E extends SvelteApplicationMixin(foundry.applications.sheets.A
 
 		return actor;
 	}
+
+	async _onDropItem(event: DragEvent, item: Item) {
+		if (!['object', 'spell'].includes(item.type)) return null;
+
+		if (item.type === 'spell') {
+			this.#onDropSpell(item);
+			return;
+		}
+
+		return super._onDropItem(event, item);
+	}
+
+	async #onDropSpell(item: Item.OfType<'spell'>) {
+		// Create Spell Scroll
+		const spellLevel = item.system.level;
+
+		const { attackBonus, cost, craftingComponent, saveDC, rarity } =
+			CONFIG.A5E.scrollData[spellLevel];
+
+		const scroll = {
+			name: `Spell Scroll (${item.name})`,
+			img: 'icons/sundries/scrolls/scroll-writing-brown-gold.webp',
+			type: 'object',
+			system: {
+				actions: {},
+				craftingComponents: craftingComponent,
+				description: item.system.description,
+				price: { value: cost.value, denomination: cost.denomination, special: '' },
+				objectType: 'consumable',
+				rarity,
+			},
+		};
+
+		scroll.system.actions = [...item.actions.values()].reduce((actions, _action) => {
+			const action = { ..._action };
+
+			action.prompts = Object.entries(action?.prompts ?? {}).reduce(
+				(prompts, [key, _prompt]: [string, any]): object => {
+					const prompt = { ..._prompt };
+
+					if (prompt.type === 'savingThrow') {
+						prompt.saveDC.type = 'custom';
+						prompt.saveDC.bonus = saveDC;
+					}
+
+					prompts[key] = prompt;
+
+					return prompts;
+				},
+				{},
+			);
+
+			action.rolls = Object.entries(action?.rolls ?? {}).reduce(
+				(rolls, [key, _roll]: [string, any]): object => {
+					const roll = { ..._roll };
+
+					if (roll.type === 'attack') {
+						roll.ability = 'none';
+						roll.bonus = attackBonus.toString(10);
+					}
+
+					if (roll.scaling) {
+						delete roll.scaling;
+					}
+
+					rolls[key] = roll;
+
+					return rolls;
+				},
+				{},
+			);
+
+			action.consumers = {
+				[foundry.utils.randomID()]: {
+					itemId: '',
+					quantity: 1,
+					type: 'quantity',
+				},
+			};
+
+			actions[foundry.utils.randomID()] = action;
+			return actions;
+		}, {});
+
+		const createdItem = (await this.actor.createEmbeddedDocuments('Item', [scroll]))?.[0];
+		if (!createdItem) return;
+
+		// Set itemId on consumer
+		const updateData = {};
+		Object.entries(createdItem.system.actions).forEach(([actionId, action]: [string, any]) => {
+			Object.entries(action.consumers ?? {}).forEach(([consumerId]) => {
+				updateData[`system.actions.${actionId}.consumers.${consumerId}.itemId`] = createdItem.id;
+			});
+		});
+
+		createdItem.update(updateData);
+		return;
+	}
 }
 
 declare namespace PartySheetA5E {}
