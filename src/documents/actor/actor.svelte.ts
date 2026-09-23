9cb6c37d68231282787a54bd80a9ec72a94f38c8
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-09-23 15:15:29 -0700

    Fixes char getRollData not being called.

--- src/documents/actor/actor.svelte.ts
+++ src/documents/actor/actor.svelte.ts
@@ -1656,15 +1656,15 @@ class ActorA5E<SubType extends Actor.SubType = Actor.SubType> extends Actor<SubT
 			data.spellDC = this.system.attributes.spellDC;
 		}
 
+		// Call Sub Modules Here becuase target needs to go last
+		if (this.type === 'character') this.getCharRollData(data, item);
+
 		// Inject target data
 		const targets = [...game.user.targets];
 		if (targets.length !== 1) return data;
 
 		const target = targets[0]?.actor;
 		if (target && target.uuid !== this.uuid) data.target = target.getRollData();
-
-		// Call Sub Modules
-		if (this.type === 'character') this.getCharRollData(data, item);
 
 		return data;
 	}
@@ -1687,6 +1687,7 @@ class ActorA5E<SubType extends Actor.SubType = Actor.SubType> extends Actor<SubT
 		data: Record<string, any>,
 		_item: BaseItemA5e | null = null,
 	) {
+		console.log('Im a char rollData');
 		data.level = this.levels?.character ?? data.level ?? this.system.details.level;
 
 		const resources = {};
--- src/view/sheets/pages/ActorCorePage.svelte
+++ src/view/sheets/pages/ActorCorePage.svelte
@@ -1,54 +1,54 @@
 <script>
-    import { getContext } from "svelte";
+  import { getContext } from "svelte";
 
-    import GenericActorResource from "#view/sheets/components/actor/GenericActorResource.svelte";
+  import GenericActorResource from "#view/sheets/components/actor/GenericActorResource.svelte";
 
-    import ItemCategory from "#view/sheets/components/ItemCategory.svelte";
-    import ActorSkillsPage from "./ActorSkillsPage.svelte";
+  import ItemCategory from "#view/sheets/components/ItemCategory.svelte";
+  import ActorSkillsPage from "./ActorSkillsPage.svelte";
 
-    import { usesRequired } from "#utils/view/usesRequired.ts";
-    import { quantityRequired } from "#utils/view/quantityRequired.ts";
+  import { usesRequired } from "#utils/view/usesRequired.ts";
+  import { quantityRequired } from "#utils/view/quantityRequired.ts";
 
-    let { resources = $bindable() } = $props();
+  let { resources = $bindable() } = $props();
 
-    const actor = getContext("actor");
+  const actor = getContext("actor");
 
-    let actorResources = $derived(actor.reactive.system.resources);
-    let flags = $derived(actor.reactive.flags);
-    let favoritesList = $derived(
-        actor.reactive.items.filter((item) => item.system.favorite === true),
-    );
+  let actorResources = $derived(actor.reactive.system.resources);
+  let flags = $derived(actor.reactive.flags);
+  let favoritesList = $derived(
+    actor.reactive.items.filter((item) => item.system.favorite === true),
+  );
 
-    let showQuantity = $derived(quantityRequired(favoritesList));
-    let showUses = $derived(usesRequired(favoritesList));
+  let showQuantity = $derived(quantityRequired(favoritesList));
+  let showUses = $derived(usesRequired(favoritesList));
 
-    // Update resources reactively
-    $effect(() => {
-        resources = actorResources;
-    });
+  // Update resources reactively
+  $effect(() => {
+    resources = actorResources;
+  });
 </script>
 
 {#if !(flags.a5e?.hideGenericResources ?? actor.type === "npc")}
-    <div class="a5e-resources-wrapper">
-        <ol class="a5e-resources-container">
-            {#each Object.entries(actorResources) as [source, resource]}
-                <GenericActorResource {resource} {source} />
-            {/each}
-        </ol>
-    </div>
+  <div class="a5e-resources-wrapper">
+    <ol class="a5e-resources-container">
+      {#each Object.entries(actorResources) as [source, resource]}
+        <GenericActorResource {resource} {source} />
+      {/each}
+    </ol>
+  </div>
 {/if}
 
 {#if flags.a5e?.showFavoritesSection ?? true}
-    <section class="a5e-page-wrapper a5e-page-wrapper--item-list">
-        <ItemCategory
-            label="A5E.tabs.favoriteItems"
-            icon="fas fa-star a5e-section-header__icon"
-            items={[...favoritesList].sort((a, b) => a.sort - b.sort)}
-            type="favorites"
-            {showQuantity}
-            {showUses}
-        />
-    </section>
+  <section class="a5e-page-wrapper a5e-page-wrapper--item-list">
+    <ItemCategory
+      label="A5E.tabs.favoriteItems"
+      icon="fas fa-star a5e-section-header__icon"
+      items={[...favoritesList].sort((a, b) => a.sort - b.sort)}
+      type="favorites"
+      {showQuantity}
+      {showUses}
+    />
+  </section>
 {:else}
-    <ActorSkillsPage />
+  <ActorSkillsPage />
 {/if}
