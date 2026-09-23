1bed5d4b2f8eadfff76747b89c1e4d42fd4c6317
Nekro Darkmoon <nekrodarkmoon@gmail.com>
2026-09-22 21:06:58 -0700

    Fixes weapon profs not changing to category proficiency if all proficiencies were selected.

--- src/view/dialogs/actor/DetailsConfigDialog.svelte
+++ src/view/dialogs/actor/DetailsConfigDialog.svelte
@@ -1,128 +1,121 @@
 <script lang="ts">
-    import updateDocumentDataFromField from "#utils/updateDocumentDataFromField.ts";
+  import updateDocumentDataFromField from "#utils/updateDocumentDataFromField.ts";
+  import Checkbox from "#view/snippets/Checkbox.svelte";
+  import ComplexDetailEmbed from "#view/snippets/ComplexDetailEmbed.svelte";
+  import CustomTagGroup from "#view/snippets/CustomTagGroup.svelte";
+  import FieldWrapper from "#view/snippets/FieldWrapper.svelte";
+  import RadioGroup from "#view/snippets/RadioGroup.svelte";
+  import Section from "#view/snippets/Section.svelte";
 
-    import Checkbox from "#view/snippets/Checkbox.svelte";
-    import CustomTagGroup from "#view/snippets/CustomTagGroup.svelte";
-    import FieldWrapper from "#view/snippets/FieldWrapper.svelte";
-    import RadioGroup from "#view/snippets/RadioGroup.svelte";
-    import Section from "#view/snippets/Section.svelte";
-    import ComplexDetailEmbed from "#view/snippets/ComplexDetailEmbed.svelte";
+  type Props = {
+    document: any;
+    propertyKey: string;
+    configObject: any;
+    heading: string;
+    type: string;
+  };
 
-    type Props = {
-        document: any;
-        propertyKey: string;
-        configObject: any;
-        heading: string;
-        type: string;
-    };
+  function getTooltipData(actor: Actor) {
+    const grantData = actor.grants.getGrantedTraits(type);
 
-    function getTooltipData(actor: Actor) {
-        const grantData = actor.grants.getGrantedTraits(type);
-
-        const data = {};
-        for (const value of Object.values(grantData)) {
-            value.traits.forEach((trait) => {
-                const docName = fromUuidSync(value.itemId)?.name;
-                data[trait] = `Granted by ${docName}`;
-            });
-        }
-
-        return data;
+    const data = {};
+    for (const value of Object.values(grantData)) {
+      value.traits.forEach((trait) => {
+        const docName = fromUuidSync(value.itemId)?.name;
+        data[trait] = `Granted by ${docName}`;
+      });
     }
 
-    let { document, propertyKey, configObject, heading, type }: Props = $props();
+    return data;
+  }
 
-    let actor = document;
-    let actorStore = $derived(actor.reactive.system);
-    const isRadioGroup = ["size"].includes(type);
-    const { weaponCategories, toolCategories } = CONFIG.A5E;
+  let { document, propertyKey, configObject, heading, type }: Props = $props();
 
-    let selected = $derived(foundry.utils.getProperty(actor.reactive, propertyKey));
-    let tooltipData = $derived(getTooltipData(actor.reactive));
+  let actor = document;
+  let actorStore = $derived(actor.reactive.system);
+  const isRadioGroup = ["size"].includes(type);
+  const { weaponCategories, toolCategories } = CONFIG.A5E;
 
-    let options = $derived(Object.entries(configObject)) as string[][];
+  let selected = $derived(
+    foundry.utils.getProperty(actor.reactive, propertyKey),
+  );
+  let tooltipData = $derived(getTooltipData(actor.reactive));
+
+  let options = $derived(Object.entries(configObject)) as string[][];
 </script>
 
 <Section --a5e-section-body-padding="0.75rem" --a5e-section-body-gap="0.75rem">
-    {#if isRadioGroup}
-        <FieldWrapper>
-            <RadioGroup
-                {heading}
-                {options}
-                {selected}
-                allowDeselect={false}
-                onUpdateSelection={(value) =>
-                    updateDocumentDataFromField(actor, propertyKey, value)}
-            />
-        </FieldWrapper>
-    {:else if type === "weapons"}
-        <ComplexDetailEmbed
-            existingProperties={actorStore.proficiencies.weapons}
-            headings={weaponCategories}
-            {configObject}
-            onUpdateSelection={(value) =>
-                updateDocumentDataFromField(actor, propertyKey, value)}
+  {#if isRadioGroup}
+    <FieldWrapper>
+      <RadioGroup
+        {heading}
+        {options}
+        {selected}
+        allowDeselect={false}
+        onUpdateSelection={(value) =>
+          updateDocumentDataFromField(actor, propertyKey, value)}
+      />
+    </FieldWrapper>
+  {:else if type === "weapons"}
+    <ComplexDetailEmbed
+      existingProperties={actorStore.proficiencies.weapons}
+      headings={weaponCategories}
+      {configObject}
+      onUpdateSelection={(value) =>
+        updateDocumentDataFromField(actor, propertyKey, value)}
+    />
+  {:else if type === "tools"}
+    <ComplexDetailEmbed
+      existingProperties={actorStore.proficiencies.tools}
+      headings={toolCategories}
+      {configObject}
+      onUpdateSelection={(value) =>
+        updateDocumentDataFromField(actor, propertyKey, value)}
+    />
+  {:else}
+    <FieldWrapper>
+      <CustomTagGroup
+        {heading}
+        {options}
+        {selected}
+        {tooltipData}
+        onUpdateSelection={(value) =>
+          updateDocumentDataFromField(actor, propertyKey, value)}
+      />
+    </FieldWrapper>
+  {/if}
+
+  {#if type === "creatureTypes"}
+    <FieldWrapper>
+      {#if actor.type === "npc"}
+        <Checkbox
+          label="A5E.details.creature.labels.shapechanger"
+          checked={actorStore.details.isShapechanger}
+          onUpdateSelection={(value) => {
+            updateDocumentDataFromField(
+              actor,
+              "system.details.isShapechanger",
+              value,
+            );
+          }}
         />
-    {:else if type === "tools"}
-        <ComplexDetailEmbed
-            existingProperties={actorStore.proficiencies.tools}
-            headings={toolCategories}
-            {configObject}
-            onUpdateSelection={(value) =>
-                updateDocumentDataFromField(actor, propertyKey, value)}
+
+        <Checkbox
+          label="A5E.details.creature.labels.squad"
+          checked={actorStore.details.isSquad}
+          onUpdateSelection={(value) => {
+            updateDocumentDataFromField(actor, "system.details.isSquad", value);
+          }}
         />
-    {:else}
-        <FieldWrapper>
-            <CustomTagGroup
-                {heading}
-                {options}
-                {selected}
-                {tooltipData}
-                onUpdateSelection={(value) =>
-                    updateDocumentDataFromField(actor, propertyKey, value)}
-            />
-        </FieldWrapper>
-    {/if}
 
-    {#if type === "creatureTypes"}
-        <FieldWrapper>
-            {#if actor.type === "npc"}
-                <Checkbox
-                    label="A5E.details.creature.labels.shapechanger"
-                    checked={actorStore.details.isShapechanger}
-                    onUpdateSelection={(value) => {
-                        updateDocumentDataFromField(
-                            actor,
-                            "system.details.isShapechanger",
-                            value,
-                        );
-                    }}
-                />
-
-                <Checkbox
-                    label="A5E.details.creature.labels.squad"
-                    checked={actorStore.details.isSquad}
-                    onUpdateSelection={(value) => {
-                        updateDocumentDataFromField(
-                            actor,
-                            "system.details.isSquad",
-                            value,
-                        );
-                    }}
-                />
-
-                <Checkbox
-                    label="A5E.details.creature.labels.swarm"
-                    checked={actorStore.details.isSwarm}
-                    onUpdateSelection={(value) => {
-                        updateDocumentDataFromField(
-                            actor,
-                            "system.details.isSwarm",
-                            value,
-                        );
-                    }}
-                />
-            {/if}
-        </FieldWrapper>
-    {/if}
+        <Checkbox
+          label="A5E.details.creature.labels.swarm"
+          checked={actorStore.details.isSwarm}
+          onUpdateSelection={(value) => {
+            updateDocumentDataFromField(actor, "system.details.isSwarm", value);
+          }}
+        />
+      {/if}
+    </FieldWrapper>
+  {/if}
 </Section>
--- src/view/sheets/ActionSheet.svelte
+++ src/view/sheets/ActionSheet.svelte
@@ -1,174 +1,174 @@
 <script lang="ts">
-    import type { Action } from "#types/action.d.ts";
-    import type { Tab } from "#view/navigation/data.ts";
+  import type { Action } from "#types/action.d.ts";
+  import type { Tab } from "#view/navigation/data.ts";
 
-    import { setContext } from "svelte";
+  import { setContext } from "svelte";
 
-    import { editDocumentImage } from "#utils/view/editDocumentImage.ts";
-    import updateDocumentDataFromField from "#utils/updateDocumentDataFromField.ts";
+  import { editDocumentImage } from "#utils/view/editDocumentImage.ts";
+  import updateDocumentDataFromField from "#utils/updateDocumentDataFromField.ts";
 
-    import NavigationBar from "../navigation/NavigationBar.svelte";
+  import NavigationBar from "../navigation/NavigationBar.svelte";
 
-    import ActionsActivationPage from "./pages/action/ActionActivationPage.svelte";
-    import ActionsConsumersPage from "./pages/action/ActionConsumersPage.svelte";
-    import ActionsDescriptionPage from "./pages/action/ActionDescriptionPage.svelte";
-    import ActionsEffectsPage from "./pages/action/ActionEffectsPage.svelte";
-    import ActionsPromptsPage from "./pages/action/ActionPromptsPage.svelte";
-    import ActionsRollsPage from "./pages/action/ActionRollsPage.svelte";
-    import ActionsTargetingPage from "./pages/action/ActionTargetingPage.svelte";
-    import ItemMacroPage from "./pages/item/ItemMacroPage.svelte";
+  import ActionsActivationPage from "./pages/action/ActionActivationPage.svelte";
+  import ActionsConsumersPage from "./pages/action/ActionConsumersPage.svelte";
+  import ActionsDescriptionPage from "./pages/action/ActionDescriptionPage.svelte";
+  import ActionsEffectsPage from "./pages/action/ActionEffectsPage.svelte";
+  import ActionsPromptsPage from "./pages/action/ActionPromptsPage.svelte";
+  import ActionsRollsPage from "./pages/action/ActionRollsPage.svelte";
+  import ActionsTargetingPage from "./pages/action/ActionTargetingPage.svelte";
+  import ItemMacroPage from "./pages/item/ItemMacroPage.svelte";
 
-    type Props = {
-        actionId: string;
-        actor?: Actor;
-        item: Item;
-        sheet: any;
-    };
+  type Props = {
+    actionId: string;
+    actor?: Actor;
+    item: Item;
+    sheet: any;
+  };
 
-    function updateCurrentTab(tabId: string) {
-        const newTab = tabs.find((tab) => tab.name === tabId);
+  function updateCurrentTab(tabId: string) {
+    const newTab = tabs.find((tab) => tab.name === tabId);
 
-        currentTab = newTab ?? tabs[0];
-    }
+    currentTab = newTab ?? tabs[0];
+  }
 
-    function getTabs(): Tab[] {
-        return [
-            {
-                name: "description",
-                label: "A5E.tabs.description",
-                icon: "fa-solid fa-file",
-                component: ActionsDescriptionPage,
-            },
-            {
-                name: "activation",
-                label: "A5E.tabs.activation",
-                icon: "fa-solid fa-clock",
-                component: ActionsActivationPage,
-            },
-            {
-                name: "targeting",
-                label: "A5E.tabs.targeting",
-                icon: "fa-solid fa-crosshairs",
-                component: ActionsTargetingPage,
-            },
-            {
-                name: "rolls",
-                label: "A5E.tabs.rolls",
-                icon: "fa-solid fa-dice-d20",
-                component: ActionsRollsPage,
-            },
-            {
-                name: "prompts",
-                label: "A5E.tabs.prompts",
-                icon: "fa-solid fa-comment-dots",
-                component: ActionsPromptsPage,
-            },
-            {
-                name: "consumers",
-                label: "A5E.tabs.resourceManagement",
-                icon: "fa-solid fa-cogs",
-                component: ActionsConsumersPage,
-            },
-            {
-                name: "effects",
-                label: "A5E.tabs.effects",
-                icon: "fa-solid fa-magic",
-                component: ActionsEffectsPage,
-            },
-            {
-                name: "macro",
-                label: "A5E.tabs.macro",
-                icon: "fa-solid fa-terminal",
-                component: ItemMacroPage,
-            },
-        ];
-    }
+  function getTabs(): Tab[] {
+    return [
+      {
+        name: "description",
+        label: "A5E.tabs.description",
+        icon: "fa-solid fa-file",
+        component: ActionsDescriptionPage,
+      },
+      {
+        name: "activation",
+        label: "A5E.tabs.activation",
+        icon: "fa-solid fa-clock",
+        component: ActionsActivationPage,
+      },
+      {
+        name: "targeting",
+        label: "A5E.tabs.targeting",
+        icon: "fa-solid fa-crosshairs",
+        component: ActionsTargetingPage,
+      },
+      {
+        name: "rolls",
+        label: "A5E.tabs.rolls",
+        icon: "fa-solid fa-dice-d20",
+        component: ActionsRollsPage,
+      },
+      {
+        name: "prompts",
+        label: "A5E.tabs.prompts",
+        icon: "fa-solid fa-comment-dots",
+        component: ActionsPromptsPage,
+      },
+      {
+        name: "consumers",
+        label: "A5E.tabs.resourceManagement",
+        icon: "fa-solid fa-cogs",
+        component: ActionsConsumersPage,
+      },
+      {
+        name: "effects",
+        label: "A5E.tabs.effects",
+        icon: "fa-solid fa-magic",
+        component: ActionsEffectsPage,
+      },
+      {
+        name: "macro",
+        label: "A5E.tabs.macro",
+        icon: "fa-solid fa-terminal",
+        component: ItemMacroPage,
+      },
+    ];
+  }
 
-    let { actionId, actor, item, sheet }: Props = $props();
+  let { actionId, actor, item, sheet }: Props = $props();
 
-    let action: Action = $derived(item.reactive.actions.get(actionId));
+  let action: Action = $derived(item.reactive.actions.get(actionId));
 
-    let tabs = $state(getTabs());
-    let currentTab = $derived(tabs[0]);
+  let tabs = $state(getTabs());
+  let currentTab = $derived(tabs[0]);
 
-    setContext("actionId", actionId);
-    setContext("actor", actor);
-    setContext("item", item);
-    setContext("sheet", sheet);
+  setContext("actionId", actionId);
+  setContext("actor", actor);
+  setContext("item", item);
+  setContext("sheet", sheet);
 </script>
 
 <main class="a5e-action-sheet">
-    <header class="a5e-action-sheet__header">
-        <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-        <!-- svelte-ignore a11y_click_events_have_key_events -->
-        <img
-            class="a5e-action-sheet__header-image"
-            src={action.img || item.reactive.img}
-            alt="{action.name} image"
-            onclick={() => editDocumentImage(item, { actionId })}
-        />
-
-        <input
-            class="a5e-input a5e-input--character-name"
-            type="text"
-            value={action.name}
-            placeholder="Action Name"
-            onchange={({ currentTarget }) =>
-                updateDocumentDataFromField(
-                    item,
-                    `system.actions.${actionId}.name`,
-                    currentTarget.value,
-                )}
-        />
-    </header>
-
-    <NavigationBar
-        {currentTab}
-        {tabs}
-        showLock={false}
-        onTabChange={updateCurrentTab}
+  <header class="a5e-action-sheet__header">
+    <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <img
+      class="a5e-action-sheet__header-image"
+      src={action.img || item.reactive.img}
+      alt="{action.name} image"
+      onclick={() => editDocumentImage(item, { actionId })}
     />
 
-    <section class="a5e-action-sheet__page">
-        <currentTab.component />
-    </section>
+    <input
+      class="a5e-input a5e-input--character-name"
+      type="text"
+      value={action.name}
+      placeholder="Action Name"
+      onchange={({ currentTarget }) =>
+        updateDocumentDataFromField(
+          item,
+          `system.actions.${actionId}.name`,
+          currentTarget.value,
+        )}
+    />
+  </header>
+
+  <NavigationBar
+    {currentTab}
+    {tabs}
+    showLock={false}
+    onTabChange={updateCurrentTab}
+  />
+
+  <section class="a5e-action-sheet__page">
+    <currentTab.component />
+  </section>
 </main>
 
 <style lang="scss">
-    .a5e-action-sheet {
-        position: relative;
-        width: 100%;
-        height: 100%;
+  .a5e-action-sheet {
+    position: relative;
+    width: 100%;
+    height: 100%;
 
-        display: grid;
-        grid-template-areas:
-            "header"
-            "primaryNavigation"
-            "page"
-            "footer";
-        grid-template-rows: min-content min-content 1fr min-content;
-        gap: 0.5rem;
+    display: grid;
+    grid-template-areas:
+      "header"
+      "primaryNavigation"
+      "page"
+      "footer";
+    grid-template-rows: min-content min-content 1fr min-content;
+    gap: 0.5rem;
 
-        padding-inline: 0.5rem;
-        padding-block: 0.5rem;
+    padding-inline: 0.5rem;
+    padding-block: 0.5rem;
 
-        &__header {
-            grid-area: header;
-            display: flex;
-            align-items: center;
-            gap: 0.5rem;
+    &__header {
+      grid-area: header;
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
 
-            &-image {
-                width: 3rem;
-                height: 3rem;
-                border-radius: 4px;
-                cursor: pointer;
-            }
-        }
-
-        &__page {
-            grid-area: page;
-            overflow-y: auto;
-        }
+      &-image {
+        width: 3rem;
+        height: 3rem;
+        border-radius: 4px;
+        cursor: pointer;
+      }
     }
+
+    &__page {
+      grid-area: page;
+      overflow-y: auto;
+    }
+  }
 </style>
